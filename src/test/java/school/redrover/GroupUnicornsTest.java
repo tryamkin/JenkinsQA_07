6f9a49b6476b441a9ae1155b35800932af6329d1
PeterKuzyk <55903558+PeterKuzyk@users.noreply.github.com>
2023-10-04 18:42:49 -0400

    Peter Kuzuk home work test (#12)
    * added usPsPageOpenTest
    
    * added usPsSendMailPackageTest

--- src/test/java/school/redrover/GroupUnicornsTest.java
+++ src/test/java/school/redrover/GroupUnicornsTest.java
@@ -1,4 +1,32 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
 public class GroupUnicornsTest {
+
+    @Test
+    public void usPsPageOpenTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.usps.com/");
+        String title = driver.getTitle();
+        assertEquals("Welcome | USPS", title);
+        driver.quit();
+    }
+
+    @Test
+    public void usPsSendMailPackageTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.usps.com/");
+        WebElement send = driver.findElement(By.xpath("//a[@id='mail-ship-width']"));
+        send.click();
+        String sendTitle = driver.getTitle();
+        assertEquals("Send Mail & Packages | USPS", sendTitle);
+        driver.quit();
+    }
 }
