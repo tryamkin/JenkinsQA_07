798ea72eb65c88d0d4c4b691493e5fbae524776c
SergeiDemyanenko <Sergei.Demyanenko@gmail.com>
2023-10-03 20:29:40 -0500

    Add first test (#1)
    Add first test

---
+++ src/test/java/school/redrover/FirstTest.java
@@ -0,0 +1,32 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class FirstTest {
+
+    @Test
+    public void  testSearch() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+
+        WebElement textBox = driver.findElement(By.className("gLFyf"));
+        textBox.sendKeys("Selenium");
+
+        Thread.sleep(1000);
+
+        WebElement searchButton = driver.findElement(By.className("gNO89b"));
+        searchButton.click();
+
+        WebElement title = driver.findElement(By.className("yKMVIe"));
+        String value = title.getText();
+        Assert.assertEquals(value, "Selenium");
+
+        driver.quit();
+    }
+}
