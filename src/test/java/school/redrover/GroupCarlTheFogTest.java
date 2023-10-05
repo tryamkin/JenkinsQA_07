3b423caf016e1ef8e8f90677a133d3a7a2d0e6d1
sergeykazak <sergey.kazakov@gmail.com>
2023-10-06 01:03:38 +0300

    sleep value change (#64)

---
+++ src/test/java/school/redrover/GroupCarlTheFogTest.java
@@ -0,0 +1,27 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GroupCarlTheFogTest {
+    @Test
+    public void hireRightTest() {
+
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.hireright.com");
+
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Employment Background Checks, Background Screening | HireRight");
+
+        WebElement cacheButton = driver.findElement(By.xpath("//div[@class='CookieConsent']//button[contains(text(), 'Continue')]"));
+
+        cacheButton.click();
+        driver.quit();
+
+    }
+}
