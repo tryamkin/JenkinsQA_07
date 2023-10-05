e375d7bda69753263251ee5357dececb1114651d
Mac User <macuser@MacBook-Pro-Mac.local>
2023-10-04 19:55:24 -0500

    Iron Teame first test of w3school

--- src/test/java/school/redrover/IronTeamGroupTest.java
+++ src/test/java/school/redrover/IronTeamGroupTest.java
@@ -1,2 +1,48 @@
-package school.redrover;public class IronTeamGroupTest {
+package school.redrover;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class IronTeamGroupTest {
+    @Test
+    public void w3schoolTest() throws InterruptedException {
+        // Check Title of site
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.get("https://www.w3schools.com/");
+            WebElement title = driver.findElement(By.cssSelector("h1.learntocodeh1"));
+            String value = title.getText();
+            Assert.assertEquals(value, "Learn to Code");
+        } finally {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void javaPageTest() throws InterruptedException {
+        // Check Java page of site
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.get("https://www.w3schools.com/");
+            WebElement textBox = driver.findElement(By.id("search2"));
+            textBox.sendKeys("Java Tutorial");
+            WebElement searchButton = driver.findElement(By.id("learntocode_searchbtn"));
+            Thread.sleep(500);
+            searchButton.click();
+           WebElement title = driver.findElement(By.cssSelector("h1"));
+            String value = title.getText();
+           Assert.assertEquals(value, "Java Tutorial");
+        } finally {
+            driver.quit();
+        }
+    }
 }
