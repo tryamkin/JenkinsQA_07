f955129ff6c7e8c19927a27ec959fb2d1f277d50
Alexandr Sdobnov <126351854+SawkaQA@users.noreply.github.com>
2023-10-05 14:26:59 +0300

    As/refactor (#24)
    * refactor: add driver and refactor code

--- src/test/java/school/redrover/PlusThreeTest.java
+++ src/test/java/school/redrover/PlusThreeTest.java
@@ -4,13 +4,16 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PlusThreeTest {
-  
+
     public static final String USERNAME = "TestUser1";
+
+    public static final String PASSWORD = "qwert12345";
     public static final String FULL_NAME = "Akiko";
     public static final String EMAIL = "Akiko@gmail.com";
     public static final String CURRENT_ADDRESS = "USA";
@@ -30,14 +33,14 @@ public class PlusThreeTest {
 
         WebElement currentAddress = driver.findElement(By.id("currentAddress"));
         currentAddress.sendKeys(CURRENT_ADDRESS);
-        
+
         WebElement permanentAddress = driver.findElement(By.id("permanentAddress"));
         permanentAddress.sendKeys(PERMANENT_ADDRESS);
 
         WebElement submitButton = driver.findElement(By.id("submit"));
         submitButton.click();
 
-        
+
         WebElement nameR = driver.findElement(By.id("name"));
         String value = nameR.getText();
         Assert.assertEquals("Name:" + FULL_NAME, value);
@@ -56,45 +59,56 @@ public class PlusThreeTest {
 
         driver.quit();
     }
-  
-   @Test(description = "Создание/регистрация пользователя в банке")
-   public void createUser() {
-       WebDriver driver = new ChromeDriver();
-       driver.manage().window().maximize();
-       driver.get("https://parabank.parasoft.com/parabank/register.htm");
 
-       WebElement firstName = driver.findElement(By.id("customer.firstName"));
-       WebElement lastName = driver.findElement(By.id("customer.lastName"));
-       WebElement address = driver.findElement(By.id("customer.address.street"));
-       WebElement city = driver.findElement(By.id("customer.address.city"));
-       WebElement state = driver.findElement(By.id("customer.address.state"));
-       WebElement zipCode = driver.findElement(By.id("customer.address.zipCode"));
-       WebElement userName = driver.findElement(By.id("customer.username"));
-       WebElement password = driver.findElement(By.id("customer.password"));
-       WebElement prepaerPass = driver.findElement(By.id("repeatedPassword"));
-       WebElement ssn = driver.findElement(By.id("customer.ssn"));
-       WebElement register = driver.findElement(By.cssSelector("[value='Register']"));
+    @Test(description = "Создание/регистрация пользователя в банке")
+    public void createUser() {
 
-       firstName.sendKeys("Test");
-       lastName.sendKeys("Test");
-       address.sendKeys("SPB");
-       city.sendKeys("SPB");
-       state.sendKeys("LO");
-       zipCode.sendKeys("123-415");
-       userName.sendKeys(USERNAME);
-       password.sendKeys("qwert12345");
-       prepaerPass.sendKeys("qwert12345");
-       ssn.sendKeys("123456");
-       register.submit();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://parabank.parasoft.com/parabank/register.htm");
 
-       WebElement title = driver.findElement(By.xpath("//div[@id='rightPanel']/h1"));
-       String resTitle = title.getText();
-       WebElement result = driver.findElement(By.xpath("//div[@id='rightPanel']/p"));
-       String resText = result.getText();
+        WebElement firstName = driver.findElement(By.id("customer.firstName"));
+        firstName.sendKeys("Test");
 
-       Assert.assertEquals(resTitle,"Welcome " + USERNAME);
-       Assert.assertEquals(resText, "Your account was created successfully. You are now logged in.");
+        WebElement lastName = driver.findElement(By.id("customer.lastName"));
+        lastName.sendKeys("Test");
 
-       driver.quit();
+        WebElement address = driver.findElement(By.id("customer.address.street"));
+        address.sendKeys("SPB");
+
+        WebElement city = driver.findElement(By.id("customer.address.city"));
+        city.sendKeys("SPB");
+
+        WebElement state = driver.findElement(By.id("customer.address.state"));
+        state.sendKeys("LO");
+
+        WebElement zipCode = driver.findElement(By.id("customer.address.zipCode"));
+        zipCode.sendKeys("123-415");
+
+        WebElement userName = driver.findElement(By.id("customer.username"));
+        userName.sendKeys(USERNAME);
+
+        WebElement password = driver.findElement(By.id("customer.password"));
+        password.sendKeys(PASSWORD);
+
+        WebElement repeatPass = driver.findElement(By.id("repeatedPassword"));
+        repeatPass.sendKeys(PASSWORD);
+
+        WebElement ssn = driver.findElement(By.id("customer.ssn"));
+        ssn.sendKeys("123456");
+
+        WebElement register = driver.findElement(By.cssSelector("[value='Register']"));
+        register.submit();
+
+
+        WebElement title = driver.findElement(By.xpath("//div[@id='rightPanel']/h1"));
+        String resTitle = title.getText();
+        Assert.assertEquals(resTitle, "Welcome " + USERNAME);
+
+        WebElement result = driver.findElement(By.xpath("//div[@id='rightPanel']/p"));
+        String resText = result.getText();
+        Assert.assertEquals(resText, "Your account was created successfully. You are now logged in.");
+
+        driver.quit();
     }
 }
