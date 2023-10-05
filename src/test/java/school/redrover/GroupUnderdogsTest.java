738e5541411ac9369386d4121fab52d807fb66a5
maksin82 <maksin82@gmail.com>
2023-10-05 21:30:03 +0300

    sleep (#49)

--- src/test/java/school/redrover/GroupUnderdogsTest.java
+++ src/test/java/school/redrover/GroupUnderdogsTest.java
@@ -4,14 +4,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class GroupUnderdogsTest {
 
+    WebDriver driver;
+
+//    @BeforeMethod
+//    public void before() {
+//        driver = new FirefoxDriver();
+//    }
+
+    @AfterMethod
+    public void after() {
+        driver.quit();
+    }
+
     @Test
     public void tereshenkov99BottlesTitleTest() {
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("http://www.99-bottles-of-beer.net/");
 
         WebElement title = driver.findElement(By.xpath("//*[@id=\"header\"]/h1"));
@@ -19,12 +34,11 @@ public class GroupUnderdogsTest {
 
         Assert.assertEquals(titleValue, "99 Bottles of Beer");
 
-        driver.quit();
     }
 
     @Test
     public void tereshenkov99BottlesLastMenuLinkTest() {
-        WebDriver driver = new ChromeDriver();
+    driver = new ChromeDriver();
         driver.get("http://www.99-bottles-of-beer.net/");
 
         WebElement lastMenuLink = driver.findElement(By.xpath("//*[@id=\"menu\"]/li[6]/a"));
@@ -37,7 +51,18 @@ public class GroupUnderdogsTest {
 //         String lastMenuLinkValue = lastMenuLink.getText();
 //         Assert.assertEquals(lastMenuLinkValue, "SUBMIT NEW LANGUAGE");
 
-        driver.quit();
+    }
+
+    //TC_11_07 что надпись об исключении красным цветом и с маленькой буквы
+    @Test
+    public void maksinTestInactive() {
+        driver = new FirefoxDriver();
+        driver.get("http://www.99-bottles-of-beer.net/team.html");
+        WebElement header = driver.findElement(By.xpath
+                ("/html/body/div/div[3]/p[7]/font/b"));
+        //Assert.assertTrue(header.getText().toLowerCase().equals(header.getText())); //одинаковые
+        Assert.assertEquals(header.getText(), header.getText().toLowerCase());     //одинаковые
+        Assert.assertEquals(header.getCssValue("color"), "rgb(255, 0, 0)");
     }
 
 }
