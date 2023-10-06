9329455fdd9a450c5dc44be6a011d2aace3e1c8a
densentest <74663686+densentest@users.noreply.github.com>
2023-10-06 15:21:06 +0200

    first test for SeleniumWebSite (#94)
    Co-authored-by: denys.sennikov <denys.sennikov@creatoriq.com>

--- src/test/java/school/redrover/GroupJavaBustersTest.java
+++ src/test/java/school/redrover/GroupJavaBustersTest.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -91,6 +94,43 @@ public class GroupJavaBustersTest {
 
         String value = fieldUsername.getText();
         assertEquals(value,"");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testAllFields() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        String mainLink = "https://www.selenium.dev/selenium/web/web-form.html";
+        String indexLink = "https://www.selenium.dev/selenium/web/index.html";
+
+        driver.get(mainLink);
+        WebElement textInput = driver.findElement(By.xpath("//input[@id='my-text-id']"));
+        WebElement passwordInput = driver.findElement(By.xpath("//label[2]/input[1]"));
+        WebElement textArea = driver.findElement(By.xpath("//label[3]/textarea[1]"));
+        WebElement  disableInput = driver.findElement(By.xpath("//label[4]/input[1]"));
+        WebElement  readOnlyField = driver.findElement(By.xpath("//label[5]/input[1]"));
+        WebElement linkReturnToIndex = driver.findElement(By.xpath("//a[contains(text(),'Return to index')]"));
+
+        textInput.sendKeys("test");
+        passwordInput.sendKeys("12345678");
+        textArea.sendKeys("This is a test text for text area");
+        Assert.assertFalse(disableInput.isEnabled());
+        Assert.assertTrue(true, readOnlyField.getAttribute("true"));
+
+        linkReturnToIndex.click();
+        Thread.sleep(2000);
+        String currentLink = driver.getCurrentUrl();
+        Assert.assertEquals(currentLink, indexLink);
+
+        driver.get(mainLink);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement dropdownSelect = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@name='my-select']")));
+        Select dropDownSelectedValue = new Select(dropdownSelect);
+        dropDownSelectedValue.selectByValue("1");
+        dropDownSelectedValue.selectByValue("2");
+        dropDownSelectedValue.selectByValue("3");
 
         driver.quit();
     }
