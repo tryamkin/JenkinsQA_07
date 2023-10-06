20f5124147eccdccf902c59200bae398c7fb55e4
Stoyana <101792053+StoyanaDi@users.noreply.github.com>
2023-10-06 15:10:42 +0300

    add testAllElementsOnFirstPage test (#90)
    fixed locator in testTextBox test

--- src/test/java/school/redrover/GroupQaClimbersTest.java
+++ src/test/java/school/redrover/GroupQaClimbersTest.java
@@ -8,6 +8,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.testng.Assert.assertEquals;
 
 public class GroupQaClimbersTest {
@@ -20,7 +24,7 @@ public class GroupQaClimbersTest {
             driver.get("https://demoqa.com");
             driver.manage().window().maximize();
 
-            driver.findElement(By.xpath("//div[@class='card-up'][1]")).click();
+            driver.findElement(By.xpath("(//div[@class='card-up'])[1]")).click();
             driver.findElement(By.xpath("//span[ contains(text(), 'Text Box')]")).click();
 
             WebElement inputName = driver.findElement(By.id("userName"));
@@ -172,6 +176,32 @@ public class GroupQaClimbersTest {
         Assert.assertEquals(actualResult,"You have selected :");
 
         driver.quit();
+    }
+
+    @Test
+    public void testAllElementsOnFirstPage() {
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.get("https://demoqa.com/");
+
+            List<WebElement> listOfWebElements = driver
+                    .findElements(By.xpath("//div[@class='card mt-4 top-card']"));
+
+            List<String> actualListOfElementsNames = new ArrayList<>();
+
+            for (WebElement el : listOfWebElements) {
+                actualListOfElementsNames.add(el.getText());
+            }
+
+            List<String> expectedListElementsNames = Arrays.asList(
+                    "Elements", "Forms", "Alerts, Frame & Windows",
+                    "Widgets", "Interactions", "Book Store Application"
+            );
+            assertEquals(actualListOfElementsNames, expectedListElementsNames);
+
+        } finally {
+            driver.quit();
+        }
     }
 
 }
