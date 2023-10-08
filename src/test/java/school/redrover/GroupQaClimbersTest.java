876edf9131e3cb453a9bdb36be5ae2f1f850d10f
nat-belyaeva <70035314+nat-belyaeva@users.noreply.github.com>
2023-10-08 20:24:01 +0300

    use diff locators (#176)

--- src/test/java/school/redrover/GroupQaClimbersTest.java
+++ src/test/java/school/redrover/GroupQaClimbersTest.java
@@ -454,4 +454,88 @@ public class GroupQaClimbersTest {
 
         driver.quit();
     }
+
+    @Test
+    public void useDiffSeleniumLocators() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.manage().window().maximize();
+        try {
+            WebElement h1Text = driver.findElement(By.cssSelector("form h1"));
+            String h1TextValue = h1Text.getText();
+            Assert.assertEquals("Sign in", h1TextValue);
+
+            WebElement nameInput = driver.findElement(By.id("inputUsername"));
+            nameInput.sendKeys("rahulshetty");
+
+            WebElement password = driver.findElement(By.name("inputPassword"));
+            password.sendKeys("hello123");
+
+            WebElement submitButton = driver.findElement(By.className("signInBtn"));
+            submitButton.click();
+            Thread.sleep(1000);
+            WebElement errorMsg = driver.findElement(By.cssSelector("p.error"));
+            String errorMessageText = errorMsg.getText();
+            Assert.assertEquals("* Incorrect username or password", errorMessageText);
+
+            WebElement forgotPasswordLink = driver.findElement(By.linkText("Forgot your password?"));
+            forgotPasswordLink.click();
+            Thread.sleep(1000);
+            WebElement h2Text = driver.findElement(By.cssSelector("h2"));
+            String h2TextValue = h2Text.getText();
+
+            Assert.assertEquals("Forgot password", h2TextValue);
+            WebElement nameNew = driver.findElement(By.xpath("//input[@placeholder='Name']"));
+            nameNew.sendKeys("John");
+            WebElement email = driver.findElement(By.xpath("//input[@type='text'][2]")); //By.cssSelector("input[type='text']:nth-child(4)")
+            email.sendKeys("123123");
+            email.clear();
+            email.sendKeys("new@new.new");
+
+
+            WebElement phoneNumber = driver.findElement(By.xpath("//input[@placeholder='Phone Number']"));
+            phoneNumber.sendKeys("1234654654646");
+
+            WebElement resetLogin = driver.findElement(By.xpath("//button[@class='reset-pwd-btn']"));
+            resetLogin.click();
+
+            Thread.sleep(500);
+            WebElement infoMsg = driver.findElement(By.xpath("//p[@class='infoMsg']"));
+            String infoMsgValue = infoMsg.getText();
+
+            Assert.assertEquals("Please use temporary password 'rahulshettyacademy' to Login.", infoMsgValue);
+
+        } finally {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void locatorsPart2() throws InterruptedException {
+        //Generating Css selectors based on regular expressions
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.manage().window().maximize();
+
+        WebElement nameInput = driver.findElement(By.cssSelector("#inputUsername"));
+        nameInput.sendKeys("rahul");
+
+        WebElement password = driver.findElement(By.cssSelector("input[type*='pass']")); //* regular exp
+        password.sendKeys("rahulshettyacademy");
+
+        WebElement rememberMyUserNameCheckbox = driver.findElement(By.xpath("//input[@id='chkboxOne']"));
+        WebElement submitButton = driver.findElement(By.xpath("//button[contains(@class, 'submit')]")); //contains method for xpath
+
+        rememberMyUserNameCheckbox.click();
+        submitButton.click();
+        Thread.sleep(1000);
+        WebElement successLoginText = driver.findElement(By.xpath("//p[@style]"));
+        String successLoginTextVal = successLoginText.getText();
+        Assert.assertEquals("You are successfully logged in.", successLoginTextVal);
+
+        driver.quit();
+    }
+
 }
