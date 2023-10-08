b2abfc168d91f124b399c20d5dadc46a3b8bba39
Anna <anna.khizhnyakova@gmail.com>
2023-10-08 11:17:49 +0300

    testCreateAcc added (#152)

--- src/test/java/school/redrover/GroupHighwayToAqaTest.java
+++ src/test/java/school/redrover/GroupHighwayToAqaTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.dom.model.ShadowRootType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -35,6 +36,55 @@ public class GroupHighwayToAqaTest {
         WebElement alertMessage = driver.findElement(By.className("alert-danger"));
 
         Assert.assertEquals(alertMessage.getText(), "Authentication failed.");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testCreateAcc() throws InterruptedException {
+        String firstName = "firstName";
+        String lastName = "LastName";
+        String password = "Qwe123!@#ASD";
+        String email = "test@mail.com";
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.prestashop.com/#/en/front");
+
+        driver.switchTo().frame("framelive");
+
+        WebElement signInLink = driver.findElement(By.xpath("//span[text()='Sign in']"));
+        signInLink.click();
+
+        WebElement createAccLink = driver.findElement(By.xpath("//a[contains(.,'No account? Create one here')]"));
+        createAccLink.click();
+
+        WebElement genderMr = driver.findElement(By.xpath("//label[contains(.,'Mrs.')]"));
+        genderMr.click();
+
+        WebElement firstnameInput = driver.findElement(By.xpath("//input[@id='field-firstname']"));
+        WebElement lastnameInput = driver.findElement(By.xpath("//input[@id='field-lastname']"));
+        WebElement emailInput = driver.findElement(By.xpath("//input[@id='field-email']"));
+        WebElement passwordInput = driver.findElement(By.xpath("//input[@id='field-password']"));
+        WebElement ifReceiveOffers = driver.findElement(By.xpath("//input[@name='optin']"));
+        WebElement ifAgreeTerms = driver.findElement(By.xpath("//input[@name='psgdpr']"));
+        WebElement ifGetNewsLetters = driver.findElement(By.xpath("//input[@name='newsletter']"));
+        WebElement ifCustomer_privacy = driver.findElement(By.xpath("//input[@name='customer_privacy']"));
+        WebElement saveBtn = driver.findElement(By.cssSelector(".form-control-submit"));
+
+        firstnameInput.sendKeys(firstName);
+        lastnameInput.sendKeys(lastName);
+        emailInput.sendKeys(email);
+        passwordInput.sendKeys(password);
+        ifReceiveOffers.click();
+        ifAgreeTerms.click();
+        ifGetNewsLetters.click();
+        ifCustomer_privacy.click();
+        saveBtn.click();
+
+        WebElement ifRegistered = driver.findElement(By.cssSelector(".account > .hidden-sm-down"));
+
+        Assert.assertEquals(ifRegistered.getText(), firstName + " " + lastName);
 
         driver.quit();
     }
