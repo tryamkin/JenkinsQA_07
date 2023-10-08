2d52ebe0e54ba5eca3e12a4780e83f68fadb48c2
jokerSharp <137673317+jokerSharp@users.noreply.github.com>
2023-10-08 20:26:48 +0400

    2 tests for woodworking website (#160)

--- src/test/java/school/redrover/LetsQAGroupTest.java
+++ src/test/java/school/redrover/LetsQAGroupTest.java
@@ -1,18 +1,22 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class LetsQAGroupTest {
+    private static final String BASE_URL = "https://www.sawinery.net/";
+    WebDriver driver = new ChromeDriver();
     @Test
     public void testChrome(){
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
@@ -33,4 +37,37 @@ public class LetsQAGroupTest {
 
         driver.quit();
     }
+
+
+    @Test
+    public void checkTitle() {
+        driver.get(BASE_URL);
+
+        String title = driver.getTitle();
+        Assert.assertEquals("Sawinery - #1 Woodworking Education Resource", title);
+    }
+    @Test
+    public void searchTest() {
+        try {
+            driver.get(BASE_URL);
+
+            WebElement searchField = driver.findElement(By.xpath("//*[@id='elementor-search-form-77435a6']"));
+            searchField.sendKeys("saw");
+            searchField.sendKeys(Keys.ENTER);
+
+            Thread.sleep(1000);
+
+            WebElement searchResult = driver.findElement(By.xpath("//*[@id=\"content\"]/div/section[2]/div/div/div/div/div/div/article[1]/div/h3/a"));
+            String searchText = searchResult.getAttribute("textContent").toLowerCase();
+            Assert.assertTrue(searchText.contains("saw"));
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @AfterTest
+    public void quitBrowser() {
+        driver.quit();
+    }
+
 }
