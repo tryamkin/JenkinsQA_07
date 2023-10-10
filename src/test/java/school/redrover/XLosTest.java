19dd28013c0afc3ebd4e388ed4f6d0684f2e9995
Xeniya Loskutova <100655741+xloskutova@users.noreply.github.com>
2023-10-10 09:47:09 -0500

    Initial commit and PR test (#247)

---
+++ src/test/java/school/redrover/XLosTest.java
@@ -0,0 +1,41 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class XLosTest {
+
+    @Test
+    public void SauceDemoPositiveLoginTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
+
+        try {
+            driver.get("https://www.saucedemo.com/");
+            WebElement username = driver.findElement(By.id("user-name"));
+            WebElement password = driver.findElement(By.id("password"));
+            WebElement loginBtn = driver.findElement(By.id("login-button"));
+
+            username.sendKeys("standard_user");
+            password.sendKeys("secret_sauce");
+            loginBtn.click();
+
+            Thread.sleep(2000);
+
+            String expectedTitle = "Swag Labs";
+            String actualTitle = driver.getTitle();
+
+            Assert.assertEquals(actualTitle, expectedTitle, "Login failed.");
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            driver.quit();
+        }
+    }
+}
