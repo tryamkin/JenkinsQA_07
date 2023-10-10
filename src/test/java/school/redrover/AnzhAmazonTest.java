8c4d6dc19155563c8f77dbb7f745b9a66b8950ab
Anzhelika Lashko <120133812+AnzhelikaLa@users.noreply.github.com>
2023-10-10 11:54:51 -0500

    Anzhelika/amazon test (#231)
    * Add new Amazon test
    
    * Add new Amazon test

---
+++ src/test/java/school/redrover/AnzhAmazonTest.java
@@ -0,0 +1,45 @@
+package school.redrover;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AnzhAmazonTest {
+    private final String PAGE_URL = "https://www.amazon.com/";
+
+    @Test
+    public void testSearch() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.get(PAGE_URL);
+
+            WebElement searchBox = driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']"));
+
+            searchBox.sendKeys("Selenium WebDriver");
+
+
+            searchBox.submit();
+
+
+            Thread.sleep(2000);
+
+
+        } finally {
+           driver.quit();
+        }
+    }
+
+
+
+
+}
+
+
+
+
+
