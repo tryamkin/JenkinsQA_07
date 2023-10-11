4226fff4913a6d3375f9920612bacbdc512ea816
a20l <aidaytyn@gmail.com>
2023-10-10 23:09:11 -0500

    refactored code

--- src/test/java/school/redrover/AikaTest.java
+++ src/test/java/school/redrover/AikaTest.java
@@ -1,68 +1,45 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
+import school.redrover.runner.BaseTest;
 
 import static org.openqa.selenium.support.locators.RelativeLocator.with;
 
-@Ignore
-public class AikaTest {
 
-    WebDriver driver;// = new ChromeDriver();
-    String categoriesOpt = "Categories";
-
-    @BeforeMethod
-    public void setup() {
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        driver.get("https://mvnrepository.com/");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
+public class AikaTest extends BaseTest {
 
     @Test
-    public void relativeLocationToLeftOfTest() {
+    public void testRelativeLocationToLeftOf() {
+        String categoriesOpt = "Categories";
+        getDriver().get("https://mvnrepository.com/");
+        // JenkinsUtils.login(getDriver());
+        WebElement popularOpt = getDriver().findElement(By.xpath("//a[text()= 'Popular']"));
 
-        WebElement popularOpt = driver.findElement(By.xpath("//a[text()= 'Popular']"));
-
-        Assert.assertEquals(driver.findElement(with(By.tagName("a"))
+        Assert.assertEquals(getDriver().findElement(with(By.tagName("a"))
                 .toLeftOf(popularOpt)).getText(), categoriesOpt);
-
     }
 
     @Test
-    public void relativeLocationBelowTest() {
+    public void testRelativeLocationBelow() {
+        getDriver().get("https://mvnrepository.com/");
+        WebElement siteDeveloped = getDriver().findElement(By.xpath("//span[contains(text(), 'developed')]"));
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(siteDeveloped).perform();
 
-         WebElement siteDeveloped = driver.findElement(By.xpath("//span[contains(text(), 'developed')]"));
-         Actions actions = new Actions(driver);
-         actions.moveToElement(siteDeveloped).perform();
-
-         Assert.assertEquals(driver.findElement(with(By.tagName("a")).below(siteDeveloped))
-                 .getText(), "Contact Us");
+        Assert.assertEquals(getDriver().findElement(with(By.tagName("a")).below(siteDeveloped))
+                .getText(), "Contact Us");
     }
 
     @Test
-    public void relativeLocationAboveTest() {
-
-        WebElement whatsNew = driver.findElement(By.tagName("h1"));
-        WebElement inputField = driver.findElement(with(By.id("query")).above(whatsNew));
+    public void testRelativeLocationAbove() {
+        getDriver().get("https://mvnrepository.com/");
+        WebElement whatsNew = getDriver().findElement(By.tagName("h1"));
+        WebElement inputField = getDriver().findElement(with(By.id("query")).above(whatsNew));
 
         Assert.assertTrue(inputField.isDisplayed());
-
     }
-
-
 }
