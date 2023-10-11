f8ea6664368b5788654d5cd838afecdc8575408f
dmitri.panichin <dmitri.panichin@amdaris.com>
2023-10-11 23:35:17 +0300

    add a test

--- src/test/java/school/redrover/LocomotiveGroupTest.java
+++ src/test/java/school/redrover/LocomotiveGroupTest.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
@@ -138,4 +139,51 @@ public class LocomotiveGroupTest extends BaseTest {
             driver.quit();
         }
       }
+
+    @Test
+
+    public void simpleSearchTest() throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://999.md/");
+
+        WebElement categoryButton = driver.findElement(By.xpath("//button[@id='js-categories-toggle']"));
+        categoryButton.click();
+
+        WebElement autoButton = driver.findElement(By.xpath("//a[contains(text(),'Autoturisme')]"));
+        autoButton.click();
+
+        Thread.sleep(1000);
+
+        WebElement masini = driver.findElement(By.xpath("//h1[contains(text(),'Mașini')]"));
+        String value = masini.getText();
+        Assert.assertEquals(value, "Mașini");
+
+        WebElement searchInput = driver.findElement(By.xpath("//input[@id='js-search-input']"));
+        searchInput.click();
+
+        WebElement input = driver.findElement(By.xpath("//input[@id='js-search-input']"));
+        input.sendKeys("audi a6");
+
+        WebElement searchButton = driver.findElement(By.xpath("//span[contains(.,'Caută')]"));
+        searchButton.click();
+
+        WebElement title = driver.findElement(By.xpath("//span[contains(text(),'Rezultatele căutării')]"));
+        String value1 = title.getText();
+        Assert.assertEquals(value1,"Rezultatele căutării \"audi a6\" (2 607 anunţuri)");
+
+        WebElement searchInput1 = driver.findElement(By.xpath("//input[@id='js-search-input']"));
+        searchInput1.click();
+
+        Thread.sleep(3000);
+
+        WebElement dropDown= driver.findElement(By.xpath("//html[1]/body[1]/div[3]/header[1]/div[2]/div[1]/form[1]/fieldset[1]/span[1]/div[1]/div[1]/div[1]"));
+        dropDown.click();
+
+        WebElement title1 = driver.findElement(By.xpath("//span[contains(text(),'Autoturisme')]"));
+        String value2 = title1.getText();
+        Assert.assertEquals(value2,"Autoturisme (600)");
+
+        driver.quit();
+    }
 }
