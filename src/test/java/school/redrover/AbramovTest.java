caae73c011335688108a6d1b72fbf69621c6871f
AlexAbramovWork <alexabramov.work@gmail.com>
2023-10-11 20:39:13 +0300

    Simple refactor commit

--- src/test/java/school/redrover/AbramovTest.java
+++ src/test/java/school/redrover/AbramovTest.java
@@ -1,62 +1,49 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
 
-@Ignore
-public class AbramovTest {
-    WebDriver driver;
-    @BeforeTest
-    public void beforeTest(){
-        //driver = new ChromeDriver();
+public class AbramovTest extends BaseTest {
+
+    private void openMainPage() {
+        getDriver().get("https://bandcamp.com/");
     }
+    @Test
+    public void testTitle() {
+        openMainPage();
 
-    @AfterTest
-    public void afterTest(){
-        //driver.quit();
+        Assert.assertEquals(getDriver().getTitle(),"Bandcamp");
     }
 
     @Test
-    public void titleTest(){
-        driver.get("https://bandcamp.com/");
+    public void testSearchByTagSelect() {
+        openMainPage();
 
-        String titleValue = driver.findElement(By.cssSelector("h1")).getText();
-        Assert.assertEquals(titleValue,"Bandcamp");
-    }
-
-    @Test
-    public void searchByTagSelectTest(){
-        driver.get("https://bandcamp.com/");
-
-        WebElement searchForm = driver.findElement(By.xpath("//div[@id='corphome-autocomplete-form']//input[contains(@class, 'search-bar')]"));
+        WebElement searchForm = getDriver().findElement(By.xpath("//div[@id='corphome-autocomplete-form']//input[contains(@class, 'search-bar')]"));
         searchForm.click();
 
-        WebElement electronicTag = driver.findElement(By.xpath("//ul[@class='genre-list']//a[contains(@href, 'electronic')]"));
+        WebElement electronicTag = getDriver().findElement(By.xpath("//ul[@class='genre-list']//a[contains(@href, 'electronic')]"));
         electronicTag.click();
 
-        String title = driver.findElement(By.xpath("//h1[@class='name']")).getText();
+        String title = getDriver().findElement(By.xpath("//h1[@class='name']")).getText();
+
         Assert.assertEquals(title, "electronic");
     }
 
     @Test
-    public void searchByTagTypeTest() throws InterruptedException {
-        driver.get("https://bandcamp.com/");
+    public void testSearchByTagType() {
+        openMainPage();
 
-        WebElement searchForm = driver.findElement(By.xpath("//div[@id='corphome-autocomplete-form']//input[contains(@class, 'search-bar')]"));
+        WebElement searchForm = getDriver().findElement(By.xpath("//div[@id='corphome-autocomplete-form']//input[contains(@class, 'search-bar')]"));
         searchForm.sendKeys("trance");
 
-        Thread.sleep(1000);
-        WebElement typedTag = driver.findElement(By.xpath("//li[@class='simple results-tags']//span[contains(text(),'trance')]"));
+        WebElement typedTag = getDriver().findElement(By.xpath("//li[@class='simple results-tags']//span[contains(text(),'trance')]"));
         typedTag.click();
 
-        String title = driver.findElement(By.xpath("//h1[@class='name']")).getText();
+        String title = getDriver().findElement(By.xpath("//h1[@class='name']")).getText();
         Assert.assertEquals(title, "trance");
     }
 }
