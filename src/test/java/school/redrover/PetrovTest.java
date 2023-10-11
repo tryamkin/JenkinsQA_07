33aa6deddda3e40de291ad9d043b1b8c544597b7
pashavik <pash.petrow2011@yandex.ru>
2023-10-11 20:59:00 +0300

    add testTextBoxForm and testPracticeForm for demoqa

--- src/test/java/school/redrover/PetrovTest.java
+++ src/test/java/school/redrover/PetrovTest.java
@@ -1,9 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -13,44 +11,37 @@ import school.redrover.runner.JenkinsUtils;
 public class PetrovTest extends BaseTest {
 
     @Test
-    public void testTextBoxForm(){
+    public void testTextBoxForm() {
 
-        JenkinsUtils.login(getDriver());
+        getDriver().get("https://demoqa.com/text-box");
 
-        try {
-            getDriver().get("https://demoqa.com/text-box");
+        WebElement fullName = getDriver().findElement(By.xpath("//*[@id=\"userName\"]"));
+        fullName.sendKeys("testName");
 
-            WebElement fullName = getDriver().findElement(By.xpath("//*[@id=\"userName\"]"));
-            fullName.sendKeys("testName");
+        WebElement email = getDriver().findElement(By.id("userEmail"));
+        email.sendKeys("test@gmail.com");
 
-            WebElement email = getDriver().findElement(By.id("userEmail"));
-            email.sendKeys("test@gmail.com");
+        WebElement submitButton = getDriver().findElement(By.id("submit"));
+        submitButton.click();
 
-            WebElement submitButton = getDriver().findElement(By.id("submit"));
-            submitButton.click();
+        WebElement nameS = getDriver().findElement(By.id("name"));
+        WebElement emailS = getDriver().findElement(By.id("email"));
 
-            WebElement nameS = getDriver().findElement(By.id("name"));
-            WebElement emailS = getDriver().findElement(By.id("email"));
-
-            Assert.assertEquals(nameS.getText(), "Name:testName");
-            Assert.assertEquals(emailS.getText(), "Email:test@gmail.com");
-            Assert.assertNotEquals(emailS.getText(), "");
-
-        } finally {
-            getDriver().quit();
-        }
+        Assert.assertEquals(nameS.getText(), "Name:testName");
+        Assert.assertEquals(emailS.getText(), "Email:test@gmail.com");
+        Assert.assertNotEquals(emailS.getText(), "");
     }
 
     @Test
-    public void testPracticeForm(){
-        WebDriver driver=new ChromeDriver();
-        driver.get("https://demoqa.com/automation-practice-form");
+    public void testPracticeForm() {
 
-        WebElement firstName=driver.findElement(By.xpath("//*[@id=\"firstName\"]"));
-        WebElement lastName=driver.findElement(By.cssSelector("#lastName"));
-        WebElement email=driver.findElement(By.cssSelector("#userEmail"));
-        WebElement gender=driver.findElement(By.xpath("//*[@id=\"genterWrapper\"]/div[2]/div[1]/label"));
-        WebElement number=driver.findElement(By.xpath("//*[@id=\"userNumber\"]"));
+        getDriver().get("https://demoqa.com/automation-practice-form");
+
+        WebElement firstName = getDriver().findElement(By.xpath("//*[@id=\"firstName\"]"));
+        WebElement lastName = getDriver().findElement(By.cssSelector("#lastName"));
+        WebElement email = getDriver().findElement(By.cssSelector("#userEmail"));
+        WebElement gender = getDriver().findElement(By.xpath("//*[@id=\"genterWrapper\"]/div[2]/div[1]/label"));
+        WebElement number = getDriver().findElement(By.xpath("//*[@id=\"userNumber\"]"));
 
         firstName.sendKeys("TestFirstName");
         lastName.sendKeys("TestLastName");
@@ -60,18 +51,16 @@ public class PetrovTest extends BaseTest {
 
         number.sendKeys("1111111111");
 
-        WebElement submitButton=driver.findElement(By.xpath("//*[@id=\"submit\"]"));
+        WebElement submitButton = getDriver().findElement(By.xpath("//*[@id=\"submit\"]"));
         submitButton.click();
 
-        WebElement afterSubmittingForm=driver.findElement(By.cssSelector("#example-modal-sizes-title-lg"));
-        String title=afterSubmittingForm.getText();
+        WebElement afterSubmittingForm = getDriver().findElement(By.cssSelector("#example-modal-sizes-title-lg"));
+        String title = afterSubmittingForm.getText();
 
         Assert.assertEquals(title, "Thanks for submitting the form");
 
-        WebElement genderAfterSubmitting=driver.findElement(By.cssSelector("body > div.fade.modal.show > div > div > div.modal-body > div > table > tbody > tr:nth-child(3) > td:nth-child(2)"));
+        WebElement genderAfterSubmitting = getDriver().findElement(By.cssSelector("body > div.fade.modal.show > div > div > div.modal-body > div > table > tbody > tr:nth-child(3) > td:nth-child(2)"));
         Assert.assertEquals(genderAfterSubmitting.getText(), "Male");
-        driver.quit();
     }
-
 
 }
