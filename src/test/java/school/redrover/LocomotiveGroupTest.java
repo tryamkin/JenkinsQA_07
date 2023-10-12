4600dc00171b5753c14edbc0b69dead503899842
Michael Rublev <m.cmsrus@gmail.com>
2023-10-12 18:31:01 +0200

    test with command Locomotive (#389)
    Добавлен групповой тест в команду Локомотив. 
    Сквош и мерж

--- src/test/java/school/redrover/LocomotiveGroupTest.java
+++ src/test/java/school/redrover/LocomotiveGroupTest.java
@@ -186,4 +186,16 @@ public class LocomotiveGroupTest extends BaseTest {
 
         driver.quit();
     }
+
+    @Test
+    public void testSkateSiteHeader() throws InterruptedException {
+        getDriver().get("http://www.skate.net/");
+
+        WebElement title = getDriver().findElement(By.className("header_text"));
+        String value = title.getText();
+        Assert.assertEquals(value, "skates, skate gear, ice skates and more");
+
+        Thread.sleep(2000);
+    }
+
 }
