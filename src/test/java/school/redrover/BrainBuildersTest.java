1b57f55dff33276f97acbff2c0e3be6a0330db10
realandreyantropov <ramchem1993@gmail.com>
2023-10-13 18:30:08 +0300

    adding item to cart test and changing class name for group

--- src/test/java/school/redrover/BrainBuildersTest.java
+++ src/test/java/school/redrover/BrainBuildersTest.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 import static org.testng.Assert.assertEquals;
 
 
-public class GroupBrainBuildersTest extends BaseTest {
+public class BrainBuildersTest extends BaseTest {
 
     @Ignore
     @Test
