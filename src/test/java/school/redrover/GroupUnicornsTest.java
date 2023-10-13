95027ba05a3684334595b2b021bf18480b3bf8d1
Bayan Zh <82990053+bzhumanova@users.noreply.github.com>
2023-10-12 21:02:56 -0400

    refactoring bayan's test in unicorn class (#422)

--- src/test/java/school/redrover/GroupUnicornsTest.java
+++ src/test/java/school/redrover/GroupUnicornsTest.java
@@ -63,37 +63,36 @@ public class GroupUnicornsTest extends BaseTest {
     }
 
     @Test
-    public void w3SchoolTest()
+    public void testW3School()
     {
-        WebDriver wd = getDriver();
-        wd.get("https://www.w3schools.com/");
+        getDriver().get("https://www.w3schools.com/");
 
         //title
-        String title = wd.getTitle();
+        String title = getDriver().getTitle();
         Assert.assertEquals(title, "W3Schools Online Web Tutorials");
 
         //H1 heading
-        WebElement h1Heading = wd.findElement(By.className("learntocodeh1"));
+        WebElement h1Heading = getDriver().findElement(By.className("learntocodeh1"));
         Assert.assertEquals(h1Heading.getText(), "Learn to Code");
 
         //H3 heading
-        WebElement h3Heading = wd.findElement(By.className("learntocodeh3"));
+        WebElement h3Heading = getDriver().findElement(By.className("learntocodeh3"));
         Assert.assertEquals(h3Heading.getText(), "With the world's largest web developer site.");
 
         //H4 heading
-        WebElement h4Heading = wd.findElement(By.className("learntocodeh4"));
+        WebElement h4Heading = getDriver().findElement(By.className("learntocodeh4"));
         Assert.assertEquals(h4Heading.getText(), "Not Sure Where To Begin?");
 
         //text box
-        WebElement textBox = wd.findElement(By.id("search2"));
+        WebElement textBox = getDriver().findElement(By.id("search2"));
 
         //search button
-        WebElement searchButton = wd.findElement(By.id("learntocode_searchbtn"));
+        WebElement searchButton = getDriver().findElement(By.id("learntocode_searchbtn"));
         textBox.sendKeys("java tutorial");
         searchButton.click();
 
         //title
-        title = wd.getTitle();
+        title = getDriver().getTitle();
         Assert.assertEquals(title, "Java Tutorial");
     }
 
