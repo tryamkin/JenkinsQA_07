d9cff7ba9e9302ede727abb9f8645bb17d287cc7
realandreyantropov <ramchem1993@gmail.com>
2023-10-13 18:13:44 +0300

    adding item to cart test and changing class name for group

--- src/test/java/school/redrover/GroupBrainBuildersTest.java
+++ src/test/java/school/redrover/GroupBrainBuildersTest.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 import static org.testng.Assert.assertEquals;
 
 
-public class BrainBuildersTest extends BaseTest {
+public class GroupBrainBuildersTest extends BaseTest {
 
     @Ignore
     @Test
@@ -137,5 +137,27 @@ public class BrainBuildersTest extends BaseTest {
         WebElement personalAccountTitle = getDriver().findElement(By.cssSelector("h1"));
         String resultTitle = personalAccountTitle.getText();
         Assert.assertEquals(resultTitle, "Личный кабинет");
+    }
+
+    @Test
+    public void testAskentAddToCart() throws InterruptedException {
+        getDriver().get("https://www.askent.ru/cat/bumazhniki/portmone_308/");
+
+        String title = getDriver().getTitle();
+        assertEquals(title, "МИНИ ПОРТМОНЕ MODULE , пол: Женский, цвет: breen, размер: 100х080х035. Купить в интернет-магазине ASKENT. Цена 4 490 руб.");
+
+        WebElement cookieButton = getDriver().findElement(By.xpath("//*[@id = 'cookie_accept']"));
+        cookieButton.click();
+
+        WebElement addToCartButton = getDriver().findElement(By.xpath("//*[@class = 'optionsBlock__add add-cart-statistics']"));
+        addToCartButton.click();
+
+        WebElement cartIcon = getDriver().findElement(By.xpath("//*[@class = 'cart_icon']"));
+        cartIcon.click();
+        Thread.sleep(2000);
+
+        WebElement itemName = getDriver().findElement(By.xpath("//*[@href = '/cat/bumazhniki/portmone_308/']"));
+        String resultName = itemName.getText();
+        Assert.assertEquals(resultName, "МИНИ ПОРТМОНЕ MODULE");
     }
 }
