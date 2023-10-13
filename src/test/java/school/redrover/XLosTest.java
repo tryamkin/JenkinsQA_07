4152c0eec33be041d1028e76fc6a811337e91561
Xeniya Loskutova <100655741+xloskutova@users.noreply.github.com>
2023-10-13 13:40:19 -0500

    Test refactoring according to new project changes (#449)

--- src/test/java/school/redrover/XLosTest.java
+++ src/test/java/school/redrover/XLosTest.java
@@ -1,28 +1,22 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
 
-import java.time.Duration;
 
-@Ignore
-public class XLosTest {
+public class XLosTest extends BaseTest {
 
     @Test
     public void SauceDemoPositiveLoginTest(){
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
 
         try {
-            driver.get("https://www.saucedemo.com/");
-            WebElement username = driver.findElement(By.id("user-name"));
-            WebElement password = driver.findElement(By.id("password"));
-            WebElement loginBtn = driver.findElement(By.id("login-button"));
+            getDriver().get("https://www.saucedemo.com/");
+            WebElement username = getDriver().findElement(By.id("user-name"));
+            WebElement password = getDriver().findElement(By.id("password"));
+            WebElement loginBtn = getDriver().findElement(By.id("login-button"));
 
             username.sendKeys("standard_user");
             password.sendKeys("secret_sauce");
@@ -31,13 +25,11 @@ public class XLosTest {
             Thread.sleep(2000);
 
             String expectedTitle = "Swag Labs";
-            String actualTitle = driver.getTitle();
+            String actualTitle = getDriver().getTitle();
 
             Assert.assertEquals(actualTitle, expectedTitle, "Login failed.");
-        }catch (InterruptedException e){
+        }catch (InterruptedException e) {
             e.printStackTrace();
-        }finally {
-            driver.quit();
         }
     }
 }
