61f64a9e61c5c6a786f876c81bc96f7f8a2f17fe
natbabich <natbabich2804@gmail.com>
2023-10-13 11:54:11 -0500

    Remove unused import

--- src/test/java/school/redrover/GroupForwardTest.java
+++ src/test/java/school/redrover/GroupForwardTest.java
@@ -1,6 +1,5 @@
 package school.redrover;
 
-import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
