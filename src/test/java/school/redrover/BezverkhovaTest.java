8c10d57bbc1c0a5ae5ca16ee8103280a4d444b70
Bezverkhova <60390586+Bezverkhova@users.noreply.github.com>
2023-10-14 13:17:55 -0500

    Be luma test extend (#478)
    * Add extension to BaseTest
    
    * Add Jenkins test
    
    * Refactoring
    
    * Delete getDriver().quit()
    
    * Delete ignore tag
    
    * Add wait to fix failed tests in CI. Refactoring

--- src/test/java/school/redrover/BezverkhovaTest.java
+++ src/test/java/school/redrover/BezverkhovaTest.java
@@ -1,18 +1,24 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Ignore;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+import school.redrover.runner.JenkinsUtils;
 
 import java.time.Duration;
 
 import static org.testng.Assert.assertEquals;
 
-@Ignore
-public class BezverkhovaTest {
+public class BezverkhovaTest extends BaseTest {
+    public static final String EMAIL = "test_redrov@yahoo.com";
+    public static final String PASS = "te5t_redr0v";
+    public static final String URL = "https://magento.softwaretestingboard.com/";
+    public static final By WELCOME_MESSAGE_LOCATOR = By.xpath("//div[@class='panel header']//li[@class='greet welcome']/span");
+    public static final By FORGET_PASS_LOCATOR = By.cssSelector("#login-form > fieldset > div.actions-toolbar > div.secondary > a > span");
+    public static final By RESET_PASS_MESSAGE_LOCATOR = By.cssSelector("#maincontent > div.page.messages > div:nth-child(2) > div > div");
     public static final String ANYITEM = "#maincontent > div.columns > div.column.main > div.products.wrapper.grid.products-grid > ol > li:nth-child(1) > div > div > strong > a";
     public static final String SIZE = "#option-label-size-143-item-168";
     public static final String COLOR = "option-label-color-93-item-56";
@@ -21,77 +27,67 @@ public class BezverkhovaTest {
     public static final String DISCOUNT = "#cart-totals > div > table > tbody > tr:nth-child(2) > td > span > span";
     public static final String PRICE = "#shopping-cart-table > tbody > tr.item-info > td.col.price > span > span > span";
     public static final String DISCOUNT_TITLE = "#cart-totals > div > table > tbody > tr:nth-child(2) > th > span.title";
-    public static final String EMAIL = "test_redrov@yahoo.com";
-    public static final String PASS = "te5t_redr0v";
-    public static final String URL = "https://magento.softwaretestingboard.com/";
 
     @Test
-    public void testLogIn() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-        driver.get(URL);
+    public void testLogIn() {
+        getDriver().get(URL);
 
-        WebElement myaccount = driver.findElement(By.className("authorization-link"));
-        myaccount.click();
-        WebElement enterEmail = driver.findElement(By.name("login[username]"));
-        enterEmail.sendKeys(EMAIL);
-        WebElement enterPass = driver.findElement(By.name("login[password]"));
-        enterPass.sendKeys(PASS);
-        WebElement submitButton = driver.findElement(By.cssSelector("#send2 > span"));
-        submitButton.click();
-        Thread.sleep(2000);
-        String greeting = driver.findElement(By.cssSelector("body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.greet.welcome > span")).getText();
-        assertEquals(greeting, "Welcome, Test Redrov!");
-        driver.quit();
+        getDriver().findElement(By.className("authorization-link")).click();
+        getDriver().findElement(By.name("login[username]")).sendKeys(EMAIL);
+        getDriver().findElement(By.name("login[password]")).sendKeys(PASS);
+        getDriver().findElement(By.cssSelector("#send2 > span")).click();
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        String expectedWelcomeMessage = "Welcome, Test Redrov!";
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(WELCOME_MESSAGE_LOCATOR, expectedWelcomeMessage));
+        String actualWelcomeMessage = getDriver().findElement(WELCOME_MESSAGE_LOCATOR).getText();
+        assertEquals(actualWelcomeMessage, expectedWelcomeMessage);
     }
 
     @Test
-    public void forgetPass() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-        driver.get(URL);
-        WebElement myAccount = driver.findElement(By.className("authorization-link"));
-        myAccount.click();
-        WebElement forgetPass = driver.findElement(By.cssSelector("#login-form > fieldset > div.actions-toolbar > div.secondary > a > span"));
-        forgetPass.click();
-        WebElement enterEmail = driver.findElement(By.name("email"));
-        enterEmail.sendKeys(EMAIL);
-        WebElement submitButton = driver.findElement(By.cssSelector("#form-validate > div > div.primary > button > span"));
-        submitButton.click();
-        Thread.sleep(2000);
-        String greeting = driver.findElement(By.cssSelector("#maincontent > div.page.messages > div:nth-child(2) > div > div")).getText();
-        assertEquals(greeting, "If there is an account associated with test_redrov@yahoo.com you " +
-                "will receive an email with a link to reset your password.");
-        driver.quit();
+    public void testForgetPass() {
+        getDriver().get(URL);
+        getDriver().findElement(By.className("authorization-link")).click();
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(FORGET_PASS_LOCATOR)).click();
+        getDriver().findElement(By.name("email")).sendKeys(EMAIL);
+        getDriver().findElement(By.cssSelector("#form-validate > div > div.primary > button > span")).click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("message-success")));
+        String actualMessage = getDriver().findElement(RESET_PASS_MESSAGE_LOCATOR).getText();
+        String expectedMessage = String.format("If there is an account associated with %1$s you will receive an email with a link to reset your password.", EMAIL );
+        assertEquals(actualMessage, expectedMessage);
     }
 
     @Test
-    public void check4TeesForPriceOf3() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.get(URL);
+    public void testCheck4TeesForPriceOf3() {
+        getDriver().get(URL);
 
-        WebElement saleBanner = driver.findElement(By.cssSelector(SALE4FOR3));
+        WebElement saleBanner = getDriver().findElement(By.cssSelector(SALE4FOR3));
         if (saleBanner.isDisplayed()) {
             saleBanner.click();
-            WebElement sellingItem = driver.findElement(By.cssSelector(ANYITEM));
+            WebElement sellingItem = getDriver().findElement(By.cssSelector(ANYITEM));
             String sellingItemName = sellingItem.getText();
             sellingItem.click();
 
-            driver.findElement(By.cssSelector(SIZE)).click();
-            driver.findElement(By.id(COLOR)).click();
-            WebElement qty = driver.findElement(By.name("qty"));
+            getDriver().findElement(By.cssSelector(SIZE)).click();
+            getDriver().findElement(By.id(COLOR)).click();
+            WebElement qty = getDriver().findElement(By.name("qty"));
             qty.clear();
             qty.sendKeys("4");
-            driver.findElement(By.id("product-addtocart-button")).click();
+            getDriver().findElement(By.id("product-addtocart-button")).click();
 
-            String greeting = driver.findElement(By.cssSelector(ADDED_MESSAGE)).getText();
-            assertEquals(greeting, "You added " + sellingItemName + " to your shopping cart.");
-            driver.findElement(By.cssSelector(ADDED_MESSAGE + " > a")).click();
+            WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.className("message-success")));
+            String actualAddedMessage = getDriver().findElement(By.cssSelector(ADDED_MESSAGE)).getText();
+            String expectedAddedMessage = "You added " + sellingItemName + " to your shopping cart.";
+            assertEquals(actualAddedMessage, expectedAddedMessage);
 
-            if (driver.findElement(By.cssSelector(DISCOUNT_TITLE)).isDisplayed()) {
-                String discount = driver.findElement(By.cssSelector(DISCOUNT)).getText();
-                String price = driver.findElement(By.cssSelector(PRICE)).getText();
+            getDriver().findElement(By.cssSelector(ADDED_MESSAGE + " > a")).click();
+            if (getDriver().findElement(By.cssSelector(DISCOUNT_TITLE)).isDisplayed()) {
+                String discount = getDriver().findElement(By.cssSelector(DISCOUNT)).getText();
+                String price = getDriver().findElement(By.cssSelector(PRICE)).getText();
                 assertEquals(discount, "-" + price, "Sale 4for3 doesn't works");
             } else {
                 System.out.println("Sale 4for3 doesn't work");
@@ -99,6 +95,15 @@ public class BezverkhovaTest {
         } else {
             System.out.println("Sale finished");
         }
-        driver.quit();
+    }
+
+    @Test
+    public void testJenkinsStatus(){
+        JenkinsUtils.login(getDriver());
+        getDriver().findElement(By.className("model-link")).click();
+        String iconText = getDriver().findElement(By.xpath("//*[@id='main-panel']/h1")).getText();
+        assertEquals(iconText, "admin");
+        String statusIconText = getDriver().findElement(By.cssSelector("#main-panel > div:nth-child(4)")).getText();
+        assertEquals(statusIconText, "Jenkins User ID: admin");
     }
 }
