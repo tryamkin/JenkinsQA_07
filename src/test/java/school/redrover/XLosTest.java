da36badd4bac92f73ee4424f33f7290efd579126
Xeniya Loskutova <100655741+xloskutova@users.noreply.github.com>
2023-10-14 21:55:56 -0500

    Adding login with wrong password test, without changing the class name due to multiple failed tests in ci (#512)

--- src/test/java/school/redrover/XLosTest.java
+++ src/test/java/school/redrover/XLosTest.java
@@ -9,11 +9,14 @@ import school.redrover.runner.BaseTest;
 
 public class XLosTest extends BaseTest {
 
+    // class name were not changed due to multiple failed tests in ci
+    private static final String BASE_URL = "https://www.saucedemo.com/";
+
     @Test
-    public void SauceDemoPositiveLoginTest(){
+    public void testSauceDemoPositiveLogin(){
 
         try {
-            getDriver().get("https://www.saucedemo.com/");
+            getDriver().get(BASE_URL);
             WebElement username = getDriver().findElement(By.id("user-name"));
             WebElement password = getDriver().findElement(By.id("password"));
             WebElement loginBtn = getDriver().findElement(By.id("login-button"));
@@ -22,12 +25,35 @@ public class XLosTest extends BaseTest {
             password.sendKeys("secret_sauce");
             loginBtn.click();
 
-            Thread.sleep(2000);
+            Thread.sleep(500);
 
             String expectedTitle = "Swag Labs";
             String actualTitle = getDriver().getTitle();
 
             Assert.assertEquals(actualTitle, expectedTitle, "Login failed.");
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testLoginWithWrongPassword(){
+        try {
+            getDriver().get(BASE_URL);
+            WebElement username = getDriver().findElement(By.id("user-name"));
+            WebElement password = getDriver().findElement(By.id("password"));
+            WebElement loginBtn = getDriver().findElement(By.id("login-button"));
+
+            username.sendKeys("standard_user");
+            password.sendKeys("test");
+            loginBtn.click();
+
+            Thread.sleep(500);
+
+            String actualErrorText = getDriver().findElement(By.tagName("h3")).getText();
+            String expectedErrorText = "Epic sadface: Username and password do not match any user in this service";
+
+            Assert.assertEquals(actualErrorText, expectedErrorText, "Error message didn't match.");
         }catch (InterruptedException e) {
             e.printStackTrace();
         }
