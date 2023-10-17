8fda6b510ea74c4bd77f407c1b71032e96f038b9
Andrei <kostuchenko-and@mail.ru>
2023-10-17 17:06:44 -0600

    added @Ignore

--- src/test/java/school/redrover/GroupSevenTest.java
+++ src/test/java/school/redrover/GroupSevenTest.java
@@ -265,7 +265,7 @@ public class GroupSevenTest extends BaseTest {
         }
     }
 
-
+    @Ignore
     @Test
     public void testBooksSearch() {
         getDriver().get("https://www.doylestownbookshop.com/");
