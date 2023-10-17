01e30a3514cd749cf4293dd4802db0e9643699e9
PavTomak <tomak.pav@gmail.com>
2023-10-17 09:04:29 -0400

    revert BaseTest

--- src/test/java/school/redrover/runner/BaseTest.java
+++ src/test/java/school/redrover/runner/BaseTest.java
@@ -1,11 +1,11 @@
 package school.redrover.runner;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
+import org.testng.annotations.*;
 
 import java.lang.reflect.Method;
 import java.time.Duration;
