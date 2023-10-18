74a39c980729cd6de57b5eb8126717d3cde690fb
Svetlana Emelyanova <114905767+chuda4ka@users.noreply.github.com>
2023-10-18 19:13:54 +0300

    add testCreateNewPipelineProject (#731)

--- src/test/java/school/redrover/GroupTestscriptCollaboratoriumTest.java
+++ src/test/java/school/redrover/GroupTestscriptCollaboratoriumTest.java
@@ -11,7 +11,6 @@ import school.redrover.runner.BaseTest;
 import school.redrover.runner.JenkinsUtils;
 
 public class GroupTestscriptCollaboratoriumTest extends BaseTest {
-
     @Test
     public void testVersion() {
 
@@ -21,6 +20,20 @@ public class GroupTestscriptCollaboratoriumTest extends BaseTest {
         WebElement version = getDriver().findElement(By.xpath("//p[@class = 'app-about-version']"));
 
         Assert.assertEquals(version.getText(), "Version 2.414.2");
+    }
+
+    @Test
+    public void testCreateNewPipelineProject() {
+
+        final String projectName = "TestNew";
+
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(projectName);
+        getDriver().findElement(By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='main-panel']/h1")).getText(),
+                String.format("Pipeline %s", projectName));
     }
 
 }
