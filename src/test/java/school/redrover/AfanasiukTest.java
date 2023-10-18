2ee2ca071b0d6aade4748e73cade6a024e6184fd
Halyna A <7108581@gmail.com>
2023-10-18 08:04:07 -0700

    Refactor

--- src/test/java/school/redrover/AfanasiukTest.java
+++ src/test/java/school/redrover/AfanasiukTest.java
@@ -3,42 +3,45 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.JenkinsUtils;
 
 
 public class AfanasiukTest extends BaseTest {
-@Ignore
+
     @Test
-    public void testSearch() throws InterruptedException {
+    public void testCreateFreeStyleProject()  {
 
-        Assert.assertEquals(
-                getDriver().findElement(By.cssSelector(".empty-state-block > h1")).getText(),
-                "Welcome to Jenkins!");
-
-
-        WebElement createJob = getDriver().findElement(By.xpath("//a [@href='newJob']"));
-        createJob.click();
+        getDriver().findElement(By.xpath("//a [@href='newJob']")).click();
 
         WebElement nameInput = getDriver().findElement(By.xpath("//input [@name='name']"));
         nameInput.click();
-        nameInput.sendKeys("New Job");
+        nameInput.sendKeys("Freestyle Project");
 
-        WebElement itemType = getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']"));
-        itemType.click();
-
-        WebElement okButton = getDriver().findElement(By.xpath("//button[@id='ok-button']"));
-        okButton.click();
-
-        WebElement saveButton = getDriver().findElement(By.xpath("//button[@name='Submit']"));
-        saveButton.click();
+        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
         Assert.assertEquals(
                 getDriver().findElement(By.xpath("//*[@id='main-panel']/h1")).getText(),
-                "Project New Job");
-
+            "Project Freestyle Project");
         }
-}
+    @Test
+        public void testCreatePipeline()  {
+
+        getDriver().findElement(By.xpath("//*[@id='tasks']/div[1]/span/a/span[1]")).click();
+
+        WebElement nameInput = getDriver().findElement(By.xpath("//input [@name='name']"));
+        nameInput.click();
+        nameInput.sendKeys("Pipeline");
+
+        getDriver().findElement(By.xpath("//*[@id='j-add-item-type-standalone-projects']")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//*[@id='main-panel']/h1")).getText(),
+                "Pipeline Pipeline");
+    }
+   }
 
