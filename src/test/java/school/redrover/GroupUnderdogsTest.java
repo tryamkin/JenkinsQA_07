d882638a2daec216645b6a0b8d7cbe5fbcdeeed2
Artuom Kudryashov <90002964+ArtuomKudryashov@users.noreply.github.com>
2023-10-18 13:44:41 -0400

    GroupUnderdogsTest_JenkinsTest (#739)

--- src/test/java/school/redrover/GroupUnderdogsTest.java
+++ src/test/java/school/redrover/GroupUnderdogsTest.java
@@ -21,11 +21,7 @@ public class GroupUnderdogsTest extends BaseTest {
     private static final String MAIN_PAGE_URL_99BOTTLES = "http://www.99-bottles-of-beer.net/";
     private static final String ABC_PAGE_URL_99BOTTLES = "http://www.99-bottles-of-beer.net/abc.html";
     private static final String SUBMIT_PAGE_URL_99BOTTLES = "http://www.99-bottles-of-beer.net/submitnewlanguage.html";
-    private static final String userName = "academic198405@gmail.com";
-    private static final String password = "BikeTrekMarlyn4!";
-    private static final String wrongPassword = "Sbbhbhbln2";
-    private static final String MAIN_PAGE_URL_TREKBIKES = "https://www.trekbikes.com/us/en_US/";
-    private static final String MAIN_PAGE_URL_MAYTAG = "https://www.maytag.ca/";
+
 
     @Test
     public void  test99BottlesTitleTest_tereshenkov29() {
@@ -106,110 +102,7 @@ public class GroupUnderdogsTest extends BaseTest {
                 "Sign in to Jenkins");
     }
 
-    @Ignore
-    @Test
-    public void testTrack_correct_Credential_Artuom() throws InterruptedException {
-        Dimension windowSize = new Dimension(1024, 768);
-        getDriver().manage().window().setSize(windowSize);
-        getDriver().get(MAIN_PAGE_URL_TREKBIKES);
-        WebElement enterButton = getDriver().findElement(By.xpath("(//*[@class='pdl-icon pdl-icon--size-24'])[1]"));
-        Thread.sleep(2000);
-        enterButton.click();
-        Thread.sleep(3000);
 
-        JavascriptExecutor js = (JavascriptExecutor) getDriver();
-        WebElement login = getDriver().findElement(By.xpath("//*[@class='mr-1 material-icons md-24']"));
-        js.executeScript("arguments[0].scrollIntoView();", login);
-        login.click();
-
-        WebElement emailField = getDriver().findElement(By.xpath("//*[@id=\"j_username\"]"));
-        emailField.click();
-        emailField.sendKeys(userName);
-        Thread.sleep(2000);
-
-        WebElement passwordField = getDriver().findElement(By.xpath("//*[@id=\"j_password\"]"));
-        passwordField.click();
-        passwordField.sendKeys(password);
-        Thread.sleep(1000);
-
-        WebElement button = getDriver().findElement(By.xpath("(//*[text()='Log in'])[3]"));
-        button.click();
-        Thread.sleep(1000);
-
-        WebElement isAllNewArrivals = getDriver().findElement(By.xpath("(//*[@class=\"pdl-heading pdl-heading--xl \"])[1]"));
-
-        String text = isAllNewArrivals.getText();
-        assertEquals(text, "All-new arrivals");
-    }
-
-    @Ignore
-    @Test
-    public void testTrack_Incorrect_Credential_Artuom() throws InterruptedException {
-        Dimension windowSize = new Dimension(1024, 768);
-        getDriver().manage().window().setSize(windowSize);
-        getDriver().get(MAIN_PAGE_URL_TREKBIKES);
-        WebElement enterButton = getDriver().findElement(By.xpath("(//*[@class='pdl-icon pdl-icon--size-24'])[1]"));
-        Thread.sleep(2000);
-        enterButton.click();
-        Thread.sleep(3000);
-
-        JavascriptExecutor js = (JavascriptExecutor)getDriver();
-        WebElement login = getDriver().findElement(By.xpath("//*[@class='mr-1 material-icons md-24']"));
-        js.executeScript("arguments[0].scrollIntoView();", login);
-        login.click();
-
-        WebElement emailField = getDriver().findElement(By.xpath("//*[@id=\"j_username\"]"));
-        emailField.click();
-        emailField.sendKeys(userName);
-        Thread.sleep(2000);
-
-        WebElement passwordField = getDriver().findElement(By.xpath("//*[@id=\"j_password\"]"));
-        passwordField.click();
-        passwordField.sendKeys(wrongPassword);
-
-        WebElement button = getDriver().findElement(By.xpath("(//*[text()='Log in'])[3]"));
-        button.click();
-        Thread.sleep(1000);
-
-        WebElement incorrectUser = getDriver().findElement(By.xpath("//*[text()='Incorrect username or password']"));
-
-        String text = incorrectUser.getText();
-        assertEquals(text, "Incorrect username or password");
-    }
-
-    @Ignore
-    @Test
-    public void testMarlin4_Artuom() throws InterruptedException {
-        Dimension windowSize = new Dimension(1024, 768);
-        getDriver().manage().window().setSize(windowSize);
-        getDriver().get(MAIN_PAGE_URL_TREKBIKES);
-        WebElement enterButton = getDriver().findElement(By.xpath("(//*[@class='pdl-icon pdl-icon--size-24'])[1]"));
-        Thread.sleep(2000);
-        enterButton.click();
-        Thread.sleep(3000);
-
-        WebElement mount = getDriver().findElement(By.xpath("//*[@id=\"expandMountainBikesMainMenu-compact\"]"));
-        mount.click();
-        Thread.sleep(1000);
-
-        JavascriptExecutor js = (JavascriptExecutor) getDriver();
-        WebElement marlin = getDriver().findElement(By.xpath("(//*[text()='Marlin'])[1]"));
-        js.executeScript("arguments[0].scrollIntoView();", marlin);
-        marlin.click();
-        Thread.sleep(1000);
-
-        WebElement seeTheBikes = getDriver().findElement(By.xpath("//*[@title=\"SEE THE BIKES\"]"));
-        seeTheBikes.click();
-        Thread.sleep(2000);
-
-        JavascriptExecutor js1 = (JavascriptExecutor) getDriver();
-        WebElement marlin4 = getDriver().findElement(By.xpath("//*[text()='Marlin 4 Gen 2']"));
-        js.executeScript("arguments[0].scrollIntoView();", marlin4);
-        Thread.sleep(2000);
-
-        String bikeName = marlin4.getText();
-        assertEquals(bikeName, "Marlin 4 Gen 2");
-    }
 
     public void closePromoDrawerIfVisible() {
         WebElement promoDrawer = getDriver().findElement(By.className("promo-drawer__drawer"));
@@ -222,100 +115,21 @@ public class GroupUnderdogsTest extends BaseTest {
             }
         }
     }
-
-    @Ignore
     @Test
-    public void testEnd_to_End_Artuom() throws InterruptedException {
-        getDriver().get(MAIN_PAGE_URL_MAYTAG);
-        closePromoDrawerIfVisible();
+    public void  testIdAdminArtuom() throws InterruptedException {
+        getDriver().findElement(By.xpath("//a[@href='/user/admin']")).click();
+        getDriver().findElement(By.xpath("(//span[@class='task-link-wrapper '])[4]")).click();
 
-        WebElement fRlocal = getDriver().findElement(By.xpath("(//*[@class='utility-nav__link'])[5]"));
-        fRlocal.click();
-        Thread.sleep(1000);
-
-        WebElement modWindOfferEnCours = getDriver().findElement(By.xpath("//*[@class='promo-drawer__heading']"));
-        modWindOfferEnCours.click();
-
-        WebElement addresseZipCode = getDriver().findElement(By.xpath("//*[@class='location-data']"));
-        addresseZipCode.click();
-        Thread.sleep(1000);
-
-        WebElement fieldSearch = getDriver().findElement(By.xpath("//input[@placeholder='Tout rechercher']"));
-        fieldSearch.clear();
-        fieldSearch.sendKeys("FILTRE À EAU");
-        fieldSearch.sendKeys(Keys.ENTER);
-
-        WebElement sortPricePertinceLtoH = getDriver().findElement(By.xpath("//*[@aria-label='sort by']/option[2]"));
-        sortPricePertinceLtoH.click();
-        Thread.sleep(1000);
-
-        WebElement FILTRE_À_AIR_FRESHFLOWTM_AIR1_POUR_RÉFRIGÉRATEUR = getDriver().findElement(By.xpath("(//*[@href='/fr_ca/accessories/kitchen-accessories/refrigerator/p.freshflow-refrigerator-air-filter-air1.w10311524.html?originVariantsOrder=NC'])[1]"));
-        JavascriptExecutor js = (JavascriptExecutor) getDriver();
-        int yOffset = FILTRE_À_AIR_FRESHFLOWTM_AIR1_POUR_RÉFRIGÉRATEUR.getLocation().getY();
-        for (int i = 0; i < yOffset; i += 20) {
-            js.executeScript("window.scrollTo(0, " + i + ")");
-            Thread.sleep(50);
-        }
-        FILTRE_À_AIR_FRESHFLOWTM_AIR1_POUR_RÉFRIGÉRATEUR.click();
-        Thread.sleep(1000);
-
-        JavascriptExecutor js1 = (JavascriptExecutor) getDriver();
-        WebElement Disponibilte_Available = getDriver().findElement(By.xpath("(//*[@class='button checking-availability-btn trigger-modal'])[2]"));
-        js1.executeScript("arguments[0].scrollIntoView();", Disponibilte_Available);
-        Thread.sleep(1000);
-        Disponibilte_Available.click();
-
-        WebElement zipCode = getDriver().findElement(By.xpath("//*[@class='signin-account-field form-input mm-zipcode-location-v2']"));
-        zipCode.click();
-
-        WebElement fieldZip = getDriver().findElement(By.xpath("//*[@placeholder='Tapez le code postal ici...']"));
-        fieldZip.click();
-        fieldZip.sendKeys("A1A 1A1");
-
-        WebElement submit = getDriver().findElement(By.xpath("//*[@id='update-location-btn']"));
-        submit.click();
-        Thread.sleep(2000);
-
-        JavascriptExecutor js2 = (JavascriptExecutor) getDriver();
-        WebElement AJOUTER_AU_PANIER_AddToBin = getDriver().findElement(By.xpath("(//span[@class='button__text'])[2]"));
-        js2.executeScript("arguments[0].scrollIntoView();", AJOUTER_AU_PANIER_AddToBin);
-
-        Thread.sleep(1000);
-        AJOUTER_AU_PANIER_AddToBin.click();
-
-        Thread.sleep(1000);
-        WebElement passer_Au_Panier = getDriver().findElement(By.xpath("//span[@data-backdrop='static']"));
-        passer_Au_Panier.click();
-        Thread.sleep(1000);
-
-        WebElement totalEst = getDriver().findElement(By.xpath("(//*[@class='row--value '])[4]"));
-        String actRes = totalEst.getText();
-        String expRes = "15,56 $";
-
-        assertEquals(actRes, expRes);
-    }
-
-    @Test
-    public void  test_idAdmin_Artuom() throws InterruptedException {
-
-        WebElement nameOfUser = getDriver().findElement(By.xpath("//a[@href='/user/admin']"));
-        nameOfUser.click();
-        WebElement config = getDriver().findElement(By.xpath("(//span[@class='task-link-wrapper '])[4]"));
-        config.click();
         WebElement button = getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate']"));
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
-        int subm = button.getLocation().getY();
-        for (int i = 0; i < subm; i += 20) {
-            js.executeScript("window.scrollTo(0, " + i + ")");
-            Thread.sleep(50);
-        }
+        js.executeScript("arguments[0].scrollIntoView(true);", button);
         button.click();
 
         WebElement UserIdAdm = getDriver().findElement(By.xpath("//div[@id='description']/following-sibling::div"));
         String actNameOfUser = UserIdAdm.getText();
         String expRes = "Jenkins User ID: admin";
-
         assertEquals(actNameOfUser, expRes);
+
     }
 
     @Test
