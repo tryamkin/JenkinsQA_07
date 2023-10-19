dc3f3c847a46d3d3d669e15da9a8513b7e5c2ff4
Alena Iadkouskaya <117815613+LenKopt@users.noreply.github.com>
2023-10-19 13:01:57 +0200

    test Jenkins Add new Item (#775)
    Co-authored-by: iod.elena86 <koptewka@mail.ru>

--- src/test/java/school/redrover/GroupJavaAutomationTest.java
+++ src/test/java/school/redrover/GroupJavaAutomationTest.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.*;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -19,6 +20,38 @@ public class GroupJavaAutomationTest extends BaseTest {
         (By.xpath("//button[@type='button']"));
         String versionJenkins = versionJenkinsButton.getText();
         Assert.assertEquals(versionJenkins,"Jenkins 2.414.2");
+    }
+    @Test
+    public void testJenkinsAddNewItemAndSearch(){
+        WebElement buttonNewItem = getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"));
+        buttonNewItem.click();
+
+        String nameProject = "Project_iod";
+        WebElement inputFieldName = getDriver().findElement(By.id("name"));
+        inputFieldName.sendKeys(nameProject);
+
+        WebElement freestyleProject = getDriver().findElement(By.xpath("//span[text()='Freestyle project']"));
+        freestyleProject.click();
+
+        WebElement buttonOk = getDriver().findElement(By.id("ok-button"));
+        buttonOk.click();
+
+        WebElement buttonSave = getDriver().findElement(By.xpath("//button[@name='Submit']"));
+        buttonSave.click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@id='skip2content']/following-sibling::*")).getText(),"Project " + nameProject);
+
+    }
+    @Ignore
+    @Test
+    public void testJenkinsSearchItem(){
+        testJenkinsAddNewItemAndSearch();
+
+        WebElement inputSearch = getDriver().findElement(By.xpath("//input[@id='search-box']"));
+        inputSearch.sendKeys("iod");
+        inputSearch.submit();
+
+
     }
 }
 
