4995b75b147a6c924749581a83ee272f3f80593b
Maria Dymsha <103815224+dymshamaria@users.noreply.github.com>
2023-10-19 15:22:30 +0300

    addTestsForJenkins (#779)

--- src/test/java/school/redrover/MariaPracticeTest.java
+++ src/test/java/school/redrover/MariaPracticeTest.java
@@ -1,79 +1,84 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.JenkinsUtils;
 
-
-@Ignore
 public class MariaPracticeTest extends BaseTest {
 
-    public static final String PAGESELENIUM = "https://www.selenium.dev/selenium/web/web-form.html";
-
     @Test
-    public void SearchTest() {
-
-        getDriver().get(PAGESELENIUM);
-        String title = getDriver().getTitle();
-
-        WebElement textBox = getDriver().findElement(By.name("my-text"));
-        textBox.sendKeys("Selenium");
-
-        WebElement submitButton = getDriver().findElement(By.cssSelector("button"));
-        submitButton.click();
-
-        WebElement message = getDriver().findElement(By.id("message"));
-
-        Assert.assertEquals("Web form", title);
-        Assert.assertEquals("Received!", message.getText());
-    }
-
-    @Test
-    public void createUserTest() {
-
-        getDriver().get(PAGESELENIUM);
-
-        WebElement textInput = getDriver().findElement(By.xpath("//input[@name='my-text']"));
-        textInput.sendKeys("Maria Dymsha");
-
-        WebElement password = getDriver().findElement(By.xpath("//input[@name='my-password']"));
-        password.sendKeys("12345");
-
-        WebElement textarea = getDriver().findElement(By.xpath("//textarea[@name='my-textarea']"));
-        textarea.sendKeys("Good morning");
-
-        WebElement clickSubmit = getDriver().findElement(By.xpath("//button[@class='btn btn-outline-primary mt-3']"));
-        clickSubmit.click();
-
-        WebElement textFormSubmitted = getDriver().findElement(By.xpath("//h1[@class='display-6']"));
-
-        Assert.assertEquals(textFormSubmitted.getText(), "Form submitted");
-    }
-    @Ignore
-    @Test
-    public void linkReturnToIndexTest() {
-
-        getDriver().get(PAGESELENIUM);
-
-        WebElement clickSubmit = getDriver().findElement(By.xpath("//a[@href='./index.html']"));
-        clickSubmit.click();
-
-        WebElement clickDisabledElement = getDriver().findElement(By.xpath("//a[@href='click_tests/disabled_element.html']"));
-        clickDisabledElement.click();
-
-        WebElement textSeeBelow = getDriver().findElement(By.xpath("//*[text()='See below']"));
-
-        Assert.assertEquals(textSeeBelow.getText(), "See below");
-    }
-
-    @Test
-    public void WelcomeToJenkinsTest() {
+    public void testWelcomeToJenkins() {
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//a[text()='Dashboard']")).getText(),
                 "Dashboard");
+    }
+
+    @Test
+    public void testCreateProjectQA() {
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input']")).click();
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input']")).sendKeys("QA");
+        getDriver().findElement(By.xpath("//span[text()='Freestyle project']")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+        getDriver().findElement(By.xpath("//label[@class='attach-previous ']")).click();
+        getDriver().findElement(By.xpath("//textarea[@name='description']")).click();
+        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys("About Java languages");
+        getDriver().findElement(By.xpath("//*[@class='jenkins-button jenkins-button--primary ']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Project QA']")).getText(), "Project QA");
+    }
+
+    @Test
+    public void testJenkinsVersion() {
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@class='jenkins-button jenkins-button--tertiary jenkins_ver']"))
+                .getText(), "Jenkins 2.414.2");
+    }
+
+    @Test
+    public void testCheckTitle() {
+
+        getDriver().findElement(By.xpath("//a[@href='/manage']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='/manage']"))
+                .getText(), "Manage Jenkins");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='System Configuration']"))
+                .getText(), "System Configuration");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='System']"))
+                .getText(), "System");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Tools']"))
+                .getText(), "Tools");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Plugins']"))
+                .getText(), "Plugins");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Clouds']"))
+                .getText(), "Clouds");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Security']"))
+                .getText(), "Security");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Users']"))
+                .getText(), "Users");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Status Information']"))
+                .getText(), "Status Information");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='System Information']"))
+                .getText(), "System Information");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Load Statistics']"))
+                .getText(), "Load Statistics");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='About Jenkins']"))
+                .getText(), "About Jenkins");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Troubleshooting']"))
+                .getText(), "Troubleshooting");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Manage Old Data']"))
+                .getText(), "Manage Old Data");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Tools and Actions']"))
+                .getText(), "Tools and Actions");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Reload Configuration from Disk']"))
+                .getText(), "Reload Configuration from Disk");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Jenkins CLI']"))
+                .getText(), "Jenkins CLI");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Script Console']"))
+                .getText(), "Script Console");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[text()='Prepare for Shutdown']"))
+                .getText(), "Prepare for Shutdown");
     }
 }
