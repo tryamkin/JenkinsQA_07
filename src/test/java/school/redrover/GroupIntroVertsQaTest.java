11eb914c5a23fff02d7465bc7871d2949ad2f4f3
AkiMira <alfa88@inbox.ru>
2023-10-20 16:14:17 +0300

    AkiMira New Item Freestyle Project Positive Test (#819)

--- src/test/java/school/redrover/GroupIntroVertsQaTest.java
+++ src/test/java/school/redrover/GroupIntroVertsQaTest.java
@@ -20,15 +20,36 @@ public class GroupIntroVertsQaTest extends BaseTest {
     // region AkiMiraTest
 
     @Test (description = "Test of Jenkins Search field")
-    public void testJenkinsSearchField () {
+    public void testJenkinsSearchFieldPositiveAkiMira() {
 
         WebElement searchField = getDriver().findElement(By.id("search-box"));
         searchField.sendKeys("admin");
         searchField.sendKeys(Keys.ENTER);
+
         WebElement searchResults = getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/div[2]"));
         Assert.assertTrue(searchResults.isDisplayed());
         String value = searchResults.getText();
         Assert.assertTrue(value.contains("admin"));
+    }
+
+    @Test (description = "New Freestyle project is created using valid name")
+    public void testNewFreestyleProjectPositiveAkiMira() {
+
+        WebElement newItem = getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/span/a"));
+        newItem.click();
+
+        WebElement itemNameField = getDriver().findElement(By.xpath("//*[@id=\"name\"]"));
+        itemNameField.sendKeys("Test");
+
+        WebElement freestyleProjectField = getDriver().findElement(By.xpath("//*[@id=\"j-add-item-type-standalone-projects\"]/ul/li[1]"));
+        freestyleProjectField.click();
+
+        WebElement submitButton = getDriver().findElement(By.xpath("//*[@id=\"ok-button\"]"));
+        submitButton.click();
+
+        String title = getDriver().getTitle();
+        Assert.assertEquals(title, "Test Config [Jenkins]");
+
     }
     // endregion
 
