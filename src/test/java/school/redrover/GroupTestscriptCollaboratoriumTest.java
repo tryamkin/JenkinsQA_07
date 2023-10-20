5721ef649654763135c0d443c6976d67114a63b3
Yahor Labanau <yahorlabanau@gmail.com>
2023-10-20 09:16:56 +0300

    231018 add 4 Jenkins test + private method (#770)
    added my 4 Jenkins test
    and a utilsCreateFreestyleProject() private method to create a new Project/Job/Item easier.
    
    Now we can re-use this private method, so that we don't write redundant code

--- src/test/java/school/redrover/GroupTestscriptCollaboratoriumTest.java
+++ src/test/java/school/redrover/GroupTestscriptCollaboratoriumTest.java
@@ -1,16 +1,21 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.JenkinsUtils;
 
 public class GroupTestscriptCollaboratoriumTest extends BaseTest {
+
+    private void utilsCreateFreestyleProject(String projectName) {
+
+        getDriver().findElement(By.xpath("//a[contains(@href, 'newJob')]")).click();
+        getDriver().findElement(By.xpath("//input[contains(@class, 'jenkins-input')]")).sendKeys(projectName);
+        getDriver().findElement(By.xpath("//li[contains(@class, 'FreeStyleProject')]")).click();
+        getDriver().findElement(By.xpath("//button[contains(@id, 'ok-button')]")).click();
+    }
+
     @Test
     public void testVersion() {
 
@@ -36,4 +41,56 @@ public class GroupTestscriptCollaboratoriumTest extends BaseTest {
                 String.format("Pipeline %s", projectName));
     }
 
+    @Test
+    public void testJenkinsGetVersionInAboutMenu() {
+
+        getDriver().findElement(By.className("page-footer__links")).findElement(By.tagName("button")).click();
+        getDriver().findElement(By.xpath("//a[@href = '/manage/about']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.className("app-about-version")).getText(),
+                "Version 2.414.2");
+    }
+
+    @Test
+    public void testJenkinsFreestyleProjectIsCreatedCheckByH1() {
+
+        final String expectedProjectName = "testJenkinsFreestyleProjectIsCreatedCheckByH1()";
+
+        utilsCreateFreestyleProject(expectedProjectName);
+        getDriver().findElement(By.xpath("//button[contains(@name, 'Submit')]")).click();
+
+        String actualProjectName = getDriver().findElement(By
+                .xpath("//h1[@class = 'job-index-headline page-headline']")).getText();
+
+        Assert.assertEquals(actualProjectName, String.format("Project %s", expectedProjectName));
+    }
+
+    @Test
+    public void testJenkinsFreestyleProjectIsCreatedCheckByBreadcrumb() {
+
+        final String expectedProjectName = "testJenkinsFreestyleProjectIsCreatedCheckByBreadcrumb()";
+
+        utilsCreateFreestyleProject(expectedProjectName);
+
+        String actualProjectName = getDriver().findElement(By
+                .xpath(String.format("//a[contains(@href, 'job/%s/')]", expectedProjectName))).getText();
+
+        Assert.assertEquals(actualProjectName, expectedProjectName);
+    }
+
+    @Test
+    public void testJenkinsNewProjectIsDisplayedOnDashboard() {
+
+        final String expectedProjectName = "testJenkinsNewProjectIsDisplayedOnDashboard()";
+
+        utilsCreateFreestyleProject(expectedProjectName);
+
+        getDriver().findElement(By.xpath("//a[@id = 'jenkins-home-link']")).click();
+
+        String actualProjectName = getDriver().findElement(By.xpath("//div[contains(@class, 'dashboard')]"))
+                .findElement(By.xpath(String.format("//a[contains(@href, 'job/%s/')]/span", expectedProjectName)))
+                .getText();
+
+        Assert.assertEquals(actualProjectName, expectedProjectName);
+    }
 }
