39f0c0b57400921375c17d3c65c50c369d50944c
Alena Iadkouskaya <117815613+LenKopt@users.noreply.github.com>
2023-10-20 11:30:36 +0200

    Iodkovskaya/tests (#809)
    * refactoring testJenkinsAddNewItemAndSearch
    
    * add tests testJenkinsSearchItem() and testJenkinsSearchItemWithEmptyList()
    
    ---------
    
    Co-authored-by: iod.elena86 <koptewka@mail.ru>

--- src/test/java/school/redrover/GroupJavaAutomationTest.java
+++ src/test/java/school/redrover/GroupJavaAutomationTest.java
@@ -6,6 +6,8 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.util.List;
+
 
 public class GroupJavaAutomationTest extends BaseTest {
 
@@ -21,37 +23,61 @@ public class GroupJavaAutomationTest extends BaseTest {
         String versionJenkins = versionJenkinsButton.getText();
         Assert.assertEquals(versionJenkins,"Jenkins 2.414.2");
     }
+
+    private void addNewItem(String nameProject) {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(nameProject);
+        getDriver().findElement(By.xpath("//span[text()='Freestyle project']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+    }
     @Test
-    public void testJenkinsAddNewItemAndSearch(){
-        WebElement buttonNewItem = getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"));
-        buttonNewItem.click();
+    public void testJenkinsAddNewItemAndSearch() {
 
-        String nameProject = "Project_iod";
-        WebElement inputFieldName = getDriver().findElement(By.id("name"));
-        inputFieldName.sendKeys(nameProject);
-
-        WebElement freestyleProject = getDriver().findElement(By.xpath("//span[text()='Freestyle project']"));
-        freestyleProject.click();
-
-        WebElement buttonOk = getDriver().findElement(By.id("ok-button"));
-        buttonOk.click();
-
-        WebElement buttonSave = getDriver().findElement(By.xpath("//button[@name='Submit']"));
-        buttonSave.click();
-
+        final String nameProject = "Project_iod";
+        addNewItem(nameProject);
         Assert.assertEquals(getDriver().findElement(By.xpath("//a[@id='skip2content']/following-sibling::*")).getText(),"Project " + nameProject);
 
     }
-    @Ignore
     @Test
-    public void testJenkinsSearchItem(){
-        testJenkinsAddNewItemAndSearch();
+    public void testJenkinsSearchItem() {
+        final String searchSubString = "project";
+
+        addNewItem("Project_1");
 
         WebElement inputSearch = getDriver().findElement(By.xpath("//input[@id='search-box']"));
-        inputSearch.sendKeys("iod");
+        inputSearch.sendKeys(searchSubString);
         inputSearch.submit();
 
+        List<WebElement> listSearchProjects = getDriver().findElements(By.xpath("//li[@id]"));
+        Assert.assertTrue(!listSearchProjects.isEmpty());
+        Assert.assertTrue(checkAllSearchMatches(listSearchProjects, searchSubString));
+    }
+    @Test
+    public void testJenkinsSearchItemWithEmptyList() {
+        final String searchSubString = "projekt";
 
+        addNewItem("Project_1");
+
+        WebElement inputSearch = getDriver().findElement(By.xpath("//input[@id='search-box']"));
+        inputSearch.sendKeys(searchSubString);
+        inputSearch.submit();
+
+        List<WebElement> listSearchProjects = getDriver().findElements(By.xpath("//li[@id]"));
+
+        WebElement textError = getDriver().findElement(By.xpath("//h1/following-sibling::*"));
+
+        Assert.assertTrue(listSearchProjects.isEmpty());
+        Assert.assertTrue(textError.getAttribute("class").contains("error"));
+
+    }
+    private boolean checkAllSearchMatches(List<WebElement> list, String searchSubString) {
+        for (WebElement item : list) {
+            if (item.getText().contains(searchSubString)){
+                return false;
+            }
+        }
+        return true;
     }
 }
 
