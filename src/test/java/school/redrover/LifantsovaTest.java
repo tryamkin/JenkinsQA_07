d7b6c8097923d0ecf70ec4425e17096a8be452e6
Ekaterina Lifantsova <141231367+Lifantsova@users.noreply.github.com>
2023-10-21 21:02:32 +0400

    Two new JenkinsTest (#855)
    Co-authored-by: Lifantsova <lifancova@mail.ru>

--- src/test/java/school/redrover/LifantsovaTest.java
+++ src/test/java/school/redrover/LifantsovaTest.java
@@ -1,17 +1,11 @@
 package school.redrover;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.JenkinsUtils;
 
-@Ignore
+
 public class LifantsovaTest extends BaseTest {
 
     @Test
@@ -21,67 +15,16 @@ public class LifantsovaTest extends BaseTest {
                 "Add description");
     }
 
-    @Ignore
     @Test
-    public void testSearch1() throws InterruptedException {
+    public void testCreateNewProject() {
 
-        getDriver().get("https://resh.edu.ru/");
+        getDriver().findElement(By.xpath("//a [@href='newJob']")).click();
+        getDriver().findElement(By.xpath("//input [@name='name']")).sendKeys("NewJenkinsProject");
+        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
-        Thread.sleep(3000);
-
-        WebElement textBox = getDriver().findElement(By.id("search"));
-        textBox.sendKeys("Математика");
-
-        Thread.sleep(1000);
-
-        WebElement searchButton = getDriver().findElement(By.xpath("/html/body/div[1]/div/header/div/div/div[2]/form/div/button"));
-        searchButton.click();
-
-        WebElement title = getDriver().findElement(By.className("content-title"));
-        String value = title.getText();
-        Assert.assertEquals(value, "РЕЗУЛЬТАТЫ ПОИСКА");
-    }
-
-    @Ignore
-    @Test
-    public void testSearch2() throws InterruptedException {
-
-        getDriver().get("https://resh.edu.ru/");
-
-        Thread.sleep(3000);
-
-        WebElement element = getDriver().findElement(By.xpath("/html/body/div[1]/div/header/nav/div/div/a[1]"));
-        element.click();
-
-        Thread.sleep(1000);
-
-        WebElement title = getDriver().findElement(By.className("content-title"));
-        String value = title.getText();
-        Assert.assertEquals(value, "УЧЕБНЫЕ ПРЕДМЕТЫ");
-    }
-
-    @Ignore
-    @Test
-    public void testSearch3() throws InterruptedException {
-
-        getDriver().get("https://resh.edu.ru/");
-
-        Thread.sleep(3000);
-
-        WebElement href1 = getDriver().findElement(By.xpath("/html/body/div[1]/div/main/div[2]/div/div[2]/a[3]"));
-        href1.click();
-
-        WebElement title1 = getDriver().findElement(By.className("content-title"));
-        String value1 = title1.getText();
-        Assert.assertEquals(value1, "МУЗЕИ");
-
-        WebElement href2 = getDriver().findElement(By.xpath("/html/body/div[1]/div/main/div[1]/div/div/div[5]/a[5]/div[2]"));
-        href2.click();
-
-        getDriver().get("https://www.culture.ru/institutes/4445/muzei-usadba-n-g-chernyshevskogo-g-saratova");
-
-        WebElement title2 = getDriver().findElement((By.className("tAsaH")));
-        String value2 = title2.getText();
-        Assert.assertEquals(value2, "Музей-усадьба Н.Г. Чернышевского г. Саратова");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[@class='job-index-headline page-headline']")).getText(),
+                "Project NewJenkinsProject");
     }
 }
