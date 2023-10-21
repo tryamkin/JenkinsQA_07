e5c7aa369893ea9b039d37d77ebb9f0b567dfecc
pas-par <116380781+pas-par@users.noreply.github.com>
2023-10-21 23:25:13 +0300

    Add universal Create Job Method and test for create all jobs (#869)

--- src/test/java/school/redrover/GroupPlusThreeTest.java
+++ src/test/java/school/redrover/GroupPlusThreeTest.java
@@ -27,7 +27,7 @@ public class GroupPlusThreeTest extends BaseTest {
     }
 
     @Test (description = "Freestyle project create from New Item")
-    public void testCreateJob(){
+    public void testCreateJob() {
         String jobName = "Freestyle";
 
         getDriver().findElement(By.xpath("//*[@class='task '][1]")).click();
@@ -41,6 +41,71 @@ public class GroupPlusThreeTest extends BaseTest {
         String resultHeading = getDriver().findElement(By.xpath("//h1")).getText();
 
         assertEquals(resultHeading, "Project " + jobName);
+    }
 
+    public String universalCreateJobMethod(String name, String radioButtonClassName) {
+        getDriver().findElement(By.xpath("//*[@class='task '][1]")).click();
+
+        getDriver().findElement(By.id("name")).sendKeys(name);
+        getDriver().findElement(By.className(radioButtonClassName)).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        getDriver().findElement(By.name("Submit")).click();
+
+        return getDriver().findElement(By.xpath("//h1")).getText();
+    }
+
+    @Test (description = "Freestyle project create from New Item")
+    public void testFreestyleProject() {
+        String jobName = "Pipeline";
+
+        String result = universalCreateJobMethod(jobName, "org_jenkinsci_plugins_workflow_job_WorkflowJob");
+
+        assertEquals(result, "Pipeline " + jobName);
+    }
+
+    @Test (description = "Pipeline project create from New Item")
+    public void testCreatePipeline() {
+        String jobName = "Pipeline";
+
+        String result = universalCreateJobMethod(jobName, "org_jenkinsci_plugins_workflow_job_WorkflowJob");
+
+        assertEquals(result, "Pipeline " + jobName);
+    }
+
+    @Test (description = "Multi-configuration project project create from New Item")
+    public void testMultiConfigurationProject() {
+        String jobName = "Multi-configuration project";
+
+        String result = universalCreateJobMethod(jobName, "hudson_matrix_MatrixProject");
+
+        assertEquals(result, "Project " + jobName);
+    }
+
+    @Test (description = "Folder project create from New Item")
+    public void testFolderProject() {
+        String jobName = "Folder";
+
+        String result = universalCreateJobMethod(jobName, "com_cloudbees_hudson_plugins_folder_Folder");
+
+        assertEquals(result, jobName);
+    }
+
+    @Test (description = "Multibranch Pipeline project create from New Item")
+    public void testMultibranchPipelineProject() {
+        String jobName = "Multibranch Pipeline";
+
+        String result = universalCreateJobMethod(jobName, "org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject");
+
+        assertEquals(result, jobName);
+    }
+
+    @Test (description = "Organization Folder project create from New Item")
+    public void testOrganizationFolderProject() {
+        String jobName = "Organization Folder";
+
+        String result = universalCreateJobMethod(jobName, "jenkins_branch_OrganizationFolder");
+
+        assertEquals(result, jobName);
     }
 }
