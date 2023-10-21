04009f3e534ae93e2aad81e048fef5bf5f268a09
pas-par <116380781+pas-par@users.noreply.github.com>
2023-10-21 22:36:16 +0300

    Add job create test (#867)

--- src/test/java/school/redrover/GroupPlusThreeTest.java
+++ src/test/java/school/redrover/GroupPlusThreeTest.java
@@ -6,21 +6,41 @@ import org.testng.annotations.Test;
 
 import school.redrover.runner.BaseTest;
 
+import static org.testng.Assert.assertEquals;
+
 public class GroupPlusThreeTest extends BaseTest {
     
     @Test (description = "Test creating new item")
     public void testNewItem () {
         
-    final String itemName = "Akiko";
-    getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
-    getDriver().findElement(By.id("name")).sendKeys(itemName);
-    getDriver().findElement(By.xpath("//li[@class = 'hudson_model_FreeStyleProject']")).click();
-    getDriver().findElement(By.id("ok-button")).click();
-    getDriver().findElement(By.name("Submit")).click();
-    
+        final String itemName = "Akiko";
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(itemName);
+        getDriver().findElement(By.xpath("//li[@class = 'hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
 
-    Assert.assertNotNull(getDriver().findElement(By.xpath("//a[@href = '/job/" + itemName + "/']")), "Ссылка на страницу созданного Item не была найдена");
-    Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Project " + itemName);
+
+        Assert.assertNotNull(getDriver().findElement(By.xpath("//a[@href = '/job/" + itemName + "/']")), "Ссылка на страницу созданного Item не была найдена");
+        assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Project " + itemName);
+
+    }
+
+    @Test (description = "Freestyle project create from New Item")
+    public void testCreateJob(){
+        String jobName = "Freestyle";
+
+        getDriver().findElement(By.xpath("//*[@class='task '][1]")).click();
+
+        getDriver().findElement(By.id("name")).sendKeys(jobName);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        getDriver().findElement(By.name("Submit")).click();
+
+        String resultHeading = getDriver().findElement(By.xpath("//h1")).getText();
+
+        assertEquals(resultHeading, "Project " + jobName);
 
     }
 }
