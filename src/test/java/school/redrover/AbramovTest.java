c6d32673d7c1aad158000b1a1af5a9bd8938bd66
AlexAbramovWork <alexabramov.work@gmail.com>
2023-10-24 17:19:55 +0300

    Add Delete job test

--- src/test/java/school/redrover/AbramovTest.java
+++ src/test/java/school/redrover/AbramovTest.java
@@ -2,25 +2,29 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class AbramovTest extends BaseTest {
 
+    private void goHome() {
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+    }
+
+    private void createNewJob(String jobName) {
+        getDriver().findElement(By.cssSelector("a[href='/view/all/newJob']")).click();
+        getDriver().findElement(By.cssSelector(".jenkins-input")).sendKeys(jobName);
+        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.cssSelector("#ok-button")).click();
+    }
+
     @Test
     public void testNewFirstJenkinsFreestyleProject() {
-        WebElement newJob = getDriver().findElement(By.cssSelector("a[href='newJob']"));
-        newJob.click();
+        final String jobName = "NewTestJob01";
 
-        WebElement inputJobName = getDriver().findElement(By.cssSelector(".jenkins-input"));
-        inputJobName.sendKeys("NewTestJob01");
-
-        WebElement freestyleJobOption = getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject"));
-        freestyleJobOption.click();
-
-        WebElement okNewJobButton = getDriver().findElement(By.cssSelector("#ok-button"));
-        okNewJobButton.click();
+        createNewJob(jobName);
 
         WebElement newJobLinkName = getDriver().findElement(By.xpath("//div[@id='breadcrumbBar']//li[3]/a"));
         String linkText = newJobLinkName.getText();
@@ -33,8 +37,8 @@ public class AbramovTest extends BaseTest {
         WebElement aboutButton = getDriver().findElement(By.xpath("//footer//button"));
         aboutButton.click();
 
-        WebElement aboutJen = getDriver().findElement(By.xpath("//a[contains(@href,'/manage/about')]"));
-        aboutJen.click();
+        WebElement aboutJenkinsLink = getDriver().findElement(By.xpath("//a[contains(@href,'/manage/about')]"));
+        aboutJenkinsLink.click();
 
         WebElement xStreamVersion = getDriver().findElement(By.xpath("//td[contains(text(),'com.thoughtworks.xstream:xstream')]"));
         String versionText = xStreamVersion.getText();
@@ -42,5 +46,27 @@ public class AbramovTest extends BaseTest {
         String expectedText = "com.thoughtworks.xstream:xstream:";
         String expectedVersion = "1.4.20";
         Assert.assertEquals(versionText,expectedText+expectedVersion);
+    }
+
+    @Test
+    public void testDeleteTheRightJobWithDropdown() {
+        final String jobNameToDelete = "JobToDelete";
+        final String jobNameToKeep = "JobToKeep";
+
+        createNewJob(jobNameToKeep);
+        goHome();
+        createNewJob(jobNameToDelete);
+        goHome();
+
+        Actions action = new Actions(getDriver());
+
+        WebElement createdJobInTheList = getDriver().findElement(By.xpath("//a[contains(@href, 'job/JobToDelete/')]"));
+        action.moveToElement(createdJobInTheList).perform();
+        getDriver().findElement(By.xpath("//tbody//button[@class='jenkins-menu-dropdown-chevron']")).click();
+        getDriver().findElement(By.xpath("//button[contains(@href, 'doDelete')]")).click();
+        getDriver().switchTo().alert().accept();
+
+        String jobNameInTheList = getDriver().findElement(By.xpath("//a[contains(@href, 'job/JobToKeep/')]")).getText();
+        Assert.assertEquals(jobNameInTheList, jobNameToKeep);
     }
 }
