d9ca24ec1669877b3f25ea05d12f2c0f591e2662
Lelik-gal <147049745+Lelik-gal@users.noreply.github.com>
2023-10-24 13:17:30 +0300

    added test changes (#949)

--- src/test/java/school/redrover/GroupSurvivorsTest.java
+++ src/test/java/school/redrover/GroupSurvivorsTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -33,9 +34,36 @@ public class GroupSurvivorsTest extends BaseTest {
         getDriver().findElement(By.xpath("//*[@id = 'ok-button']")).click();
         getDriver().findElement(By.xpath("//button[contains(text(), 'Save')]")).click();
 
-        Assert.assertEquals(getDriver().findElement(By.cssSelector(".job-index-headline.page-headline")).getText(),
-                "Project First project test");
+        Assert.assertEquals(getDriver().findElement(By.cssSelector(".job-index-headline.page-headline")).getText(), "Project First project test");
     }
+
+    @Test
+    public void testOlgaEditDescription() {
+
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.xpath("//*[@name = 'description']")).sendKeys("Test description");
+        getDriver().findElement(By.xpath("//*[@id = 'description']/form/div[2]/button")).click();
+        getDriver().findElement(By.id("description-link")).click();
+
+        getDriver().findElement(By.xpath("//*[@name = 'description']")).clear();
+        getDriver().findElement(By.xpath("//*[@name = 'description']")).sendKeys("Test new");
+        getDriver().findElement(By.xpath("//*[@id = 'description']/form/div[2]/button")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id = 'description']/div")).getText(), "Test new");
+    }
+
+    @Test
+    public void testOlgaTransitionAnalysis() {
+
+        getDriver().findElement(By.xpath("//*[@id='jenkins']/footer/div/div[2]/button")).click();
+        getDriver().findElement(By.className("jenkins-dropdown__item")).click();
+        getDriver().findElement(By.xpath("//input[@role='searchbox']")).sendKeys("log", Keys.ENTER);
+        getDriver().findElement(By.className("jenkins-table__link")).click();
+
+        Assert.assertEquals(getDriver().getTitle(), "All Jenkins Logs [Jenkins]");
+
+    }
+
 }
 
 
