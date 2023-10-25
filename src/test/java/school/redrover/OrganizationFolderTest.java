ecd7799f9c731fb5f48a6209915263d76e913dff
Elisey Rodriguez <eliseyrodriguez@gmail.com>
2023-10-25 14:11:36 +0100

    TC_00.005.02 | Create Organization Folder with valid name (#978)
    TC_00.005.02 | Create Organization Folder with valid name

--- src/test/java/school/redrover/OrganizationFolderTest.java
+++ src/test/java/school/redrover/OrganizationFolderTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class OrganizationFolderTest extends BaseTest {
+
     private void creationNewOrganizationFolder(String folderName) {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.id("name")).sendKeys(folderName);
@@ -26,6 +27,20 @@ public class OrganizationFolderTest extends BaseTest {
 
     }
 
+    @Test
+    public void testCreateWithValidName() {
+        final String validName = "MyOrganization";
+
+        getDriver().findElement(By.cssSelector("a[href='/view/all/newJob'")).click();
+        getDriver().findElement(By.id("name")).sendKeys(validName);
+        getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        getDriver().findElement(By.id("jenkins-head-icon")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.cssSelector("td a[href='job/" + validName + "/'] span")).getText(),
+                validName,
+                "Created organization name is incorrect");
+    }
 }
-
-
