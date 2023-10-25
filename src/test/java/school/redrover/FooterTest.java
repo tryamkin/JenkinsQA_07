9b29c7841d17b3ad9a56d782635d447569907e5b
Nataliia B <114429981+NataliiaBor1@users.noreply.github.com>
2023-10-25 11:02:41 -0700

    TC_12.002.01 added new test for verifying clickability of REST API link (#988)

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class FooterTest extends BaseTest {
+
     @Test
     public void testVersionJenkins() {
         Assert.assertEquals(
@@ -13,5 +14,12 @@ public class FooterTest extends BaseTest {
                 .findElement(By.xpath("//*[@id='jenkins']/footer/div/div[2]/button"))
                 .getText(),
         "Jenkins 2.414.2");
+    }
+
+    @Test
+    public void testVerifyClickabilityOfRestAPILink() {
+        getDriver().findElement(By.xpath("//a[@href='api/']")).click();
+
+        Assert.assertEquals(getDriver().getTitle(), "Remote API [Jenkins]");
     }
 }
