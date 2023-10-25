3707331f618018241e317654a5d2651ac39bf82e
SleznikEvgeniy <77625845+Yauheni95@users.noreply.github.com>
2023-10-25 09:38:51 -0400

    TC_01.003.01 | FreestyleProject > Delete project (#980)
    * TC_01.003.01 | FreestyleProject > Delete project
    
    * TC_01.003.01 | FreestyleProject > Delete project

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -2,15 +2,41 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import java.util.List;
+import java.util.stream.Collectors;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
 import java.util.UUID;
 
-import static org.testng.Assert.assertEquals;
+
 
 public class FreestyleProjectTest extends BaseTest {
+    private void goToJenkinsHomePage() {
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+    }
+
+    private boolean isProjectExist(String projectName) {
+        return !getDriver().findElements(By.id("job_" + projectName)).isEmpty();
+    }
+
+    private void createFreeStyleProject(String projectName) {
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.id("name")).sendKeys(projectName);
+        getDriver().findElement(By.id("ok-button")).click();
+    }
+
+    private List<String> getAllProjectsNames() {
+        return getDriver()
+                .findElements(By.xpath("//a[@class='jenkins-table__link model-link inside']"))
+                .stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+    }
+
 
     @Test
     public void testCreate() {
@@ -26,7 +52,7 @@ public class FreestyleProjectTest extends BaseTest {
 
         getDriver().findElement(By.xpath("//td/a[@href = 'job/" + projectName + "/']")).click();
 
-        Assert.assertEquals(
+        assertEquals(
                 getDriver().findElement(By.cssSelector("#main-panel > h1")).getText(),
                 "Project " + projectName);
     }
@@ -54,4 +80,22 @@ public class FreestyleProjectTest extends BaseTest {
         String actualProjectName = projectName.getText();
         assertEquals(actualProjectName, randomName);
     }
+  
+  @Test
+    public void testDelete() {
+        final String projectName = "Test Project";
+        int initialProjectsAmount = getAllProjectsNames().size();
+        createFreeStyleProject(projectName);
+        goToJenkinsHomePage();
+
+        getDriver().findElement(By.xpath("//span[contains(text(),'" + projectName + "')]")).click();
+        getDriver().findElement(By.xpath("//a[contains(@data-message,'Delete')]")).click();
+        getDriver().switchTo().alert().accept();
+        goToJenkinsHomePage();
+
+        int resultingProjectsAmount = getAllProjectsNames().size();
+        assertEquals(initialProjectsAmount, resultingProjectsAmount);
+        assertFalse(isProjectExist(projectName));
+}
+  
 }
