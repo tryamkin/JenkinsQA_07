781885547e87a9ef973a62cbb8dbe52a1f47242a
Katya Suhr <38622937+katy1313@users.noreply.github.com>
2023-10-26 17:07:33 -0400

    TC_00.005 | Create Organization Folder with Empty Name (#1061)

--- src/test/java/school/redrover/OrganizationFolder3Test.java
+++ src/test/java/school/redrover/OrganizationFolder3Test.java
@@ -39,7 +39,6 @@ public class OrganizationFolder3Test extends BaseTest {
         String errorMessage = getDriver().findElement(By.id("itemname-invalid")).getText();
 
         Assert.assertEquals(errorMessage, "» ‘" + invalidData + "’ is an unsafe character");
-
     }
 
     @Test
@@ -52,5 +51,14 @@ public class OrganizationFolder3Test extends BaseTest {
         getDriver().findElement(By.id("ok-button")).click();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Error");
+    }
+
+    @Test
+    public void testCreateProjectWithEmptyName() {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//li//span[text()='Organization Folder']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.id("itemname-required")).getText(),
+                "» This field cannot be empty, please enter a valid name");
     }
 }
