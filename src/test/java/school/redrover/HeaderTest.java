9487bd4eeccee486dd153a3c9063ba91166625bb
Anzhelika Lashko <120133812+AnzhelikaLa@users.noreply.github.com>
2023-10-26 15:10:33 -0500

    AddNewTestClickLogoToMainPage (#1054)

--- src/test/java/school/redrover/HeaderTest.java
+++ src/test/java/school/redrover/HeaderTest.java
@@ -15,4 +15,14 @@ public class HeaderTest extends BaseTest {
 
         Assert.assertTrue(getDriver().getTitle().contains("Dashboard"));
     }
+    @Test
+    public void testClickLogoToMainPage() {
+
+        getDriver().findElement(By.xpath("//a[@href='/me/my-views']")).click();
+
+        getDriver().findElement(By.id("jenkins-head-icon")).click();
+
+        Assert.assertTrue(
+                getDriver().findElement(By.cssSelector(".empty-state-block > h1")).getText().contains("Welcome to Jenkins!"));
+    }
 }
