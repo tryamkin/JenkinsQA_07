093247cefe2a71f92a7291cb5a627c1711961e0d
AkiMira <alfa88@inbox.ru>
2023-10-26 15:40:42 +0300

    TC_00.002.03 Create Pipeline with dublicate name (#1021)
    * TC_00.002.02
    
    * TC_00.002.03

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -37,6 +37,39 @@ public class PipelineTest extends BaseTest {
     }
 
     @Test
+    public void testCreateWithEmptyName() {
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
+
+        getDriver().findElement(By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//*[@id=\"itemname-required\"]")).getText(),
+                "» This field cannot be empty, please enter a valid name");
+        Assert.assertTrue(
+                getDriver().findElement(By.cssSelector(".disabled")).isDisplayed());
+    }
+
+    @Test
+    public void testCreateWithDublicateName() {
+        final String pipelineName = "PipelineName";
+        createPipeline(pipelineName);
+
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(pipelineName);
+
+        getDriver().findElement(By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//*[@id=\"itemname-invalid\"]")).getText(),
+                "» A job already exists with the name ‘" + pipelineName + "’");
+
+
+        getDriver().findElement(By.xpath("//button[@id = 'ok-button']")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.cssSelector("#main-panel")).getText().contains("A job already exists with the name ‘" + pipelineName + "’"));
+
+    }
+
+    @Test
     public void testRenamePipeline() {
         String pipelineNameForCreate = "Hello";
         String pipelineNameForRename = "HaveAGoodDay";
