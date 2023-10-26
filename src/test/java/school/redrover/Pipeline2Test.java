751aa75a098ef37c57ed8c9eda73c6aa7f6ad913
Victoria_Lem <115832814+VictoriaLema@users.noreply.github.com>
2023-10-26 11:50:49 -0400

    TC_02.001.01 | Pipeline > View pipeline page > Verify the pipeline description displays (#1036)
     TC_02.001.01 | Verify the pipeline description displays

--- src/test/java/school/redrover/Pipeline2Test.java
+++ src/test/java/school/redrover/Pipeline2Test.java
@@ -7,8 +7,6 @@ import school.redrover.runner.BaseTest;
 
 public class Pipeline2Test extends BaseTest {
 
-    private static final String JOB_NAME = "New_Pipeline";
-
     private void createAPipeline (String jobName) {
         getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
 
@@ -19,27 +17,50 @@ public class Pipeline2Test extends BaseTest {
         getDriver().findElement(By.name("Submit")).click();
     }
 
+    private void goDashboardByBreadcrumb () {
+        getDriver().findElement(By.xpath("//li[@class = 'jenkins-breadcrumbs__list-item']/a[@href='/']")).click();
+    }
     @Test
     public void testCreate() {
-        createAPipeline(JOB_NAME);
+        final String jobName = "New_Pipeline";
+
+        createAPipeline(jobName);
 
         getDriver().findElement(By.xpath("//li[@class = 'jenkins-breadcrumbs__list-item']/a[@href='/']")).click();
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).getText(), JOB_NAME);
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).getText(), jobName);
     }
 
     @Test
     public void testDelete() {
+        final String jobName = "MyPipeline";
 
-        createAPipeline(JOB_NAME);
+        createAPipeline(jobName);
 
         getDriver().findElement(By.xpath("//li[@class = 'jenkins-breadcrumbs__list-item']/a[@href='/']")).click();
 
-        getDriver().findElement(By.xpath("//td/a[@href='job/" + JOB_NAME + "/']")).click();
+        getDriver().findElement(By.xpath("//td/a[@href='job/" + jobName + "/']")).click();
         getDriver().findElement(By.xpath("//span[contains(text(),'Delete')]")).click();
 
         getDriver().switchTo().alert().accept();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Welcome to Jenkins!");
+    }
+
+    @Test
+    public void testDescriptionDisplays() {
+        final String jobName = "Pipeline1";
+        final String description = "Description of the Pipeline ";
+
+        createAPipeline(jobName);
+        goDashboardByBreadcrumb();
+
+        getDriver().findElement(By.xpath("//td/a[@href='job/" + jobName + "/']")).click();
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.cssSelector("textarea[name='description']")).sendKeys( description + jobName);
+        getDriver().findElement(By.xpath("//div[@id = 'description']//button[@name = 'Submit']")).click();
+
+        Assert.assertEquals(getDriver().findElement(
+                By.xpath("//div[@class = 'jenkins-buttons-row jenkins-buttons-row--invert']/preceding-sibling :: div")).getText(), description + jobName);
     }
 }
