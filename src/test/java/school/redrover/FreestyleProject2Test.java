14a04175adc3bd75339b6ed85b0cdaf5d6010754
Maria Dymsha <103815224+dymshamaria@users.noreply.github.com>
2023-10-26 12:53:05 +0300

    addTestCreate2 (#1025)

--- src/test/java/school/redrover/FreestyleProject2Test.java
+++ src/test/java/school/redrover/FreestyleProject2Test.java
@@ -25,4 +25,19 @@ public class FreestyleProject2Test extends BaseTest {
                 getDriver().findElement(By.cssSelector("#main-panel > h1")).getText(),
                 "Project " + projectName);
     }
+
+    @Test
+    public void testCreate2() {
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.cssSelector(".jenkins-input")).sendKeys("Online store");
+        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.cssSelector("#ok-button")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.cssSelector("#jenkins-name-icon")).click();
+        getDriver().findElement(By.xpath("//span[text()='Online store']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[@class='job-index-headline page-headline']"))
+                .getText(),"Project Online store");
+    }
 }
