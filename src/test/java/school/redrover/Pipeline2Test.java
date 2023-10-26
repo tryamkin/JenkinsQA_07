af55ad8fb4280de5cb242756636b18da6c29ffde
Victoria_Lem <115832814+VictoriaLema@users.noreply.github.com>
2023-10-26 17:20:37 -0400

    TC_02.001.02 | Verify pipeline permalinks section before builds is empty (#1040)

--- src/test/java/school/redrover/Pipeline2Test.java
+++ src/test/java/school/redrover/Pipeline2Test.java
@@ -7,7 +7,7 @@ import school.redrover.runner.BaseTest;
 
 public class Pipeline2Test extends BaseTest {
 
-    private void createAPipeline (String jobName) {
+    private void createAPipeline(String jobName) {
         getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
 
         getDriver().findElement(By.id("name")).sendKeys(jobName);
@@ -17,9 +17,10 @@ public class Pipeline2Test extends BaseTest {
         getDriver().findElement(By.name("Submit")).click();
     }
 
-    private void goDashboardByBreadcrumb () {
+    private void goDashboardByBreadcrumb() {
         getDriver().findElement(By.xpath("//li[@class = 'jenkins-breadcrumbs__list-item']/a[@href='/']")).click();
     }
+
     @Test
     public void testCreate() {
         final String jobName = "New_Pipeline";
@@ -57,10 +58,24 @@ public class Pipeline2Test extends BaseTest {
 
         getDriver().findElement(By.xpath("//td/a[@href='job/" + jobName + "/']")).click();
         getDriver().findElement(By.id("description-link")).click();
-        getDriver().findElement(By.cssSelector("textarea[name='description']")).sendKeys( description + jobName);
+        getDriver().findElement(By.cssSelector("textarea[name='description']")).sendKeys(description + jobName);
         getDriver().findElement(By.xpath("//div[@id = 'description']//button[@name = 'Submit']")).click();
 
         Assert.assertEquals(getDriver().findElement(
                 By.xpath("//div[@class = 'jenkins-buttons-row jenkins-buttons-row--invert']/preceding-sibling :: div")).getText(), description + jobName);
+    }
+
+    @Test
+    public void testPermalinksIsEmpty() {
+        final String jobName = "NewPipeline";
+
+        createAPipeline(jobName);
+        goDashboardByBreadcrumb();
+
+        getDriver().findElement(By.xpath("//td/a[@href='job/" + jobName + "/']")).click();
+
+        String permalinksInfo = getDriver().findElement(By.xpath("//ul[@class= 'permalinks-list']")).getText();
+
+        Assert.assertTrue(permalinksInfo.isEmpty());
     }
 }
