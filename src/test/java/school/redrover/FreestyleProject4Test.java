6c01026f9871a67df963f39e777e29c73a56ecd8
KaterynaYazici <katerynayazici7@gmail.com>
2023-10-27 15:41:19 +0300

    TC_01.001.06 | FreestyleProject > Add desription

--- src/test/java/school/redrover/FreestyleProject4Test.java
+++ src/test/java/school/redrover/FreestyleProject4Test.java
@@ -5,21 +5,38 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import static old.PetrovTest.projectName;
+
 public class FreestyleProject4Test extends BaseTest {
-    @Test
-    public void testCreateFreestyleProject() {
-        final String projectName = "Test Project";
+
+    public void createFreestyleProject(String projectName) {
+
         getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
 
         getDriver().findElement(By.id("name")).sendKeys(projectName);
         getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).click();
+    }
 
-        getDriver().findElement(By.id("jenkins-home-link")).click();
-        getDriver().findElement(By.xpath("//tr[@id= 'job_" + projectName + "' ] //td[3]/a")).click();
+    @Test
+    public void testFreestyleProjectAddDescription() {
+        final String descriptionForFreestyleProject = "It s a description adding test";
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(),
-                "Project " + projectName);
+        createFreestyleProject("FREE");
+
+        getDriver().findElement(By.id("jenkins-name-icon")).click();//click logo
+        getDriver().findElement(By.xpath("//span[contains(text(),'FR')]"));
+
+        getDriver().findElement(By.id("description-link")).click();
+
+        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys("It s a description adding test");
+        getDriver().findElement(By.xpath("//button[contains(text(),'Save')]")).click();
+
+        getDriver().findElement(By.xpath("//div[contains(text(),'" + descriptionForFreestyleProject + "')]"));
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class='jenkins-buttons-row jenkins-buttons-row--invert']/preceding-sibling::div"))
+                .getText(), descriptionForFreestyleProject);
+
     }
 }
+
