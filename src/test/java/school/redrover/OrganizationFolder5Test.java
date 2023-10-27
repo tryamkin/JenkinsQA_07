9f18dfece3f096a63f52c8759c126a5d4d5bfb27
Natalia Vasylieva <123753026+NatalyaVasylyeva@users.noreply.github.com>
2023-10-27 10:45:36 +0200

    TC_00.005.05 | New Item > Organization Folder > Verify the warning message for dot at the end of Name during the Organization Folder Creation (#1050)

--- src/test/java/school/redrover/OrganizationFolder5Test.java
+++ src/test/java/school/redrover/OrganizationFolder5Test.java
@@ -22,5 +22,20 @@ public class OrganizationFolder5Test extends BaseTest {
         Assert.assertEquals(warningMessageTextActual, WARNING_MESSAGE_TEXT_EXPECTED);
     }
 
+    @Test
+    public void testVerifyWarningMessageWithDotName() {
+        final String WARNING_MESSAGE_TEXT_EXPECTED = "» A name cannot end with ‘.’";
+        final String CSS_COLOR_WARNING_MESSAGE_EXPECTED = "rgba(255, 0, 0, 1)";
+        final String ORGANIZATION_FOLDER_WITH_DOT_NAME = "Organization Folder.";
+
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(ORGANIZATION_FOLDER_WITH_DOT_NAME);
+
+        String warningMessageTextActual = getDriver().findElement(By.cssSelector("#itemname-invalid")).getText();
+        String cssColorWarningMessageActual = getDriver().findElement(By.cssSelector("#itemname-invalid")).getCssValue("color");
+
+        Assert.assertEquals(cssColorWarningMessageActual, CSS_COLOR_WARNING_MESSAGE_EXPECTED);
+        Assert.assertEquals(warningMessageTextActual, WARNING_MESSAGE_TEXT_EXPECTED);
+    }
 
 }
