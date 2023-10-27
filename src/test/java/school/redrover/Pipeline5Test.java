e993a73c4cd56574ede4c29d3f5b46e3a871aa49
Eleksor <94993497+Eleksor@users.noreply.github.com>
2023-10-27 18:57:30 +0300

    Update and rename PipelineDeleteTest.java to Pipeline5Test.java

--- src/test/java/school/redrover/Pipeline5Test.java
+++ src/test/java/school/redrover/Pipeline5Test.java
@@ -6,7 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-public class PipelineDeleteTest extends BaseTest {
+public class Pipeline5Test extends BaseTest {
 
     private boolean isElementExist(String xpath) {
         By locator = By.xpath(xpath);
