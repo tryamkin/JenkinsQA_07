e82a678bd113fe46170918185a7bce7cd7d81693
Nataliia B <114429981+NataliiaBor1@users.noreply.github.com>
2023-10-27 13:29:11 -0700

    TC_14.001.02 added new test for verifying return for Main page from Main menu items pages by clicking Jenkins Logo (#1074)

--- src/test/java/school/redrover/HeaderTest.java
+++ src/test/java/school/redrover/HeaderTest.java
@@ -5,7 +5,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HeaderTest extends BaseTest {
+
+    private void clickJenkinsLogo() {
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+    }
 
     @Test
     public void testReturnWithLogo() {
@@ -15,6 +22,7 @@ public class HeaderTest extends BaseTest {
 
         Assert.assertTrue(getDriver().getTitle().contains("Dashboard"));
     }
+
     @Test
     public void testClickLogoToMainPage() {
 
@@ -24,5 +32,32 @@ public class HeaderTest extends BaseTest {
 
         Assert.assertTrue(
                 getDriver().findElement(By.cssSelector(".empty-state-block > h1")).getText().contains("Welcome to Jenkins!"));
+    }
+
+    @Test
+    public void testReturningBackToMainPageFromMainMenuPages() {
+
+        List<By> mainPageMenuItems = new ArrayList<>();
+
+        mainPageMenuItems.add(By.xpath("//a[@href='/view/all/newJob']"));
+        mainPageMenuItems.add(By.xpath("//a[@href='/asynchPeople/']"));
+        mainPageMenuItems.add(By.xpath("//a[@href='/view/all/builds']"));
+        mainPageMenuItems.add(By.xpath("//a[@href='/manage']"));
+        mainPageMenuItems.add(By.xpath("//a[@href='/me/my-views']"));
+
+        boolean isSuccess = true;
+
+        for (By locator : mainPageMenuItems) {
+
+            getDriver().findElement(locator).click();
+            clickJenkinsLogo();
+
+            if (!getDriver().getTitle().equals("Dashboard [Jenkins]")) {
+                isSuccess = false;
+                break;
+            }
+        }
+
+        Assert.assertTrue(isSuccess);
     }
 }
