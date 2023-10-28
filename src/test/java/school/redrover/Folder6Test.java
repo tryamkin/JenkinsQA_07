b5f43849dfecb243768074e81f735adb051b2e1f
EkaterinaRogozhnikova <134730100+ekaterinarogozhnikova@users.noreply.github.com>
2023-10-28 09:49:27 +0300

    TC_00.005.10 | add create Folder (#1116)

--- src/test/java/school/redrover/Folder6Test.java
+++ src/test/java/school/redrover/Folder6Test.java
@@ -7,19 +7,35 @@ import school.redrover.runner.BaseTest;
 
 public class Folder6Test extends BaseTest {
 
+    final String validName = "Folder1";
+    final String emptyName = "";
+    final String invalidName = "&";
+
+    private void utilsGoDashboard() {
+        getDriver().findElement(By.xpath("//img[@alt = 'Jenkins']")).click();
+    }
+
     @Test
     public void testCreate() {
 
-        getDriver().findElement(By.xpath("//a[@href = \"/view/all/newJob\"]")).click();
-        getDriver().findElement(By.cssSelector(".jenkins-input")).sendKeys("TestFolder");
-        getDriver().findElement(By.xpath("//span[text() = \"Folder\"]")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.xpath("//button[@name = \"Submit\"]")).click();
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
 
-        getDriver().findElement(By.id("jenkins-home-link")).click();
+        getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']")).sendKeys(emptyName);
+        getDriver().findElement(By.xpath("//div[@id = 'itemname-required']"));
 
-        Assert.assertEquals(
-                getDriver().findElement(By.xpath("//td//a[@href = 'job/TestFolder/']")).getText(),
-                "TestFolder");
+        getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']")).sendKeys(invalidName);
+        getDriver().findElement(By.xpath("//div[@id = 'itemname-invalid']"));
+        getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']")).clear();
+
+        getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']")).sendKeys(validName);
+        getDriver().findElement(By.xpath("//li[@class = 'com_cloudbees_hudson_plugins_folder_Folder']")).click();
+        getDriver().findElement(By.xpath("//button[@type = 'submit']")).click();
+
+        utilsGoDashboard();
+
+        Assert.assertEquals(getDriver()
+                .findElement(By.xpath("//a[@class = 'jenkins-table__link model-link inside']"))
+                .getText(), validName);
+        
     }
 }
