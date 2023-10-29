cb7ba4759471577f0b9a2f3383fee78877b752c0
Lana K <nika.benz.21@gmail.com>
2023-10-30 02:43:23 +0300

    TC_01.010.01 | Freestyle project > Configure Build Environment settings > Add timestamps to the Console Output (#1189)

--- src/test/java/school/redrover/FreestyleProject1Test.java
+++ src/test/java/school/redrover/FreestyleProject1Test.java
@@ -1,9 +1,12 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+
+import java.util.List;
 
 import static java.sql.DriverManager.getDriver;
 
@@ -11,6 +14,17 @@ public class FreestyleProject1Test extends BaseTest {
     private final static String PROJECT_NAME = "FreestyleProject";
     private final static String HOME_PAGE = "jenkins-home-link";
     private final static String NAME_SEARCH = "//span[text()='FreestyleProject']";
+
+    private void createJobAndGoToHomePage() {
+        getDriver().findElement(By.xpath("//div[@id='side-panel']//a[contains(@href,'newJob')]")).click();
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input']"))
+                .sendKeys(PROJECT_NAME);
+        getDriver().findElement(By.xpath("//span[text()='Freestyle project']/..")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+
+        getDriver().findElement(By.id(HOME_PAGE)).click();
+    }
+
     @Test
     public void testCreateFreestyleProject() {
 
@@ -41,5 +55,30 @@ public class FreestyleProject1Test extends BaseTest {
         getDriver().findElement(By.xpath(NAME_SEARCH)).click();
         getDriver().findElement(By.xpath("//span[text()='Delete Project']")).click();
         getDriver().switchTo().alert().accept();
+    }
+
+    @Test
+    public void testConfigureBuildEnvironmentSettingsAddTimestamp() throws InterruptedException {
+        createJobAndGoToHomePage();
+
+        getDriver().findElement(By.xpath("//span[text()='" + PROJECT_NAME + "']/..")).click();
+        getDriver().findElement(By.xpath("//span[text()='Configure']/..")).click();
+
+        getDriver().findElement(By.xpath("//button[@data-section-id='build-environment']")).click();
+        Thread.sleep(600);
+        getDriver().findElement(By.xpath("//label[text()='Add timestamps to the Console Output']")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+
+        getDriver().findElement(By.xpath("//span[text()='Build Now']/..")).click();
+        Thread.sleep(5000);
+        getDriver().navigate().refresh();
+        getDriver().findElement(By.xpath("//span[@class='build-status-icon__outer']")).click();
+
+        List<WebElement> timestamps = getDriver().findElements(
+                By.xpath("//pre[@class='console-output']//span[@class='timestamp']"));
+
+        for (WebElement timestamp : timestamps) {
+            Assert.assertTrue(timestamp.getText().trim().matches("[0-9]{2}:[0-9]{2}:[0-9]{2}"));
+        }
     }
 }
