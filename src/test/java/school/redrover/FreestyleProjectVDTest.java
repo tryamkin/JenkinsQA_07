10d31c579c51ecff0c1269db5966837994a7ba79
Vitalii Dimitriiev <vitalydimitriev@gmail.com>
2023-10-29 08:54:00 +0200

    TC_01.006.10 | Freestyle project > Configure General settings > Select Discard old builds checkbox (#1139)
    Co-authored-by: Vidim83 <08091985n@gmail.com>

--- src/test/java/school/redrover/FreestyleProjectVDTest.java
+++ src/test/java/school/redrover/FreestyleProjectVDTest.java
@@ -12,6 +12,8 @@ public class FreestyleProjectVDTest extends BaseTest {
 
     private static final String SUBMIT_BUTTON = "//button[@name='Submit']";
 
+    private static final String CONFIGURE_LINK = "//a[@href='/job/" + PROJECT_NAME + "/configure']";
+
     private void createFreeStyleProject(String projectName) {
         getDriver().findElement(By.linkText("New Item")).click();
         getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
@@ -35,10 +37,31 @@ public class FreestyleProjectVDTest extends BaseTest {
         getDriver().findElement(By.xpath(inputUrlField)).sendKeys("https://github.com/RedRoverSchool/JenkinsQA_07");
         getDriver().findElement(By.xpath(SUBMIT_BUTTON)).click();
 
-        getDriver().findElement(By.xpath("//a[@href='/job/" + PROJECT_NAME + "/configure']")).click();
+        getDriver().findElement(By.xpath(CONFIGURE_LINK)).click();
         getDriver().findElement(By.xpath(sourseCodeManagement)).click();
         js.executeScript("window.scrollBy(0,600)");
 
         Assert.assertEquals(getDriver().findElement(By.xpath(inputUrlField)).getAttribute("value"), "https://github.com/RedRoverSchool/JenkinsQA_07");
+    }
+
+    @Test
+    public void testCheckDiscardOldBuildsCheckbox() {
+        final String inputDaysToKeepBuildsField = "//input[@name='_.daysToKeepStr']";
+        final String inputMaxNumberOfBuildsToKeepField = "//input[@name='_.numToKeepStr']";
+
+        createFreeStyleProject(PROJECT_NAME);
+
+        getDriver().findElement(By.xpath("//label[normalize-space()='Discard old builds']")).click();
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        js.executeScript("window.scrollBy(0,300)");
+
+        getDriver().findElement(By.xpath(inputDaysToKeepBuildsField)).sendKeys("2");
+        getDriver().findElement(By.xpath(inputMaxNumberOfBuildsToKeepField)).sendKeys("3");
+        getDriver().findElement(By.xpath(SUBMIT_BUTTON)).click();
+        getDriver().findElement(By.xpath(CONFIGURE_LINK)).click();
+        js.executeScript("window.scrollBy(0,300)");
+
+        Assert.assertEquals(getDriver().findElement(By.xpath(inputDaysToKeepBuildsField)).getAttribute("value"), "2");
+        Assert.assertEquals(getDriver().findElement(By.xpath(inputMaxNumberOfBuildsToKeepField)).getAttribute("value"), "3");
     }
 }
