7f9d347597a1d3a8582ada75e1781277acf03377
DmitriyKlv <143330510+DmitriyKlv@users.noreply.github.com>
2023-10-29 19:46:26 +0100

    TestCase_01.003.012 (#1181)
    Co-authored-by: DmitriyKlv <rtjoyhome8@gmail.com>

--- src/test/java/school/redrover/FreestyleProject1Test.java
+++ src/test/java/school/redrover/FreestyleProject1Test.java
@@ -9,8 +9,10 @@ import static java.sql.DriverManager.getDriver;
 
 public class FreestyleProject1Test extends BaseTest {
     private final static String PROJECT_NAME = "FreestyleProject";
+    private final static String HOME_PAGE = "jenkins-home-link";
+    private final static String NAME_SEARCH = "//span[text()='FreestyleProject']";
     @Test
-    public void testCreateFreestyle1Project() {
+    public void testCreateFreestyleProject() {
 
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.xpath("//input[@class='jenkins-input']"))
@@ -19,9 +21,25 @@ public class FreestyleProject1Test extends BaseTest {
         getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
         getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate']")).click();
 
-        getDriver().findElement(By.id("jenkins-home-link")).click();
+        getDriver().findElement(By.id(HOME_PAGE)).click();
         String actualName = getDriver()
-                .findElement(By.xpath("//*[@id='job_FreestyleProject']/td[3]")).getText();
+                .findElement(By.xpath(NAME_SEARCH)).getText();
         Assert.assertEquals(actualName, PROJECT_NAME);
+    }
+
+    @Test
+    public void testDeleteFreestyleProject() {
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input']"))
+                .sendKeys(PROJECT_NAME);
+        getDriver().findElement(By.xpath("(//span[@class='label'])[1]")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+        getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate']")).click();
+
+        getDriver().findElement(By.id(HOME_PAGE)).click();
+        getDriver().findElement(By.xpath(NAME_SEARCH)).click();
+        getDriver().findElement(By.xpath("//span[text()='Delete Project']")).click();
+        getDriver().switchTo().alert().accept();
     }
 }
