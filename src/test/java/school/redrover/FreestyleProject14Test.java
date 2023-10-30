3c533c1f31a681b8bb7505d9d3a630ad13ca0f46
tnelyubova <tdnelyubova@gmail.com>
2023-10-30 23:41:29 +0100

    TC_00.001.16 | Add create Freestyle Project

---
+++ src/test/java/school/redrover/FreestyleProject14Test.java
@@ -0,0 +1,27 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class FreestyleProject14Test extends BaseTest {
+
+    @Test
+    public void createFreestyleProject() {
+        final String projectName = "Test Project";
+
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']")).sendKeys(projectName);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
+
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+        getDriver().findElement(By.xpath("//span[text()='Test Project']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Project " + projectName);
+    }
+
+}
