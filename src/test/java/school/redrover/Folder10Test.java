987dadc5229793f0168fc45dbdfd280bc350ab2b
Vladimir Fokichev <106383598+vfokichev2022@users.noreply.github.com>
2023-10-30 19:58:19 +0300

    TC_00.007.06 Create folder (#1202)

--- src/test/java/school/redrover/Folder10Test.java
+++ src/test/java/school/redrover/Folder10Test.java
@@ -1,11 +1,31 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import school.redrover.runner.BaseTest;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
 public class Folder10Test extends BaseTest {
+
+    private static final String NEW_ITEM_NAME = "testfolder";
+
+    @Test
+    public void testCreate() {
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(NEW_ITEM_NAME);
+        getDriver().findElement(By.xpath("//li[@class='com_cloudbees_hudson_plugins_folder_Folder']")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.xpath("//a[@id='jenkins-home-link']")).click();
+
+        getDriver().findElement(By.xpath("//td/a[@href='job/" + NEW_ITEM_NAME + "/']")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//div[@id='main-panel']//h1")).getText(),
+                NEW_ITEM_NAME);
+    }
 
     @Test
     public void testCreatingANewFolder() {
@@ -21,4 +41,5 @@ public class Folder10Test extends BaseTest {
         Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id='job_Folder_1']/td[3]/a")).getText(), folder);
 
     }
+
 }
