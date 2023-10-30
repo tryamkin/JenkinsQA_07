67d8e51b02056835d0213eccf46ae52a17c7842e
Bayan Zhumanova <82990053+bzhumanova@users.noreply.github.com>
2023-10-30 19:45:45 -0400

    TC_12.001.08 | Footer > Jenkins version > About Jenkins (#1242)

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -1,11 +1,24 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class FooterTest extends BaseTest {
+
+    @Test
+    //https://trello.com/c/3ie7dy8O/403-tc1200108-footer-jenkins-version-about-jenkins
+    public void testAboutJenkins() {
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);",
+                getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")));
+
+        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")).click();
+        getDriver().findElement(By.xpath("//a[@href='/manage/about']")).click();
+
+        Assert.assertEquals(getDriver().getTitle(), "About Jenkins 2.414.2 [Jenkins]");
+    }
 
     private void clickRestApi() {
         getDriver().findElement(By.xpath("//a[@href='api/']")).click();
