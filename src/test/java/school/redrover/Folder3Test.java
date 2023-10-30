282b391cc9a2ada3defb504338f2b12fd4e7d1be
AllaBird <146133797+AllaBird@users.noreply.github.com>
2023-10-30 00:09:52 -0400

    TC_04.001.09 | Add Rename folder from drop-down menu next to folder name (#1153)

--- src/test/java/school/redrover/Folder3Test.java
+++ src/test/java/school/redrover/Folder3Test.java
@@ -6,22 +6,45 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class Folder3Test extends BaseTest {
-    
-    @Test
-    public void testCreate() {
-        final String folderName = "Folder1";
-        
+
+    private void createFolder(String folderName) {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
 
         getDriver().findElement(By.cssSelector("#name")).sendKeys(folderName);
-        getDriver().findElement(By.xpath("//span[text()='Folder']")).click();
+        getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
         getDriver().findElement(By.id("ok-button")).click();
 
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
         getDriver().findElement(By.id("jenkins-name-icon")).click();
+    }
 
-        Assert.assertEquals(
-                getDriver().findElement(By.xpath("//td/a[@href='job/" + folderName + "/']")).getText(),
-                folderName);
+    @Test
+    public void testCreate() {
+        final String folderName = "Folder1";
+
+        createFolder(folderName);
+
+        Assert.assertEquals(getDriver().findElement(
+                By.xpath("//td/a[@href='job/" + folderName + "/']")).getText(), folderName);
+    }
+
+    @Test
+    public void testRename() {
+        final String folderName = "Folder1";
+        final String renamedFolder = "Edited Folder1";
+
+        createFolder(folderName);
+
+        getDriver().findElement(By.xpath("//*[@id='job_" + folderName + "']/td[3]/a")).click();
+        getDriver().findElement(By.xpath("//a[@href='/job/" + folderName + "/confirm-rename']")).click();
+
+        getDriver().findElement(By.xpath("//input[@name='newName']")).clear();
+        getDriver().findElement(By.xpath("//input[@name='newName']")).sendKeys(renamedFolder);
+        getDriver().findElement(By.name("Submit")).click();
+
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+
+        Assert.assertEquals(getDriver().findElement(
+                By.xpath("//a[@class='jenkins-table__link model-link inside']")).getText(), renamedFolder);
     }
 }
