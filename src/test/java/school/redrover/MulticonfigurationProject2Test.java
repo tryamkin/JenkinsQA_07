3d3045f9f67e381c65a5066cef9eb3e9100c85f5
MartaShepel <135738827+MartaShepel@users.noreply.github.com>
2023-10-31 18:32:51 +0100

    Marta/tc 00.003.001 (#1233)
    * refactoring fillInFormTest
    
    * TC_00.003.001
    
    * TC_00.003.001
    
    * TC_00.003.001 Resolve conflict

--- src/test/java/school/redrover/MulticonfigurationProject2Test.java
+++ src/test/java/school/redrover/MulticonfigurationProject2Test.java
@@ -27,4 +27,21 @@ public class MulticonfigurationProject2Test extends BaseTest {
 
     }
 
+    @Test
+    public void testCreate1 () {
+        final String ProjectName = "MulticonfigurationProject";
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("MulticonfigurationProject");
+        getDriver().findElement(By.xpath("//li[@class='hudson_matrix_MatrixProject']")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.xpath("//a[@id='jenkins-home-link']")).click();
+
+        getDriver().findElement(By.xpath("//td/a[@href='job/" + ProjectName + "/']")).click();
+        Assert.assertEquals(
+                getDriver().findElement(By.cssSelector(".matrix-project-headline.page-headline")).getText(),
+                "Project " + ProjectName);
+    }
 }
