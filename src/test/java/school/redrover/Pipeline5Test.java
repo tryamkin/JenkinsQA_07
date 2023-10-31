461ad9ee7e7ed9d4ebcdd22aabfdfced371aa967
irina-kuts <125409959+irina-kuts@users.noreply.github.com>
2023-10-30 21:42:37 -0500

    TC_02.007.04 | Pipeline > Rename project > Verify rename from the left menu (#1226)
    * TC_02.007.04 | Pipeline > Rename project > Verify rename from the left menu
    
    * TC_02.007.04 | Pipeline > Rename project > Verify rename from the left menu
    
    * TC_02.007.04 | Pipeline > Rename project > Verify rename from the left menu add changes

--- src/test/java/school/redrover/Pipeline5Test.java
+++ src/test/java/school/redrover/Pipeline5Test.java
@@ -7,6 +7,52 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class Pipeline5Test extends BaseTest {
+    private void createPipeline(String pipelineName) {
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input']")).sendKeys(pipelineName);
+        getDriver().findElement(By.xpath("//li[@class='org_jenkinsci_plugins_workflow_job_WorkflowJob']")).click();
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.xpath("//a[normalize-space()='Dashboard']")).click();
+    }
+
+    @Test
+    public void testCreate() {
+        final String pipelineName = "Pipeline5";
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input']")).sendKeys(pipelineName);
+        getDriver().findElement(By.xpath("//li[@class='org_jenkinsci_plugins_workflow_job_WorkflowJob']")).click();
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.xpath("//a[normalize-space()='Dashboard']")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//span[normalize-space()='" + pipelineName + "']")).getText(),
+                "Pipeline5");
+    }
+
+    @Test
+    public void testRename() {
+        final String pipelineName = "Pipeline5";
+        createPipeline(pipelineName);
+        String aNewName = "MyPipeline10";
+
+        getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).click();
+
+        getDriver().findElement(By.xpath("//a[@href='/job/Pipeline5/confirm-rename']")).click();
+        getDriver().findElement(By.name("newName")).clear();
+        getDriver().findElement(By.name("newName")).sendKeys(aNewName);
+        getDriver().findElement(By.name("Submit")).click();
+        getDriver().findElement(By.xpath("//a[normalize-space()='Dashboard']")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).getText(),
+                "MyPipeline10");
+    }
 
     private boolean isElementExist(String xpath) {
         By locator = By.xpath(xpath);
@@ -39,23 +85,4 @@ public class Pipeline5Test extends BaseTest {
 
         isElementExist("//span[contains(text(), '" + pipelineName + "')]");
     }
-
-
-    @Test
-    public void testCreate() {
-        final String pipelineName = "Pipeline5";
-
-        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//input[@class='jenkins-input']")).sendKeys(pipelineName);
-        getDriver().findElement(By.xpath("//li[@class='org_jenkinsci_plugins_workflow_job_WorkflowJob']")).click();
-        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
-
-        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
-        getDriver().findElement(By.xpath("//a[normalize-space()='Dashboard']")).click();
-
-        Assert.assertEquals(
-                getDriver().findElement(By.xpath("//span[normalize-space()='" + pipelineName + "']")).getText(),
-                "Pipeline5");
-    }
-
 }
