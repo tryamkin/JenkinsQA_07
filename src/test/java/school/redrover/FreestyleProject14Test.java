0c0b3e86358c221204cebaea156bcb387223927e
TetianaBalchunas <147020082+TetianaBalchunas@users.noreply.github.com>
2023-10-30 23:27:26 -0400

    TC_00.001.13 Creating new item > Creating Freestyle project (#1245)
    * TC_00.001.13 Add Create Freestyle project
    
    * TC_00.001.13 Add Create Freestyle project
    renamed class

---
+++ src/test/java/school/redrover/FreestyleProject14Test.java
@@ -0,0 +1,35 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class FreestyleProject14Test extends BaseTest {
+
+    @Test
+
+    public void testCreate () {
+
+        /*
+        Create a freestyle project with valid name
+See created project on a Dashboard
+Can go to created project configuration page
+         */
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@class= 'jenkins-input']")).sendKeys("FreestyleProject1");
+        getDriver().findElement(By.xpath("//li[@class= 'hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.xpath("//button[@id= 'ok-button']")).click();
+        getDriver().findElement(By.xpath("//button[@name= 'Submit']")).click();
+
+        getDriver().findElement(By.xpath("//li/a[@class= 'model-link']")).click();
+        getDriver().findElement(By.xpath("//a[@href='job/FreestyleProject1/']")).click();
+
+
+        Assert.assertEquals
+                (getDriver().findElement(By.xpath("//h1[@class='job-index-headline page-headline']")).getText(), "Project FreestyleProject1");
+
+
+
+    }
+}
