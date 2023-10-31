67a44d79353f98b16f6673e16d5e59e8ed2c9ed7
Ekaterina Lifantsova <141231367+Lifantsova@users.noreply.github.com>
2023-10-31 13:54:15 +0400

    TC_01.001.09 | Add description (#1223)
    Co-authored-by: Lifantsova <lifancova@mail.ru>

--- src/test/java/school/redrover/FreestyleProject13Test.java
+++ src/test/java/school/redrover/FreestyleProject13Test.java
@@ -7,19 +7,45 @@ import school.redrover.runner.BaseTest;
 
 public class FreestyleProject13Test extends BaseTest {
 
-    @Test
-    public void testCreateFreestyleProject() {
+    private final static String PROJECT_NAME = "FreestyleProject";
+
+    private void createFreestyleProject () {
 
         getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
-        getDriver().findElement(By.xpath("//input [@name='name']")).sendKeys("FreestyleProject");
+        getDriver().findElement(By.xpath("//input [@name='name']")).sendKeys(PROJECT_NAME);
         getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
         getDriver().findElement(By.xpath("//img[@id='jenkins-name-icon']")).click();
 
+    }
+
+    @Test
+    public void testCreateFreestyleProject() {
+
+        createFreestyleProject();
+
         getDriver().findElement(By.xpath("//a[@href='job/FreestyleProject/']")).click();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1[@class='job-index-headline page-headline']")).getText(),
-                "Project FreestyleProject");
+                "Project " + PROJECT_NAME);
+    }
+
+    @Test
+    public void testAddDescription() {
+
+        createFreestyleProject();
+
+        getDriver().findElement(By.xpath("//a[@href='job/FreestyleProject/']")).click();
+        getDriver().findElement(By.xpath("//a[@id='description-link']")).click();
+        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys("Add description");
+        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--primary ']")).click();
+
+        getDriver().findElement(By.xpath("//img[@id='jenkins-name-icon']")).click();
+        getDriver().findElement(By.xpath("//a[@href='job/FreestyleProject/']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id='description']/div[1]")).getText(),
+                "Add description");
+
     }
 }
