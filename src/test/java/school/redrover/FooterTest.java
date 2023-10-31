df2a4f9f95470e6d52740c260e195c288d6c64ff
Bayan Zhumanova <82990053+bzhumanova@users.noreply.github.com>
2023-10-31 12:09:29 -0400

    Bayan/tc 12.001.07 footer jenkins version check the tippy box (#1240)
    * https://trello.com/c/OOkxIvHX/402-tc1200107-footer-jenkins-version-check-the-tippy-box
    
    * TC_12.001.07 | Footer > Jenkins version > Check the tippy box
    
    * added sleep for 1 sec
    
    * added sleep for 1 sec cause test was failing on CI

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -1,10 +1,15 @@
 package school.redrover;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FooterTest extends BaseTest {
 
@@ -27,6 +32,27 @@ public class FooterTest extends BaseTest {
                 getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")));
         Assert.assertEquals(getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")).getText(),
                 "Jenkins 2.414.2");
+    }
+
+    @Test
+    //https://trello.com/c/OOkxIvHX/402-tc1200107-footer-jenkins-version-check-the-tippy-box
+    public void checkTippyBox() throws InterruptedException
+    {
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);",
+                getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")));
+
+        List<String> expectedMenu = List.of("About Jenkins", "Get involved", "Website");
+        List<String> actualMenu = new ArrayList<>();
+
+        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")).click();
+        Thread.sleep(1000);
+        List<WebElement> tippyBoxElements = getDriver().findElements(By.xpath("//div[@class = 'tippy-content']/a"));
+
+        for (WebElement tippyContectMenu : tippyBoxElements) {
+            actualMenu.add(tippyContectMenu.getText());
+        }
+
+        Assert.assertEquals(actualMenu, expectedMenu, "Tippy box context menu doesn't macth");
     }
 
     private void clickRestApi() {
