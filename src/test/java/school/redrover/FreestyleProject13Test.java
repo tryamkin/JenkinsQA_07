2015c0aebe8c69ce1aa2de1b003f041f5fd87449
Ekaterina Lifantsova <141231367+Lifantsova@users.noreply.github.com>
2023-11-01 20:40:34 +0400

    TC_01.001.09 | Refactoring (#1311)
    Co-authored-by: Lifantsova <lifancova@mail.ru>

--- src/test/java/school/redrover/FreestyleProject13Test.java
+++ src/test/java/school/redrover/FreestyleProject13Test.java
@@ -8,21 +8,19 @@ import school.redrover.runner.BaseTest;
 public class FreestyleProject13Test extends BaseTest {
 
     private final static String PROJECT_NAME = "FreestyleProject";
+    private final static String DESCRIPTION = "Add description";
 
-    private void createFreestyleProject () {
-
+    private void createFreestyleProject() {
         getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
-        getDriver().findElement(By.xpath("//input [@name='name']")).sendKeys(PROJECT_NAME);
+        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(PROJECT_NAME);
         getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
-        getDriver().findElement(By.xpath("//img[@id='jenkins-name-icon']")).click();
-
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
     }
 
     @Test
     public void testCreateFreestyleProject() {
-
         createFreestyleProject();
 
         getDriver().findElement(By.xpath("//a[@href='job/FreestyleProject/']")).click();
@@ -31,21 +29,21 @@ public class FreestyleProject13Test extends BaseTest {
                 "Project " + PROJECT_NAME);
     }
 
+    private void addDescription() {
+        getDriver().findElement(By.xpath("//a[@href='job/FreestyleProject/']")).click();
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys(DESCRIPTION);
+        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--primary ']")).click();
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+    }
+
     @Test
     public void testAddDescription() {
-
         createFreestyleProject();
+        addDescription();
 
         getDriver().findElement(By.xpath("//a[@href='job/FreestyleProject/']")).click();
-        getDriver().findElement(By.xpath("//a[@id='description-link']")).click();
-        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys("Add description");
-        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--primary ']")).click();
 
-        getDriver().findElement(By.xpath("//img[@id='jenkins-name-icon']")).click();
-        getDriver().findElement(By.xpath("//a[@href='job/FreestyleProject/']")).click();
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id='description']/div[1]")).getText(),
-                "Add description");
-
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id='description']/div[1]")).getText(), DESCRIPTION);
     }
 }
