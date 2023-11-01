af0e0425600e59b8983b5b64266cb67c3a52b614
valeriia-vasylyshyna <110113695+valeriia-vasylyshyna@users.noreply.github.com>
2023-11-01 18:43:52 +0200

    TC_10.002.17 | Views > Edit view > Verify the view can be renamed (#1328)

--- src/test/java/school/redrover/View2Test.java
+++ src/test/java/school/redrover/View2Test.java
@@ -16,21 +16,41 @@ public class View2Test extends BaseTest {
         getDriver().findElement(By.xpath("//*[@id='bottom-sticker']/div/button[1]")).click();
         getDriver().findElement(By.id("jenkins-name-icon")).click();
     }
+
+    private void createNewView (String myViewName) {
+        getDriver().findElement(By.className("addTab")).click();
+        getDriver().findElement(By.id("name")).sendKeys(myViewName);
+        getDriver().findElement(By.xpath("//*[@id='createItemForm']/div[1]/div[2]/fieldset/div[2]/label")).click();
+        getDriver().findElement(By.name("Submit")).click();
+    }
+
     @Test
     public void testCreateNewView () {
         final String myViewName = "testView";
 
         createFreestyleProject("testProject");
+        createNewView(myViewName);
 
-        getDriver().findElement(By.className("addTab")).click();
-        getDriver().findElement(By.id("name")).sendKeys(myViewName);
-        getDriver().findElement(By.xpath("//*[@id=\"createItemForm\"]/div[1]/div[2]/fieldset/div[2]/label")).click();
-        getDriver().findElement(By.name("Submit")).click();
-
-        WebElement getViewName = getDriver().findElement(By.xpath("//a[@href='/view/testView/']"));
+        WebElement getViewName = getDriver().findElement(By.xpath("//a[@href='/view/"+ myViewName +"/']"));
 
         Assert.assertEquals(getViewName.getText(), myViewName);
-
     }
 
+    @Test
+    public void testRenameView () {
+        final String newViewName = "renamedView";
+        final String myViewName = "testView";
+
+        createFreestyleProject("testProject");
+        createNewView(myViewName);
+
+        getDriver().findElement(By.xpath("//*[@id='tasks']/div[4]/span/a")).click();
+        getDriver().findElement(By.name("name")).clear();
+        getDriver().findElement(By.name("name")).sendKeys(newViewName);
+        getDriver().findElement(By.name("Submit")).click();
+
+        WebElement getNewViewName = getDriver().findElement(By.xpath("//a[@href='/view/"+ newViewName +"/']"));
+
+        Assert.assertEquals(getNewViewName.getText(), newViewName);
+    }
 }
