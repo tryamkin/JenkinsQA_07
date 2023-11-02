a91cdf4db969fc4c9e0e00a0b9a2036b1df58e95
Marina Avr <118004084+MarinaAvr@users.noreply.github.com>
2023-11-02 06:46:26 -0500

    TC_09.001.05 | Manage Jenkins>Create user> The created user is displayed in the users table (#1369)

--- src/test/java/school/redrover/NewUser2Test.java
+++ src/test/java/school/redrover/NewUser2Test.java
@@ -9,6 +9,9 @@ import school.redrover.runner.BaseTest;
 import java.util.List;
 
 public class NewUser2Test extends BaseTest {
+
+    private static final String TEST_INPUT = "Test";
+
     @Test
     public void testEmptyFields() {
         getDriver().findElement(By.xpath("//a[@href='/manage']")).click();
@@ -19,6 +22,35 @@ public class NewUser2Test extends BaseTest {
         List<WebElement> error = getDriver().findElements(By.cssSelector(".error"));
 
         Assert.assertEquals(error.size(), 5);
+    }
+
+    @Test
+    public void testUserIsDisplayedInUsersTable() {
+        createUserSuccess();
+        WebElement createdUser = getDriver().findElement(By.xpath("//tbody/tr[2]/td[2]/a[1]"));
+
+        Assert.assertTrue(createdUser.isDisplayed());
+        Assert.assertEquals(createdUser.getText(), TEST_INPUT);
+    }
+
+    private void goToUserCreateFormPage() {
+        getDriver().findElement(By.xpath("//a[@href='/manage']")).click();
+        getDriver().findElement(By.xpath("//dt[text() = 'Users']")).click();
+        getDriver().findElement(By.xpath("//*[@href='addUser']")).click();
+    }
+
+    private void createUserSuccess() {
+        goToUserCreateFormPage();
+        List<WebElement> valueInputs = getDriver().findElements(
+            By.xpath("//*[@class = 'jenkins-input']"));
+        for (int i = 0; i < valueInputs.size(); i++) {
+            if (i == 0) {
+                valueInputs.get(i).sendKeys(TEST_INPUT);
+            } else {
+                valueInputs.get(i).sendKeys(TEST_INPUT + "@" + TEST_INPUT + ".com");
+            }
+        }
+        getDriver().findElement(By.name("Submit")).click();
     }
 }
 
