8258e1c5713630cdfa4bece9d94f17cae7654c23
Ju Dmitrieva <yulia.dmitrieva@yahoo.com>
2023-11-02 08:30:29 +0300

    TC_01.006.15 | Freestyle project > Configure General settings > Verify the set number of builds are kept (#1327)

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -5,12 +5,16 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import org.testng.annotations.Ignore;
 
+import java.time.Duration;
+import java.util.List;
 import java.util.UUID;
 
 import static org.testng.Assert.*;
@@ -50,6 +54,14 @@ public class FreestyleProjectTest extends BaseTest {
         getDriver().findElement(By.xpath("//textarea[@name = 'description']")).clear();
         getDriver().findElement(By.xpath("//textarea[@name = 'description']")).sendKeys(text);
         getDriver().findElement(By.xpath("//button[contains(text(),'Save')]")).click();
+    }
+
+    private void clickBuildNow() {
+        getDriver().findElement(By.xpath("//a[@class='task-link ' and contains(@href, 'build')]")).click();
+        new WebDriverWait(getDriver(), Duration.ofSeconds(3))
+                .until(ExpectedConditions.visibilityOfAllElements(
+                        getDriver().findElements(By.xpath("//a[@class='model-link inside build-link display-name']"))
+                ));
     }
 
     @Test
@@ -650,5 +662,28 @@ public class FreestyleProjectTest extends BaseTest {
         Assert.assertTrue(
                 getDriver().findElement(By.xpath("//label[contains(text(), 'This project is parameterized')]/../input"))
                         .isSelected());
+    }
+
+    @Test
+    public void testOldBuildsAreDiscarded() {
+
+        final int numOfBuildNowClicks = 2;
+
+        createFreeStyleProject(PROJECT_NAME);
+        getDriver().findElement(By.xpath("//label[text()='Discard old builds']")).click();
+        getDriver().findElement(By.name("_.numToKeepStr")).sendKeys(String.valueOf(numOfBuildNowClicks));
+        getDriver().findElement(By.name("Submit")).click();
+
+        for (int i = 0; i < numOfBuildNowClicks + 1; i++) {
+            clickBuildNow();
+        }
+
+        getDriver().navigate().refresh();
+
+        List<String> buildsList = getDriver().findElements(
+                        By.xpath("//a[@class='model-link inside build-link display-name']"))
+                .stream().map(WebElement::getText).toList();
+
+        Assert.assertEquals(buildsList.get(buildsList.size() - 1), "#2");
     }
 }
