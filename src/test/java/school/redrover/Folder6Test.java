b538b9c1c5b687b70ba9bc3486e9c7bee27fa329
EkaterinaRogozhnikova <134730100+ekaterinarogozhnikova@users.noreply.github.com>
2023-11-02 12:18:51 +0300

    TC_00.005.04 | Create Folder with an empty name > Checking warning me… (#1324)
    * TC_00.005.04 | Create Folder with an empty name > Checking warning message
    
    * TC_00.005.14 | Checking the warning message for an empty name field when creating a folder

--- src/test/java/school/redrover/Folder6Test.java
+++ src/test/java/school/redrover/Folder6Test.java
@@ -1,10 +1,15 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Folder6Test extends BaseTest {
 
@@ -14,7 +19,6 @@ public class Folder6Test extends BaseTest {
     private static final String NEW_VALID_NAME = "Folder2";
 
     private void utilsCreate(String folderName) {
-
         getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
         getDriver().findElement(By.xpath("//li[@class = 'com_cloudbees_hudson_plugins_folder_Folder']")).click();
         getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']")).sendKeys(folderName);
@@ -23,45 +27,26 @@ public class Folder6Test extends BaseTest {
     }
 
     private void utilsGoDashboard() {
-
         getDriver().findElement(By.xpath("//img[@alt = 'Jenkins']")).click();
     }
 
     private void utilsDelete(String folderName) {
-
         getDriver().findElement(By.xpath("//span[text() = '" + folderName + "']")).click();
         getDriver().findElement(By.xpath("//a[@href = '/job/" + folderName + "/delete']")).click();
         getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).click();
     }
 
     @Test
-    public void testCreate() {
-
+    public void testCreateEmptyName() {
         getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
         getDriver().findElement(By.xpath("//li[@class = 'com_cloudbees_hudson_plugins_folder_Folder']")).click();
 
-        WebElement INPUT_FIELD = getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']"));
-
-        INPUT_FIELD.sendKeys(EMPTY_NAME);
-        getDriver().findElement(By.xpath("//div[@id = 'itemname-required']"));
-
-        INPUT_FIELD.sendKeys(INVALID_NAME);
-        getDriver().findElement(By.xpath("//div[@id = 'itemname-invalid']"));
-        INPUT_FIELD.clear();
-
-        INPUT_FIELD.sendKeys(VALID_NAME);
-        getDriver().findElement(By.xpath("//button[@type = 'submit']")).click();
-
-        utilsGoDashboard();
-
-        Assert.assertEquals(getDriver()
-                        .findElement(By.xpath("//a[@class = 'jenkins-table__link model-link inside']"))
-                        .getText(), VALID_NAME);
+        getDriver().findElement(By.xpath("//input[@class = 'jenkins-input']")).sendKeys(EMPTY_NAME);
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id = 'itemname-required']")).getText(), "» This field cannot be empty, please enter a valid name");
     }
 
     @Test
     public void testRename() {
-
         utilsCreate(VALID_NAME);
 
         getDriver().findElement(By.xpath("//a[@href = '/job/" + VALID_NAME + "/confirm-rename']")).click();
@@ -72,8 +57,6 @@ public class Folder6Test extends BaseTest {
 
         utilsGoDashboard();
 
-        Assert.assertEquals(getDriver()
-                        .findElement(By.xpath("//a[@class = 'jenkins-table__link model-link inside']"))
-                        .getText(), NEW_VALID_NAME);
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@class = 'jenkins-table__link model-link inside']")).getText(), NEW_VALID_NAME);
     }
 }
