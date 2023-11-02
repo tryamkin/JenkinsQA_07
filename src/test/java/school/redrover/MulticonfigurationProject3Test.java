0c17c368292f435fab01862e4a4daee5d97e7e0c
Aleksandra Makeeva <106404779+Aleksandra-Makeeva@users.noreply.github.com>
2023-11-02 18:11:22 +0300

    rename const (#1394)

--- src/test/java/school/redrover/MulticonfigurationProject3Test.java
+++ src/test/java/school/redrover/MulticonfigurationProject3Test.java
@@ -7,17 +7,17 @@ import school.redrover.runner.BaseTest;
 
 public class MulticonfigurationProject3Test extends BaseTest {
 
-    final private String nameProject = "TestName";
+    final static private String NAME_PROJECT = "TestName";
 
     @Test
     public void testCreateMultiProject(){
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(nameProject);
+        getDriver().findElement(By.id("name")).sendKeys(NAME_PROJECT);
         getDriver().findElement(By.xpath("//li[@class='hudson_matrix_MatrixProject']")).click();
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
         getDriver().findElement(By.name("Submit")).click();
 
-        Assert.assertEquals(getDriver().findElement(By.tagName("h1")).getText(), "Project " + nameProject);
+        Assert.assertEquals(getDriver().findElement(By.tagName("h1")).getText(), "Project " + NAME_PROJECT);
 
     }
 }
