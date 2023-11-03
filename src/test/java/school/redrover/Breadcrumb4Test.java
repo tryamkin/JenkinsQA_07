3c9dda30656247f7fe550da2a070457d140be99f
katya-test <katyasuhr@gmail.com>
2023-11-03 13:22:41 -0400

    TC_11.001 | Breadcrumb Dashboard dropdown-menu

---
+++ src/test/java/school/redrover/Breadcrumb4Test.java
@@ -0,0 +1,47 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Breadcrumb4Test extends BaseTest {
+
+    @Test
+    public void testBreadcrumbDashboardMenuItemsSameAsSideMenuItems() {
+        new Actions(getDriver()).moveToElement(getDriver().findElement(By.xpath("//a[text()='Dashboard']"))).perform();
+
+        WebElement dashboardChevron = getDriver().findElement(By.xpath("//ol[@id='breadcrumbs']//a[text()='Dashboard']/button"));
+        new Actions(getDriver())
+                .moveToElement(dashboardChevron, 7, 7)
+                .pause(500)
+                .perform();
+        dashboardChevron.sendKeys(Keys.RETURN);
+
+        List<WebElement> itemsListBreadcrumb = getDriver().findElements(By.xpath("//div[@id='tippy-3']//a"));
+
+        Assert.assertTrue(itemsListBreadcrumb.size() > 0);
+
+        List<String> nameListBreadcrumb = new ArrayList<>();
+        for(WebElement element : itemsListBreadcrumb) {
+            nameListBreadcrumb.add(element.getText());
+        }
+
+        List<WebElement> itemsListSideMenu = getDriver().findElements(By.xpath("//div[@class='task ']"));
+
+        Assert.assertTrue(itemsListSideMenu.size() > 0);
+
+        List<String> nameListSideMenu = new ArrayList<>();
+        for(WebElement element : itemsListSideMenu) {
+            nameListSideMenu.add(element.getText());
+        }
+
+        Assert.assertEquals(nameListBreadcrumb, nameListSideMenu);
+    }
+}
