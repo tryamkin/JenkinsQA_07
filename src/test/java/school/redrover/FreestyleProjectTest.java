074eb7449fce36e93033ed8e78520b015e3e3aa2
SleznikEvgeniy <77625845+Yauheni95@users.noreply.github.com>
2023-11-03 08:31:11 -0400

    TC_01.002.02 | Verify the message “No name is specified” below New Name field when New Name field is clear (#1364)
    * TC_01.002.02 Verify the message “No name is specified” below New Name field, when New Name field is clear
    
    * TC_01.002.02 Verify the message “No name is specified” below New Name field, when New Name field is clear
    
    * TC_01.002.02 Verify the message “No name is specified” below New Name field, when New Name field is clear

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -2,7 +2,6 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -29,15 +28,18 @@ public class FreestyleProjectTest extends BaseTest {
     }
 
     private boolean isProjectExist(String projectName) {
+        goToJenkinsHomePage();
         return !getDriver().findElements(By.id("job_" + projectName)).isEmpty();
     }
 
     private void disableProjectByName(String projectName) {
+        goToJenkinsHomePage();
         getDriver().findElement(By.xpath("//span[contains(text(),'" + projectName + "')]")).click();
         getDriver().findElement(By.name("Submit")).click();
     }
 
     private void createFreeStyleProject(String projectName) {
+        goToJenkinsHomePage();
         getDriver().findElement(By.linkText("New Item")).click();
         getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
         getDriver().findElement(By.id("name")).sendKeys(projectName);
@@ -129,13 +131,28 @@ public class FreestyleProjectTest extends BaseTest {
 
         getDriver().findElement(By.xpath("//span[contains(text(),'" + initialProjectName + "')]")).click();
         getDriver().findElement(By.xpath("//a[contains(@href,'rename')]")).click();
-        getDriver().findElement(By.name("newName")).sendKeys(Keys.CONTROL + "a");
+        getDriver().findElement(By.name("newName")).clear();
         getDriver().findElement(By.name("newName")).sendKeys(newProjectName);
         getDriver().findElement(By.name("Submit")).click();
         goToJenkinsHomePage();
 
         assertTrue(isProjectExist(newProjectName));
         assertFalse(isProjectExist(initialProjectName));
+    }
+
+    @Test
+    public void testErrorMessageWhenNewNameFieldEmpty() {
+        final String initialProjectName = "Test Project";
+        createFreeStyleProject(initialProjectName);
+        goToJenkinsHomePage();
+
+        getDriver().findElement(By.xpath("//span[contains(text(),'" + initialProjectName + "')]")).click();
+        getDriver().findElement(By.xpath("//a[contains(@href,'rename')]")).click();
+        getDriver().findElement(By.name("newName")).clear();
+        getDriver().findElement(By.name("newName")).sendKeys(Keys.TAB);
+
+        String errorMessage = getDriver().findElement(By.className("error")).getText();
+        assertEquals(errorMessage, "No name is specified");
     }
 
     @Test
@@ -293,7 +310,6 @@ public class FreestyleProjectTest extends BaseTest {
     public void testEnableProjectFromStatusPage() {
         final String projectName = "Test Project";
         createFreeStyleProject(projectName);
-        goToJenkinsHomePage();
         disableProjectByName(projectName);
 
         getDriver().findElement(By.name("Submit")).click();
@@ -309,10 +325,9 @@ public class FreestyleProjectTest extends BaseTest {
     public void testEnableProjectFromConfigurePage() {
         final String projectName = "Test Project";
         createFreeStyleProject(projectName);
-        goToJenkinsHomePage();
         disableProjectByName(projectName);
-        getDriver().findElement(By.linkText("Configure")).click();
 
+        getDriver().findElement(By.linkText("Configure")).click();
         getDriver().findElement(By.className("jenkins-toggle-switch__label")).click();
         getDriver().findElement(By.name("Submit")).click();
 
@@ -327,7 +342,6 @@ public class FreestyleProjectTest extends BaseTest {
     public void testWarningMessageOnStatusPageWhenDisabled() {
         final String projectName = "Test Project";
         createFreeStyleProject(projectName);
-        goToJenkinsHomePage();
         disableProjectByName(projectName);
 
         boolean isDisabled = getDriver()
@@ -341,7 +355,6 @@ public class FreestyleProjectTest extends BaseTest {
     public void testEnableButtonOnStatusPageWhenDisabled() {
         final String projectName = "Test Project";
         createFreeStyleProject(projectName);
-        goToJenkinsHomePage();
         disableProjectByName(projectName);
 
         boolean isVisible = getDriver().findElement(By.name("Submit")).isDisplayed();
@@ -354,7 +367,6 @@ public class FreestyleProjectTest extends BaseTest {
     public void testStatusDisabledOnDashboardWhenDisabled() {
         final String projectName = "Test Project";
         createFreeStyleProject(projectName);
-        goToJenkinsHomePage();
         disableProjectByName(projectName);
         goToJenkinsHomePage();
 
@@ -369,16 +381,11 @@ public class FreestyleProjectTest extends BaseTest {
     public void testScheduleBuildButtonOnDashboardWhenDisabled() {
         final String projectName = "Test Project";
         createFreeStyleProject(projectName);
-        goToJenkinsHomePage();
         disableProjectByName(projectName);
         goToJenkinsHomePage();
 
-        try {
-            getDriver().findElement(By.xpath("//*[@id='job_" + projectName + "']//*[@class='jenkins-table__cell--tight']//a"));
-        } catch (NoSuchElementException e) {
-            return;
-        }
-        Assert.fail();
+        boolean isDisabled = getDriver().findElements(By.xpath("//*[@id='job_" + projectName + "']//*[@class='jenkins-table__cell--tight']//a")).isEmpty();
+        assertTrue(isDisabled);
     }
 
     @Test
