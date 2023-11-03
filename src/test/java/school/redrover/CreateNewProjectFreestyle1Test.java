8a419f86ae7f8710aed988b77518e9be638d8f63
ptchela <80599907+ptchela@users.noreply.github.com>
2023-11-03 12:53:49 +0100

    TC_00.001.31 (#1433)
    * TC_000.001.031
    
    * TC_000.001.031
    
    * TC_00.001.31
    
    * TC_00.001.31
    
    * TC_00.001.31
    
    * TC_00.001.31

---
+++ src/test/java/school/redrover/CreateNewProjectFreestyle1Test.java
@@ -0,0 +1,28 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class CreateNewProjectFreestyle1Test extends BaseTest {
+
+    @Test
+        public void testCreate() {
+        final String projectName = "FreeStyleProjectName";
+
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(projectName);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.xpath("//button[@id = 'ok-button']")).click();
+        getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+
+        getDriver().findElement(By.xpath("//td/a[@href = 'job/" + projectName + "/']")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.cssSelector("#main-panel > h1")).getText(),
+                "Project " + projectName);
+    }
+
+}
