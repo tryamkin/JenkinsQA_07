d8c6fbf4202ee85a8773997095e0c1f8600a2e9b
OlgaBrest <147086182+OlgaBrest@users.noreply.github.com>
2023-11-04 16:36:36 +0300

    Tc 02.005.09 pipeline delete2 (#1445)
    * TC_02.005.09-Pipeline-Delete2
    
    * TC_02.005.09-Pipeline-Delete2
    
    * TC_02.005.09-Pipeline-Delete2

--- src/test/java/school/redrover/Pipeline10Test.java
+++ src/test/java/school/redrover/Pipeline10Test.java
@@ -22,4 +22,24 @@ public class Pipeline10Test extends BaseTest {
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Pipeline " + pipelineName);
     }
+    @Test
+    public void testDeleteMyPipeline2() {
+        final String jobName = "MyPipeline2";
+
+        getDriver().findElement(By.xpath("//a[@href= '/view/all/newJob']")).click();
+
+        getDriver().findElement(By.id("name")).sendKeys(jobName);
+        getDriver().findElement(By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        getDriver().findElement(By.name("Submit")).click();
+
+        getDriver().findElement(By.xpath("//li[@class = 'jenkins-breadcrumbs__list-item']/a[@href ='/']")).click();
+
+        getDriver().findElement(By.xpath("//td/a[@href ='job/" + jobName + "/']")).click();
+        getDriver().findElement(By.xpath("//*[@id='tasks']/div[5]/span/a")).click();
+        getDriver().switchTo().alert().accept();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Welcome to Jenkins!");
+    }
 }
