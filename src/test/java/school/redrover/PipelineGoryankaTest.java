669bc2cbe7848d283600682435507ae2653ae6b3
goryanka <All4us85@yahoo.com>
2023-11-04 16:50:11 -0400

    TC_02.005.06 | Pipeline> Create project

--- src/test/java/school/redrover/PipelineGoryankaTest.java
+++ src/test/java/school/redrover/PipelineGoryankaTest.java
@@ -20,7 +20,6 @@ public class PipelineGoryankaTest extends BaseTest {
         getDriver().findElement(By.xpath("//li[@class= 'jenkins-breadcrumbs__list-item']")).click();
     }
 
-
     @Test
     public void testCreateJob() {
         final String jobName = "Pipeline_1";
@@ -32,21 +31,5 @@ public class PipelineGoryankaTest extends BaseTest {
         String actual = getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).getText();
         Assert.assertEquals(actual, jobName);
 
-    }
-
-    @Test
-    public void testDelete() {
-        final String jobName = "myJob";
-
-        createApiPipeline(jobName);
-        goToDashBoard();
-
-        getDriver().findElement(By.xpath("//span[normalize-space()='" + jobName + "']")).click();
-
-        getDriver().findElement(By.xpath("//button[@href = '/job/myJob/doDelete']")).click();
-
-        getDriver().switchTo().alert().accept();
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[normalize-space()='Welcome to Jenkins!']")).getText(), "Welcome to Jenkins!");
     }
 }
