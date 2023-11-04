bf0b03dacab9c005ae4e1da9aca446c2e2b0d551
SvetlanaSolyris <146766667+SvetlanaSolyris@users.noreply.github.com>
2023-11-04 23:52:03 +0300

    TC_09.001.02 | Create User with empty Name (#1461)
    * TC_09.001.02 | Create User with empty Name
    
    * TC_09.001.02 | Create User with empty Name

--- src/test/java/school/redrover/CreateUserTest.java
+++ src/test/java/school/redrover/CreateUserTest.java
@@ -15,7 +15,6 @@ public class CreateUserTest extends BaseTest {
 
     @Test
     public void testCreateUserValidData() {
-
         goToUsersTab();
 
         getDriver().findElement(By.id("username")).sendKeys("USER_NAME_Test");
@@ -28,5 +27,20 @@ public class CreateUserTest extends BaseTest {
 
         Assert.assertEquals(getDriver().findElement(
                 By.xpath("//a[@href='user/user_name_test/']")).getText(), "USER_NAME_Test");
+    }
+
+    @Test
+    public void testCreateUserEmptyName(){
+        goToUsersTab();
+
+        getDriver().findElement(By.xpath("//input[@name='password1']")).sendKeys("Test_Test");
+        getDriver().findElement(By.xpath("//input[@name='password2']")).sendKeys("Test_Test");
+        getDriver().findElement(By.xpath("//input[@name='fullname']")).sendKeys("TestName");
+        getDriver().findElement(By.xpath("//input[@name='email']")).sendKeys("Test@mail.ru");
+
+        getDriver().findElement(By.xpath("//div[@id='bottom-sticker']/div/button")).click();
+
+        Assert.assertEquals(getDriver().findElement(
+                By.xpath("//div[@id='main-panel']/form/div[1]/div[2]")).getText(), "\"\" is prohibited as a username for security reasons.");
     }
 }
