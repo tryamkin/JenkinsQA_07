1f33a8f95acd52a5beba5ffe84aa2ac7a6a050f2
Marianna <105119046+MariannaLiss@users.noreply.github.com>
2023-11-04 21:24:10 +0300

    TC_12.002.04 | Footer > REST API link > Visibility and clickability (#1457)
    * TC_12.002.04

---
+++ src/test/java/school/redrover/RestApiLink3Test.java
@@ -0,0 +1,19 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+public class RestApiLink3Test extends BaseTest {
+    @Test
+    public void testVerifyRestApiLink() {
+        final String apiLink = "REST API";
+
+        WebElement restApiLink = getDriver().findElement(By.xpath("//a[contains(@href,'api')]"));
+        restApiLink.click();
+        WebElement restApiTittle = getDriver().findElement(By.xpath("//h1[1]"));
+
+        Assert.assertEquals(restApiTittle.getText(), apiLink);
+    }
+}
