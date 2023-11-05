86b2502ea281662e7c3893ed0b3bbe683a3ff04f
Turii <44880961+Turii@users.noreply.github.com>
2023-11-05 07:09:19 -0800

    TC_12.003.2 | Footer > "Jenkins 2.414.2" button > Visibility, clickability and functionality > Verify links when click Jenkins version button (#1472)

--- src/test/java/school/redrover/Footer3Test.java
+++ src/test/java/school/redrover/Footer3Test.java
@@ -13,9 +13,73 @@ public class Footer3Test extends BaseTest {
         return getDriver().findElement(By.xpath("//button[@type = 'button']"));
     }
 
+    public void clickDropdownItemJenkinsVersion(String itemText) {
+        jenkinsVersionButton().click();
+        WebElement dropdownItem = getDriver().findElement(By.xpath("//a[contains(text(),'" + itemText + "')]"));
+        dropdownItem.click();
+    }
+
+    public void clickDropdownItemJenkinsVersionButton(String dropdownItem) {
+        String startWindow = getDriver().getWindowHandle();
+
+        clickDropdownItemJenkinsVersion(dropdownItem);
+
+        for (String windowHandle : getDriver().getWindowHandles()) {
+            if (!startWindow.contentEquals(windowHandle)) {
+                getDriver().switchTo().window(windowHandle);
+                break;
+            }
+        }
+    }
+
     @Test
     public void testJenkinsVersion() {
 
         Assert.assertEquals(jenkinsVersionButton().getText(), jenkinsExpectedVersion);
+    }
+
+    @Test
+    public void testClickAboutJenkins() {
+        String expectedPageName = "Jenkins";
+        String expectedPageTitle = "About Jenkins 2.414.2 [Jenkins]";
+        String expectedItem = "About Jenkins";
+
+        clickDropdownItemJenkinsVersionButton(expectedItem);
+
+        String actualPageName = getDriver().findElement(By.tagName("h1")).getText();
+        String actualPageTitle = getDriver().getTitle();
+
+        Assert.assertEquals(actualPageName, expectedPageName, "The page name is not Jenkins");
+        Assert.assertEquals(actualPageTitle, expectedPageTitle, "The title is not About Jenkins 2.414.2 [Jenkins]");
+    }
+
+    @Test
+    public void testClickGetInvolved() {
+        String expectedPageName = "Participate and Contribute";
+        String expectedPageTitle = "Participate and Contribute";
+        String expectedItem = "Get involved";
+
+        clickDropdownItemJenkinsVersionButton(expectedItem);
+
+        String actualPageName = getDriver().findElement(By.tagName("h1")).getText();
+        String actualPageTitle = getDriver().getTitle();
+
+        Assert.assertEquals(actualPageName, expectedPageName, "The page name is not Participate and Contribute");
+        Assert.assertEquals(actualPageTitle, expectedPageTitle, "The title is not Participate and Contribute");
+    }
+
+    @Test
+    public void testClickWebsite() {
+        String expectedPageName = "Jenkins";
+        String expectedPageTitle = "Jenkins";
+        String expectedItem = "Website";
+
+        clickDropdownItemJenkinsVersionButton(expectedItem);
+
+        String actualPageName = getDriver().findElement(By.tagName("h1")).getText();
+        String actualPageTitle = getDriver().getTitle();
+
+        Assert.assertEquals(actualPageName, expectedPageName, "The page name is not Jenkins");
+        Assert.assertEquals(actualPageTitle, expectedPageTitle, "The title is not Jenkins");
     }
 }
