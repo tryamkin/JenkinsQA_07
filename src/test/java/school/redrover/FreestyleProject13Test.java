303eee10617624c4b8d3f334a2ba0a6a11634c69
Ekaterina Lifantsova <141231367+Lifantsova@users.noreply.github.com>
2023-11-05 19:13:34 +0400

    TC_00.007.15 | Create folder (#1478)
    Co-authored-by: Lifantsova <lifancova@mail.ru>

--- src/test/java/school/redrover/FreestyleProject13Test.java
+++ src/test/java/school/redrover/FreestyleProject13Test.java
@@ -9,6 +9,7 @@ public class FreestyleProject13Test extends BaseTest {
 
     private final static String PROJECT_NAME = "FreestyleProject";
     private final static String DESCRIPTION = "Add description";
+    private final static String NEW_FOLDER = "New folder";
 
     private void createFreestyleProject() {
         getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
@@ -61,5 +62,24 @@ public class FreestyleProject13Test extends BaseTest {
         getDriver().findElement(By.xpath("//a[@href='job/FreestyleProject/']")).click();
         Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id='description']/div[1]")).getText(),
                 "Edit description " + DESCRIPTION);
+    }
+
+    private void createFolder() {
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(NEW_FOLDER);
+        getDriver().findElement(By.id("j-add-item-type-nested-projects")).click();
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+    }
+
+    @Test
+    public void testCreateFolder() {
+        createFolder();
+
+        getDriver().findElement(By.xpath("//a[@href='job/New%20folder/']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id='main-panel']/h1")).getText(),
+                NEW_FOLDER);
     }
 }
