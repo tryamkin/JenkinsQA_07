d560869a20f2de5298da0478731bfcb6ac788dab
Bondarenko-I-M <130083850+Bondarenko-I-M@users.noreply.github.com>
2023-11-06 15:30:10 +0200

    TC_04.003.07 | Folder > Create a new job inside a folder (#1503)

--- src/test/java/school/redrover/Folder15Test.java
+++ src/test/java/school/redrover/Folder15Test.java
@@ -6,19 +6,45 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class Folder15Test extends BaseTest {
-    private static final String NEW_FOLDER_NAME = "WorkFolder";
-    private void createNewFolder() {
+    private void createNewFolder(String folderName) {
+
         getDriver().findElement(By.xpath("//a [@href = '/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(NEW_FOLDER_NAME);
+        getDriver().findElement(By.id("name")).sendKeys(folderName);
         getDriver().findElement(By.xpath("//img[@class = 'icon-folder icon-xlg']")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).click();
         getDriver().findElement(By.id("jenkins-home-link")).click();
     }
+    private void createFreestPro(String nameFreestyleProject) {
+
+        getDriver().findElement(By.id("name")).sendKeys(nameFreestyleProject);
+        getDriver().findElement(By.xpath("//img[@class='icon-freestyle-project icon-xlg']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).click();
+
+    }
+
     @Test
     public void testCreateView() {
-        createNewFolder();
+        createNewFolder("WorkFolder");
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//span[contains(text(), 'WorkFolder')]")).getText(), "WorkFolder");
+    }
+    @Test
+    public void testCreateNewJob() {
+        createNewFolder("WorkFolder123");
+
+        getDriver().findElement(By.xpath("//span[contains(text(), 'WorkFolder123')]")).click();
+        getDriver().findElement(By.xpath("//span[contains(text(), 'Create a job')]")).click();
+
+        createFreestPro("MyFreestyleProject");
+
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+        getDriver().findElement(By.xpath("//span[contains (text(), 'WorkFolder123')]")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//span[contains(text(), 'MyFreestyleProject')]")).getText(),"MyFreestyleProject");
+
+
+
     }
 }
