5d393cd4f453f3cb01e8edafcc6b68805a9c8f07
SleznikEvgeniy <77625845+Yauheni95@users.noreply.github.com>
2023-11-06 11:41:59 -0500

    TC_00.004.02| New item > Сreate a new item from existing job > Verify the 'Create New Item from Existing Job' section is NOT displayed on "New Item" page when there is no any item created (#1510)

--- src/test/java/school/redrover/NewItemTest.java
+++ src/test/java/school/redrover/NewItemTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class NewItemTest extends BaseTest {
@@ -20,14 +21,26 @@ public class NewItemTest extends BaseTest {
         getDriver().findElement(By.id("ok-button")).click();
     }
 
+    private boolean isCloneItemSectionDisplayed() {
+        return !getDriver().findElements(By.className("item-copy")).isEmpty();
+    }
+
     @Test
-    public void testNewItemFromExistedJobFieldIsDisplayedWhenItemCreated(){
+    public void testNewItemFromExistedJobSectionIsDisplayedWhenItemCreated() {
         final String projectName = "Test Project";
         createFreeStyleProject(projectName);
         goToJenkinsHomePage();
         getDriver().findElement(By.linkText("New Item")).click();
 
-        boolean isDisplayed = !getDriver().findElements(By.className("item-copy")).isEmpty();
-        assertTrue(isDisplayed);
+        assertTrue(isCloneItemSectionDisplayed());
     }
+
+    @Test
+    public void testNewItemFromExistedJobSectionIsNotDisplayedWhenNoItemsCreated() {
+        goToJenkinsHomePage();
+        getDriver().findElement(By.linkText("New Item")).click();
+
+        assertFalse(isCloneItemSectionDisplayed());
+    }
+
 }
