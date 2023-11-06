622a2eff21114c661902ddecb00e94dab304dc3e
Marianna <105119046+MariannaLiss@users.noreply.github.com>
2023-11-06 22:09:53 +0300

    RF 00.003.01, 03.001.01 | Creating new item > Creating Multiconfiguration project > Add description > Edit description (#1514)
    * RF_00.003.01, RF_03.001.01

--- src/test/java/school/redrover/MulticonfigurationProject4Test.java
+++ src/test/java/school/redrover/MulticonfigurationProject4Test.java
@@ -7,36 +7,21 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class MulticonfigurationProject4Test extends BaseTest {
-    private static final By NEW_ITEM_LINK = By.linkText("New Item");
-    private static final By ITEM_NAME_INPUT = By.id("name");
-    private static final By MULTICONFIGURATION_PROJECT_TAB = By.xpath("//span[text()='Multi-configuration project']");
-    private static final By OK_BUTTON = By.cssSelector("#ok-button");
-    private static final By SAVE_BUTTON = By.name("Submit");
-    private static final By JENKINS_HOME_LINK = By.xpath("//a[@id='jenkins-home-link']");
-    private static final By PROJECT_TITTLE = By.cssSelector(".matrix-project-headline.page-headline");
     private static final By ADD_DESCRIPTION_LINK = By.xpath("//a[@id='description-link']");
     private static final By DESCRIPTION_INPUT = By.xpath("//textarea[@name='description']");
-    private static final By SAVE_DESCRIPTION_BUTTON = By.xpath("//button[@class='jenkins-button jenkins-button--primary ']");
-    private static final By DESCRIPTION = By.xpath("//div[@id='description']/div[1]");
-    private static final By EDIT_DESCRIPTION_BUTTON = By.id("description-link");
-    private static final By DESCRIPTION_TEXT = By.tagName("textarea");
+    private static final By SAVE_DESCRIPTION_BUTTON = By.xpath(
+            "//button[@class='jenkins-button jenkins-button--primary ']");
 
     private void createMulticonfigurationProject() {
         final String projectName = "MulticonfigurationProject";
 
-        WebElement newItemLink = getDriver().findElement(NEW_ITEM_LINK);
-        newItemLink.click();
-        WebElement itemNameInput = getDriver().findElement(ITEM_NAME_INPUT);
-        itemNameInput.click();
-        itemNameInput.sendKeys(projectName);
-        WebElement multiconfigurationProject = getDriver().findElement(MULTICONFIGURATION_PROJECT_TAB);
-        multiconfigurationProject.click();
-        WebElement okButton = getDriver().findElement(OK_BUTTON);
-        okButton.click();
-        WebElement saveButton = getDriver().findElement(SAVE_BUTTON);
-        saveButton.click();
-        WebElement jenkinsHomeLink = getDriver().findElement(JENKINS_HOME_LINK);
-        jenkinsHomeLink.click();
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).click();
+        getDriver().findElement(By.id("name")).sendKeys(projectName);
+        getDriver().findElement(By.xpath("//span[text()='Multi-configuration project']")).click();
+        getDriver().findElement(By.cssSelector("#ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+        getDriver().findElement(By.xpath("//a[@id='jenkins-home-link']")).click();
     }
 
     @Test
@@ -47,7 +32,8 @@ public class MulticonfigurationProject4Test extends BaseTest {
         WebElement nameProject = getDriver().findElement(By.xpath("//td/a[@href='job/" + projectName + "/']"));
         nameProject.click();
 
-        Assert.assertEquals(getDriver().findElement(PROJECT_TITTLE).getText(), String.format("Project %s", projectName));
+        Assert.assertEquals(getDriver().findElement(By.cssSelector(".matrix-project-headline.page-headline")).getText(),
+                String.format("Project %s", projectName));
     }
 
     @Test
@@ -63,7 +49,8 @@ public class MulticonfigurationProject4Test extends BaseTest {
         WebElement saveDescription = getDriver().findElement(SAVE_DESCRIPTION_BUTTON);
         saveDescription.click();
 
-        Assert.assertEquals(getDriver().findElement(DESCRIPTION).getText(), desriptionText);
+        Assert.assertEquals(getDriver().findElement(By.xpath(
+                "//div[@id='description']/div[1]")).getText(), desriptionText);
     }
 
     @Test
@@ -79,13 +66,12 @@ public class MulticonfigurationProject4Test extends BaseTest {
         descriptionInput.sendKeys(desriptionText);
         WebElement saveDescription = getDriver().findElement(SAVE_DESCRIPTION_BUTTON);
         saveDescription.click();
-        WebElement editDescription = getDriver().findElement(EDIT_DESCRIPTION_BUTTON);
-        editDescription.click();
-        WebElement descriptionText = getDriver().findElement(DESCRIPTION_TEXT);
-        descriptionText.clear();
-        descriptionText.sendKeys(editDescriptionText);
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.tagName("textarea")).clear();
+        getDriver().findElement(By.tagName("textarea")).sendKeys(editDescriptionText);
         getDriver().findElement(SAVE_DESCRIPTION_BUTTON).click();
 
-        Assert.assertEquals(getDriver().findElement(DESCRIPTION).getText(), editDescriptionText);
+        Assert.assertEquals(getDriver().findElement(By.xpath(
+                "//div[@id='description']/div[1]")).getText(), editDescriptionText);
     }
 }
