32745c631cbacc3e132f9f6671aaf713b8847b78
SerhiiHaponiuk <92944600+SerhiiHaponiuk@users.noreply.github.com>
2023-11-06 16:21:48 +0200

    TC_01.001.17 | Freestyle project > Add description (#1500)

--- src/test/java/school/redrover/FreestyleProject17Test.java
+++ src/test/java/school/redrover/FreestyleProject17Test.java
@@ -6,10 +6,9 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class FreestyleProject17Test extends BaseTest {
+    final String freestyleProject = "Freestyle Project";
 
-    @Test
-    public void testCreateFreestyleProject() {
-        final String freestyleProject = "Freestyle Project";
+    public void createFreestyleProject(){
 
         getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
 
@@ -18,8 +17,41 @@ public class FreestyleProject17Test extends BaseTest {
         getDriver().findElement(By.xpath("//button[@id = 'ok-button']")).click();
 
         getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
+    }
+
+    @Test
+    public void testCreateFreestyleProject() {
+
+        createFreestyleProject();
 
         String nameOfProject = getDriver().findElement(By.xpath("//h1")).getText();
         Assert.assertTrue(nameOfProject.contains(freestyleProject));
+    }
+
+    @Test
+    public void testAddDescription() {
+        final String emptyDescription = "";
+
+        createFreestyleProject();
+
+        getDriver().findElement(By.xpath("//a[text() = 'Dashboard']")).click();
+
+        getDriver().findElement(By.xpath("//span[text() = 'Freestyle Project']")).click();
+
+        getDriver().findElement(By.xpath("//a[@id = 'description-link']")).click();
+        getDriver().findElement(By.name("description")).sendKeys("Description for freestyle project");
+        getDriver().findElement(By.name("Submit")).click();
+
+        getDriver().findElement(By.xpath("//a[@id = 'description-link']")).click();
+        getDriver().findElement(By.name("description")).clear();
+        getDriver().findElement(By.name("description")).sendKeys("Edited description");
+        getDriver().findElement(By.name("Submit")).click();
+
+        getDriver().findElement(By.xpath("//a[@id = 'description-link']")).click();
+        getDriver().findElement(By.name("description")).clear();
+        getDriver().findElement(By.name("Submit")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id = 'description']/div[1]"))
+                .getText(), emptyDescription);
     }
 }
