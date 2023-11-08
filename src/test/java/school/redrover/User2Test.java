1c90f60afa1414d7589e5c6912cbbfa277f0d747
Bezverkhova <60390586+Bezverkhova@users.noreply.github.com>
2023-11-08 06:40:29 -0600

    TC_13.005.03 Delete user, User can't log in (#1549)

--- src/test/java/school/redrover/User2Test.java
+++ src/test/java/school/redrover/User2Test.java
@@ -1,5 +1,6 @@
 package school.redrover;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -37,5 +38,32 @@ public class User2Test extends BaseTest {
 
         String userIconText = getDriver().findElement(By.xpath("//a[contains(@href,'user')]")).getText();
         assertEquals(userIconText, userName);
+    }
+
+    @Test
+    public void testDeleteUserAndLogIn() {
+        final String userName = "NewUser";
+        final String password = "te5t";
+        final String email = "test_redrov@yahoo.com";
+
+        createUser(userName, password, email);
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+
+        getDriver().findElement(By.xpath("//a[@href='/asynchPeople/']")).click();
+
+        getDriver().findElement(By.xpath(String.format("//a[@href='/user/%s/']",userName.toLowerCase()))).click();
+
+        getDriver().findElement(By.xpath("//span[contains(text(),'Delete')]")).click();
+        Alert alert = getDriver().switchTo().alert();
+        alert.accept();
+
+        getDriver().findElement(By.xpath("//span[contains(text(),'log out')]")).click();
+
+        getDriver().findElement(By.name("j_username")).sendKeys(userName);
+        getDriver().findElement(By.name("j_password")).sendKeys(password);
+        getDriver().findElement(By.name("Submit")).click();
+
+        String errorText = getDriver().findElement(By.className("app-sign-in-register__error")).getText();
+        assertEquals(errorText, "Invalid username or password");
     }
 }
