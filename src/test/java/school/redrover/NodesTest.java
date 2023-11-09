4b641326a1232e4f6a4eb53aaea0e7256585a684
alma227 <happleness227@gmail.com>
2023-11-09 17:21:01 +0600

    fix

--- src/test/java/school/redrover/NodesTest.java
+++ src/test/java/school/redrover/NodesTest.java
@@ -3,6 +3,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
