7db1b2f2674124f877408a8cc55f83c89d9029bf
MartaShepel <135738827+MartaShepel@users.noreply.github.com>
2023-11-11 20:05:36 +0100

    refactoring 00.007.11 (#1625)

--- src/test/java/school/redrover/OrganizationFolder9Test.java
+++ src/test/java/school/redrover/OrganizationFolder9Test.java
@@ -78,6 +78,10 @@ public class OrganizationFolder9Test extends BaseTest {
     @Test
     public void testCreateOrganizationFolderWithValidNameNew () {
         createFolder();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//tr[@id='job_" + folderName + "']//td[3]")).getText(),
+                folderName);
     }
 
     @Test
