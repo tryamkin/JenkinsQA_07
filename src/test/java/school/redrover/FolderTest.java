45ae2da4a612793d3f732ec3cfb0d8c4f274ba48
Darya <darya.slinkova@gmail.com>
2023-11-12 00:32:48 +0300

    delete imports

--- src/test/java/school/redrover/FolderTest.java
+++ src/test/java/school/redrover/FolderTest.java
@@ -3,13 +3,10 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
-import java.time.Duration;
 
 import static org.testng.AssertJUnit.assertEquals;
 
