459ac98ad53afbe7dc54b5989c7d9bce202c6bf2
Bondarenko-I-M <130083850+Bondarenko-I-M@users.noreply.github.com>
2023-11-12 15:12:57 +0200

    TC_10.002.18 | Views > Edit view>Rename view (#1639)

--- src/test/java/school/redrover/CreateNevViewTest.java
+++ src/test/java/school/redrover/CreateNevViewTest.java
@@ -7,10 +7,10 @@ import school.redrover.runner.BaseTest;
 
 
   public class CreateNevViewTest extends BaseTest {
-    private static final String PROJECT_NAME = "MyProject";
-
+     private static final String PROJECT_NAME = "MyProject";
+     
     @Test
-    public void createFreestPro() {
+    public void testCreateFreestPro() {
         getDriver().findElement(By.xpath("//a [@href = '/view/all/newJob']")).click();
         getDriver().findElement(By.id("name")).sendKeys(PROJECT_NAME);
         getDriver().findElement(By.xpath("//img[@class='icon-freestyle-project icon-xlg']")).click();
@@ -18,19 +18,34 @@ import school.redrover.runner.BaseTest;
         getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).click();
         getDriver().findElement(By.id("jenkins-home-link")).click();
 
-        String newFreestPro = getDriver().findElement(By.xpath("//tr[@id=\"job_MyProject\"]/td[3]/a/span")).getText();
+        String newFreestPro = getDriver().findElement(By.xpath(
+                "//tr[@id=\"job_MyProject\"]/td[3]/a/span")).getText();
         Assert.assertEquals(newFreestPro,"MyProject");
     }
 
-    @Test (dependsOnMethods = "createFreestPro")
+    @Test (dependsOnMethods = "testCreateFreestPro")
     public void testCreateView() {
-
         getDriver().findElement(By.xpath("//a[@aria-label='New View']")).click();
         getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("My view");
-        getDriver().findElement(By.xpath("//label[@for='hudson.model.MyView']")).click();
+        getDriver().findElement(By.xpath("//label[@for = 'hudson.model.ListView']")).click();
         getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
+        getDriver().findElement(By.name("Submit")).click();
 
-        String nameView = getDriver().findElement(By.xpath("//a[contains(text(), 'My view')]")).getText();
+        String nameView = getDriver().findElement(By.xpath(
+                "//div[@id='main-panel']/div[2]/div[1]/div[2]/a")).getText();
         Assert.assertEquals(nameView, "My view");
     }
+
+      @Test (dependsOnMethods = "testCreateView")
+      public void testRenameView() {
+          getDriver().findElement(By.xpath("//div[@id='projectstatus-tabBar']/div/div[1]/div[2]/a")).click();
+          getDriver().findElement(By.xpath("//div[@id='tasks']/div[4]/span/a")).click();
+          getDriver().findElement(By.name("name")).clear();
+          getDriver().findElement(By.name("name")).sendKeys("Job view");
+          getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate']")).click();
+
+          Assert.assertEquals(getDriver().findElement(
+                  By.xpath("//div[@id=\"main-panel\"]/div[2]/div[1]/div[2]/a")).getText(),
+                  "Job view");
+      }
   }
