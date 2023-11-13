b7db3d52c2cb1bedd1f2d115287437a1249949f1
Yakov Tereshenkov <50947571+tereshenkov29@users.noreply.github.com>
2023-11-13 14:40:27 +0300

    13.001.09 (#1657)

---
+++ src/test/java/school/redrover/User4Test.java
@@ -0,0 +1,42 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+import java.util.List;
+
+public class User4Test extends BaseTest {
+
+    @Test
+    public void testUserCreation() {
+        final String username = "testUser";
+        final String password = "1";
+        final String email = "test@test.com";
+
+        getDriver().findElement(By.xpath("//a[@href = '/manage']")).click();
+        getDriver().findElement(By.xpath("//a[@href = 'securityRealm/']")).click();
+        getDriver().findElement(By.xpath("//a[@href = 'addUser']")).click();
+        getDriver().findElement(By.xpath("//input[@name = 'username']")).sendKeys(username);
+        getDriver().findElement(By.xpath("//input[@name = 'password1']")).sendKeys(password);
+        getDriver().findElement(By.xpath("//input[@name = 'password2']")).sendKeys(password);
+        getDriver().findElement(By.xpath("//input[@name = 'email']")).sendKeys(email);
+        getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
+        List<WebElement> listOfUserID = getDriver().findElements(By.xpath(
+                "//td/a[@class = 'jenkins-table__link model-link inside']"));
+
+        Assert.assertFalse(listOfUserID.isEmpty());
+
+        boolean isNewUserIDShown = false;
+        for (WebElement webElement : listOfUserID) {
+            if (webElement.getText().contains(username)) {
+                isNewUserIDShown = true;
+                break;
+            }
+        }
+
+        Assert.assertTrue(isNewUserIDShown);
+    }
+}
