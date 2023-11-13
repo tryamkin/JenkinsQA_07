180b3e6a3b59d2bc7eed208a17f2e2ce01fc874a
DmitriyKlv <rtjoyhome8@gmail.com>
2023-11-13 09:52:22 +0100

    TС_01.002.012 | Pipeline > Renaming a Pipeline

--- src/test/java/school/redrover/Pipeline1Test.java
+++ src/test/java/school/redrover/Pipeline1Test.java
@@ -2,12 +2,18 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.time.Duration;
 import java.util.List;
+
+import static school.redrover.FreestyleProject4Test.PROJECT_NAME;
 
 public class Pipeline1Test extends BaseTest {
     private final static String PIPELINE_NAME = "Pipeline name test";
@@ -29,7 +35,7 @@ public class Pipeline1Test extends BaseTest {
         getDriver().findElement(By.id(HOME_PAGE)).click();
     }
 
-    @Ignore
+
     @Test
     public void testCreatePipeline() {
         getDriver().findElement(By.id(HOME_PAGE));
@@ -47,7 +53,7 @@ public class Pipeline1Test extends BaseTest {
         Assert.assertEquals(actualName, PIPELINE_NAME);
     }
 
-    @Ignore
+
     @Test
     public void testPipelineDelete() {
         createProject();
@@ -60,7 +66,7 @@ public class Pipeline1Test extends BaseTest {
         Assert.assertTrue(deleted, "Element is not present after deletion");
     }
 
-    @Ignore
+
     @Test
     public void testPipelineBuildNowLaunch() {
         createProject();
@@ -70,10 +76,10 @@ public class Pipeline1Test extends BaseTest {
 
         String actualMessage = getDriver()
                 .findElement(By.xpath("//span[@class='pipeline-new-node'][1]")).getText();
-        Assert.assertEquals(actualMessage,"[Pipeline] Start of Pipeline");
+        Assert.assertEquals(actualMessage, "[Pipeline] Start of Pipeline");
     }
 
-    @Ignore
+
     @Test
     public void testPipelineAddDescriptionInConfiguration() {
         createProject();
@@ -89,10 +95,10 @@ public class Pipeline1Test extends BaseTest {
         String actualDescription = getDriver().findElement(By.xpath("//*[@id='description']/div[1]"))
                 .getText();
 
-        Assert.assertEquals(actualDescription,"Test description");
+        Assert.assertEquals(actualDescription, "Test description");
     }
 
-    @Ignore
+
     @Test
     public void testPipelineDeleteDescriptionInConfiguration() {
         createProject();
@@ -112,6 +118,27 @@ public class Pipeline1Test extends BaseTest {
         List<WebElement> elements = getDriver().findElements(By.xpath(DESCRIPTION_SEARCH));
         boolean deletedDescription = elements.isEmpty();
         Assert.assertTrue(deletedDescription, "Element is not present after deletion");
+    }
+
+    @Test
+    public void testPipelineRename() {
+        createProject();
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(getDriver()
+                .findElement(By.xpath("(//button[@class='jenkins-menu-dropdown-chevron'])[3]"))).perform();
+        getWait5().until(ExpectedConditions
+                .visibilityOfElementLocated(By.xpath("(//button[@class='jenkins-menu-dropdown-chevron'])[3]"))).click();
+        getWait5().until(ExpectedConditions
+                .visibilityOfElementLocated(By.xpath("//a[@class='jenkins-dropdown__item'][4]"))).click();
+
+        getDriver().findElement(By.xpath("//*[@name='newName']")).clear();
+        getDriver().findElement(By.xpath("//*[@name='newName']")).sendKeys("Pipeline rename name");
+        getDriver().findElement(By.xpath("//*[@name='Submit']")).click();
+
+        getDriver().findElement(By.id(HOME_PAGE)).click();
+        String actualDescription = getDriver().findElement(By.xpath("//*[@class='jenkins-table__link model-link inside']"))
+                .getText();
+        Assert.assertEquals(actualDescription, "Pipeline rename name");
     }
 }
 
