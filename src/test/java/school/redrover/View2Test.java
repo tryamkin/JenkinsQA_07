41889d4455008df1d596a68a970c16c36423ccc6
valeriia-vasylyshyna <110113695+valeriia-vasylyshyna@users.noreply.github.com>
2023-11-13 11:25:43 +0200

    TC_10.003.04 | Views > Delete view >Verify the view is deleted from the Dashboard (#1655)

--- src/test/java/school/redrover/View2Test.java
+++ src/test/java/school/redrover/View2Test.java
@@ -8,49 +8,54 @@ import school.redrover.runner.BaseTest;
 
 public class View2Test extends BaseTest {
 
-    private void createFreestyleProject (String freestyleProjectName) {
+    private static final String MY_VIEW_NAME = "testView";
+    private static final String FREESTYLE_PROJECT_NAME = "testProject";
+    private static final String NEW_VIEW_NAME = "renamedView";
+
+    private void createFreestyleProject () {
         getDriver().findElement(By.className("task")).click();
-        getDriver().findElement(By.id("name")).sendKeys(freestyleProjectName);
+        getDriver().findElement(By.id("name")).sendKeys(FREESTYLE_PROJECT_NAME);
         getDriver().findElement(By.xpath("//*[@id='j-add-item-type-nested-projects']/ul/li[1]")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.xpath("//*[@id='bottom-sticker']/div/button[1]")).click();
         getDriver().findElement(By.id("jenkins-name-icon")).click();
     }
 
-    private void createNewView (String myViewName) {
-        getDriver().findElement(By.className("addTab")).click();
-        getDriver().findElement(By.id("name")).sendKeys(myViewName);
-        getDriver().findElement(By.xpath("//*[@id='createItemForm']/div[1]/div[2]/fieldset/div[2]/label")).click();
-        getDriver().findElement(By.name("Submit")).click();
-    }
-
     @Test
     public void testCreateNewView () {
-        final String myViewName = "testView";
-
-        createFreestyleProject("testProject");
-        createNewView(myViewName);
-
-        WebElement getViewName = getDriver().findElement(By.xpath("//a[@href='/view/"+ myViewName +"/']"));
-
-        Assert.assertEquals(getViewName.getText(), myViewName);
-    }
-
-    @Test
-    public void testRenameView () {
-        final String newViewName = "renamedView";
-        final String myViewName = "testView";
-
-        createFreestyleProject("testProject");
-        createNewView(myViewName);
-
-        getDriver().findElement(By.xpath("//*[@id='tasks']/div[4]/span/a")).click();
-        getDriver().findElement(By.name("name")).clear();
-        getDriver().findElement(By.name("name")).sendKeys(newViewName);
+        createFreestyleProject();
+        getDriver().findElement(By.className("addTab")).click();
+        getDriver().findElement(By.id("name")).sendKeys(MY_VIEW_NAME);
+        getDriver().findElement(By.xpath("//*[@id='createItemForm']/div[1]/div[2]/fieldset/div[2]/label")).click();
         getDriver().findElement(By.name("Submit")).click();
 
-        WebElement getNewViewName = getDriver().findElement(By.xpath("//a[@href='/view/"+ newViewName +"/']"));
+        WebElement getViewName = getDriver().findElement(By.xpath("//a[@href='/view/"+ MY_VIEW_NAME +"/']"));
 
-        Assert.assertEquals(getNewViewName.getText(), newViewName);
+        Assert.assertEquals(getViewName.getText(), MY_VIEW_NAME);
     }
+
+    @Test(dependsOnMethods = "testCreateNewView")
+    public void testRenameView () {
+        getDriver().findElement(By.xpath("//a[normalize-space()='testView']")).click();
+        getDriver().findElement(By.xpath("//a[@href='/view/" + MY_VIEW_NAME + "/configure']")).click();
+        getDriver().findElement(By.name("name")).clear();
+        getDriver().findElement(By.name("name")).sendKeys(NEW_VIEW_NAME);
+        getDriver().findElement(By.name("Submit")).click();
+
+        WebElement getNewViewName = getDriver().findElement(By.xpath("//a[@href='/view/"+ NEW_VIEW_NAME +"/']"));
+
+        Assert.assertEquals(getNewViewName.getText(), NEW_VIEW_NAME);
+    }
+
+    @Test(dependsOnMethods = {"testCreateNewView", "testRenameView"})
+    public void testDeleteView () {
+        getDriver().findElement(By.xpath("//a[@href='/view/"+ NEW_VIEW_NAME +"/']")).click();
+        getDriver().findElement(By.xpath("//span[text()='Delete View']")).click();
+        getDriver().switchTo().alert().accept();
+
+        String getTabBarText = getDriver().findElement(By.xpath("//div[@class='tab']")).getText();
+
+        Assert.assertFalse(getTabBarText.contains(NEW_VIEW_NAME));
+    }
+
 }
