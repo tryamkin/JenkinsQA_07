c0e050ad7874871a8767391a13a2e5f9bdcf1f5b
DmitriyKlv <rtjoyhome8@gmail.com>
2023-11-13 10:07:16 +0100

    TС_01.002.012 | Pipeline > Renaming a Pipeline

--- src/test/java/school/redrover/Pipeline1Test.java
+++ src/test/java/school/redrover/Pipeline1Test.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
