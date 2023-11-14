7888c6be34f02efcf2899865cf14b04cac51c78a
Denis Prokofyev <denisprokofyev@gmail.com>
2023-11-14 20:43:59 +0300

    TC_05.002.12 | Multibranch pipeline > Rename Multibranch pipeline using dropdown in the breadcrumb

--- src/test/java/school/redrover/MultibranchPipelineTest.java
+++ src/test/java/school/redrover/MultibranchPipelineTest.java
@@ -1,7 +1,6 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
