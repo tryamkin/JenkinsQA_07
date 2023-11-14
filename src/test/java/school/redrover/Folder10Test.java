dfa7dee3263794937019378b5090dcb3bc2f7869
Vladimir Fokichev <106383598+vfokichev2022@users.noreply.github.com>
2023-11-14 21:29:43 +0300

    TC_04.001.13 folder rename test after folder create (#1693)

--- src/test/java/school/redrover/Folder10Test.java
+++ src/test/java/school/redrover/Folder10Test.java
@@ -7,18 +7,36 @@ import school.redrover.runner.BaseTest;
 
 public class Folder10Test extends BaseTest {
 
-    private static final String NEW_ITEM_NAME = "testfolder";
+    private static final String ITEM_NAME = "testfolder";
+    private static final String NEW_ITEM_NAME = "testfolder_rename";
 
     @Test
     public void testCreate() {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(NEW_ITEM_NAME);
+        getDriver().findElement(By.id("name")).sendKeys(ITEM_NAME);
         getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
         getDriver().findElement(By.id("ok-button")).click();
-
         getDriver().findElement(By.name("Submit")).click();
-        getDriver().findElement(By.id("jenkins-home-link")).click();
 
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+        getDriver().findElement(By.xpath("//td/a[@href='job/" + ITEM_NAME + "/']")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//div[@id='main-panel']//h1")).getText(),
+                ITEM_NAME);
+    }
+
+    @Test(dependsOnMethods = "testCreate")
+    public void testRename() {
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+        getDriver().findElement(By.xpath("//td/a[@href='job/" + ITEM_NAME + "/']")).click();
+
+        getDriver().findElement(By.xpath("//a[@href='/job/" + ITEM_NAME + "/confirm-rename']")).click();
+        getDriver().findElement(By.name("newName")).clear();
+        getDriver().findElement(By.name("newName")).sendKeys(NEW_ITEM_NAME);
+        getDriver().findElement(By.name("Submit")).click();
+
+        getDriver().findElement(By.id("jenkins-home-link")).click();
         getDriver().findElement(By.xpath("//td/a[@href='job/" + NEW_ITEM_NAME + "/']")).click();
 
         Assert.assertEquals(
