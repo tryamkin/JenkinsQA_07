de660c3f69e62b58eb959107760f209ddcc695c2
AllaBird <146133797+AllaBird@users.noreply.github.com>
2023-11-14 03:16:09 -0500

    Update Folder3Test.java

--- src/test/java/school/redrover/Folder3Test.java
+++ src/test/java/school/redrover/Folder3Test.java
@@ -1,7 +1,6 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
