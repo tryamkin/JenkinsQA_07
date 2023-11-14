02eb4051d0fdc0a2dcb673f2bdcda76ff47f98fd
Olgla <elenagladenko@gmail.com>
2023-11-14 16:16:01 -0500

    test FullName Appears Same As UserID when left empty during new user creation

--- src/test/java/school/redrover/AdditionalUtils.java
+++ src/test/java/school/redrover/AdditionalUtils.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import school.redrover.runner.BaseTest;
 
+import java.security.SecureRandom;
 import java.util.UUID;
 
 public class AdditionalUtils extends BaseTest{
@@ -15,6 +16,28 @@ public class AdditionalUtils extends BaseTest{
                 .substring(0, 5);
         return randomName;
     }
+
+    public static String generateRandomPassword(int length) {
+        final String charLower = "abcdefghijklmnopqrstuvwxyz";
+        final String charUpper = charLower.toUpperCase();
+        final String number = "0123456789";
+
+        final String randomString = charLower + charUpper + number;
+        SecureRandom random = new SecureRandom();
+
+        if (length <= 0) {
+            throw new IllegalArgumentException("Length must be a positive number");
+        }
+
+        StringBuilder randomPassword = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int rndCharAt = random.nextInt(randomString.length());
+            char rndChar = randomString.charAt(rndCharAt);
+            randomPassword.append(rndChar);
+        }
+        return randomPassword.toString();
+    }
+
 
     public static void jsClick(WebDriver driver, WebElement element) {
         JavascriptExecutor executor = (JavascriptExecutor) driver;
