b8772094fca1f7e10e0e6b5a4ebbad4acdfefae6
Yuliyash1106 <yuliyash1106@gmail.com>
2023-11-14 22:07:22 -0500

    Refactoring of tasks TC_02.007.11 and TC_00.002.13

--- src/test/java/school/redrover/Pipeline13Test.java
+++ src/test/java/school/redrover/Pipeline13Test.java
@@ -2,53 +2,47 @@ package school.redrover;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class Pipeline13Test extends BaseTest {
-    final String pipeLineName = "PipeLineProject";
+    private  final static String PIPE_LINE_NAME = "PipeLineProject";
+    private  final static String PIPE_LINE_NEW_NAME = "PipeLineNewProject";
 
-    private void createNewPipeline() {
-        getDriver().findElement(By.xpath("//a[@href = 'newJob']")).click();
-        getDriver().findElement(By.xpath("//input[@name = 'name']")).sendKeys(pipeLineName);
-        getDriver().findElement(By.xpath("//li[@class = 'org_jenkinsci_plugins_workflow_job_WorkflowJob']")).click();
+    private void createPipeline() {
+        getDriver().findElement(By.cssSelector("a[href='newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(PIPE_LINE_NAME);
+        getDriver().findElement(By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
         getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.name("Submit")).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.name ("Submit"))).click();
         getDriver().findElement(By.xpath("//a[contains(text(), 'Dashboard')]")).click();
-
     }
 
-    @Ignore
     @Test
-    public void testCreatPipeline() {
-        createNewPipeline();
+    public void testCreatePipeline() {
+        createPipeline();
 
-        getDriver().findElement(By.xpath("//span[text() = 'PipeLineProject']")).click();
+        getDriver().findElement(By.xpath("//span[text() = \"PipeLineProject\"]")).click();
 
         Assert.assertEquals(
                 getDriver().findElement(By.xpath("//h1[contains(text() ,'Pipeline PipeLineProject')]")).getText(),
                 "Pipeline PipeLineProject");
     }
 
-    @Ignore
-    @Test
+    @Test(dependsOnMethods = "testCreatePipeline")
     public void testRenamePipeline() {
-        final String pipeLineNewName = "PipeLineNewProject";
-
-        createNewPipeline();
-
         getDriver().findElement(By.xpath("//a[@class = 'jenkins-table__link model-link inside']")).click();
-        getDriver().findElement(By.xpath("//a[@href = '/job/PipeLineProject/confirm-rename']")).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href = '/job/PipeLineProject/confirm-rename']"))).click();
         getDriver().findElement(By.xpath("//input[@name = 'newName']")).clear();
-        getDriver().findElement(By.xpath("//input[@name = 'newName']")).sendKeys(pipeLineNewName);
+        getDriver().findElement(By.xpath("//input[@name = 'newName']")).sendKeys(PIPE_LINE_NEW_NAME);
         getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
-        getDriver().findElement(By.xpath("//a[contains(text(), 'Dashboard')]")).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(), 'Dashboard')]"))).click();
 
-        Assert.assertEquals(
-                getDriver().findElement(By.xpath("//span[text() = '" + pipeLineNewName + "']")).getText(),
-                pipeLineNewName);
+       Assert.assertEquals(
+                getDriver().findElement(By.xpath("//span[text() = '" + PIPE_LINE_NEW_NAME + "']")).getText(),
+               PIPE_LINE_NEW_NAME);
     }
 }
 
